a9009debf25359d05f69fa0da1c41d063922253b
tedspare <ted.spare@gmail.com>
2024-01-10 10:13:01 -0500

    Fix unused import warning

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,12 +1,14 @@
-import { NextRequest, NextResponse } from "next/server"
-import { db } from "@/server/db"
+import { type NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) { 
-  const { searchParams } = new URL(req.url)
-  const email = searchParams.get('email')
-  console.log(email)
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+  console.log(email);
 
   /* Add part to add to db */
 
-  return NextResponse.json({ message: "Signed up successfully." }, { status: 200 })
+  return NextResponse.json(
+    { message: "Signed up successfully." },
+    { status: 200 },
+  );
 }

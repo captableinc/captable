3113fd0162800f2a2f392d5434aecd01ef0461fa
Mehul Sharma <the8bitmehul@gmail.com>
2024-01-10 20:50:23 +0530

    Added prisma stuff

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -3,10 +3,18 @@ import { db } from "@/server/db"
 
 export async function GET(req: NextRequest) { 
   const { searchParams } = new URL(req.url)
-  const email = searchParams.get('email')
-  console.log(email)
+  const email = searchParams.get('email') as string
 
-  /* Add part to add to db */
+  try {
+    const newWaitlistUser = await db.waitlistUser.create({
+      data: {
+        email: email,
+      }
+    })
 
-  return NextResponse.json({ message: "Signed up successfully." }, { status: 200 })
+    return NextResponse.json({ message: `Waitlist user ${newWaitlistUser.id} created successfully ` }, { status: 200 })
+  } catch (e) {
+    return NextResponse.json({ message: e }, { status: 400 })
+  }
+
 }

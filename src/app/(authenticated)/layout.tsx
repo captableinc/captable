a3d0092c042cf147a1b374f4107f4b16e0dad38b
nafees nazik <nafeesnazik21@gmail.com>
2024-01-12 00:17:59 +0530

    feat: add authenticated layout

---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,15 @@
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+export default async function AuthenticatedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+  return <>{children}</>;
+}

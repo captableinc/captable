cab9626cff528e9cce82ac1e5f8e4717e0e2dfe4
nafees nazik <nafeesnazik21@gmail.com>
2024-01-12 08:19:14 +0530

    fix: skip validation

--- src/env.js
+++ src/env.js
@@ -57,7 +57,7 @@ export const env = createEnv({
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
    */
-  skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   /**
    * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
    * `SOME_VAR=''` will throw an error.

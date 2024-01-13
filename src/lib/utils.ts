530543878083bba9e4260f34b67fe5566bc230f3
Nana K <73991180+topboyasante@users.noreply.github.com>
2024-01-13 17:19:46 +0000

    fix: uploader UI Fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getFileSizeSuffix(bytes: number): string {
+  const suffixes = ["", "K", "M", "G", "T"];
+  const magnitude = Math.floor(Math.log2(bytes) / 10);
+  const suffix = suffixes[magnitude] + "B";
+  return suffix;
+}

4907f078dc042e92591b4a8f4691899782c7346d
nafees nazik <nafeesnazik21@gmail.com>
2024-01-14 10:19:06 +0530

    feat: add onboarded layout

---
+++ src/app/(authenticated)/(onboarded)/layout.tsx
@@ -0,0 +1,16 @@
+import { withServerSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+export default async function OnboardedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await withServerSession();
+
+  if (!session.user.isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  return <>{children}</>;
+}

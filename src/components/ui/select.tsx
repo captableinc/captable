1ec3000cc1a7874bc923147ce835a298310935ad
nafees nazik <nafeesnazik21@gmail.com>
2024-01-17 15:06:31 +0530

    chore: use remix icon

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -2,9 +2,13 @@
 
 import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
-import { Check, ChevronDown, ChevronUp } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import {
+  RiArrowUpSLine,
+  RiCheckLine,
+  RiArrowDownSLine,
+} from "@remixicon/react";
 
 const Select = SelectPrimitive.Root;
 
@@ -26,7 +30,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
+      <RiArrowDownSLine className="h-4 w-4 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
@@ -44,7 +48,7 @@ const SelectScrollUpButton = React.forwardRef<
     )}
     {...props}
   >
-    <ChevronUp className="h-4 w-4" />
+    <RiArrowUpSLine aria-hidden className="h-4 w-4" />
   </SelectPrimitive.ScrollUpButton>
 ));
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
@@ -61,7 +65,7 @@ const SelectScrollDownButton = React.forwardRef<
     )}
     {...props}
   >
-    <ChevronDown className="h-4 w-4" />
+    <RiArrowDownSLine className="h-4 w-4" />
   </SelectPrimitive.ScrollDownButton>
 ));
 SelectScrollDownButton.displayName =
@@ -125,7 +129,7 @@ const SelectItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
+        <RiCheckLine aria-hidden className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
 
--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ToastPrimitives from "@radix-ui/react-toast";
 import { cva, type VariantProps } from "class-variance-authority";
-import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { RiCloseLine } from "@remixicon/react";
 
 const ToastProvider = ToastPrimitives.Provider;
 
@@ -81,7 +81,7 @@ const ToastClose = React.forwardRef<
     toast-close=""
     {...props}
   >
-    <X className="h-4 w-4" />
+    <RiCloseLine className="h-4 w-4" />
   </ToastPrimitives.Close>
 ));
 ToastClose.displayName = ToastPrimitives.Close.displayName;

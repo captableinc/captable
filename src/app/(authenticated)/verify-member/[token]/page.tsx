37dbf6ee42ee7200b299cdd1de45acd58d88d308
nafees nazik <nafeesnazik21@gmail.com>
2024-01-18 15:34:56 +0530

    feat: use refactored logic

--- src/app/(authenticated)/verify-member/[token]/page.tsx
+++ src/app/(authenticated)/verify-member/[token]/page.tsx
@@ -1,6 +1,6 @@
 import { VerifyMemberForm } from "@/components/stakeholder/verify-member-form";
 import { withServerSession } from "@/server/auth";
-import { db } from "@/server/db";
+import { handleVerificationToken } from "@/server/stakeholder";
 
 export default async function VerifyMember({
   params: { token },
@@ -9,29 +9,10 @@ export default async function VerifyMember({
 }) {
   const session = await withServerSession();
 
-  const verificationToken = await db.verificationToken.findFirst({
-    where: {
-      token,
-    },
-  });
-
-  if (!verificationToken) {
-    throw new Error("invalid token");
-  }
-
-  const [userEmail, membershipId] = verificationToken.identifier.split(":");
-
-  if (!membershipId) {
-    throw new Error("membership id not found");
-  }
-
-  if (!userEmail) {
-    throw new Error("user email not found");
-  }
-
-  if (session.user.email !== userEmail) {
-    throw new Error("invalid email");
-  }
+  const { membershipId } = await handleVerificationToken(
+    token,
+    session.user.email,
+  );
 
   return <VerifyMemberForm membershipId={membershipId} />;
 }
--- src/server/stakeholder.ts
+++ src/server/stakeholder.ts
@@ -1,3 +1,4 @@
+import { type Prisma } from "@prisma/client";
 import { db } from "./db";
 
 export const getMembers = (companyId: string) => {
@@ -17,3 +18,54 @@ export const getMembers = (companyId: string) => {
 };
 
 export type TypeGetMembers = Awaited<ReturnType<typeof getMembers>>;
+
+// credits https://github.com/nextauthjs/next-auth/blob/46264fb42af4c3ef7137a5694875eaa1309462ea/packages/adapter-prisma/src/index.ts
+
+export const deleteVerificationToken = async (token: string) => {
+  try {
+    const verificationToken = await db.verificationToken.delete({
+      where: {
+        token,
+      },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    // If token already used/deleted, just return null
+    // https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+    if ((error as Prisma.PrismaClientKnownRequestError).code === "P2025")
+      return null;
+    throw error;
+  }
+};
+
+export const handleVerificationToken = async (
+  token: string,
+  userEmail: string | null | undefined,
+) => {
+  // based on https://github.com/nextauthjs/next-auth/blob/46264fb42af4c3ef7137a5694875eaa1309462ea/packages/core/src/lib/actions/callback/index.ts#L200
+  const invite = await deleteVerificationToken(token);
+  const hasInvite = !!invite;
+  const expired = invite ? invite.expires.valueOf() < Date.now() : undefined;
+  const invalidInvite = !hasInvite || expired;
+
+  if (invalidInvite) {
+    throw new Error("invalid invite or invite expired");
+  }
+
+  const [email, membershipId] = invite.identifier.split(":");
+
+  if (!membershipId) {
+    throw new Error("membership id not found");
+  }
+
+  if (!email) {
+    throw new Error("user email not found");
+  }
+
+  if (userEmail !== email) {
+    throw new Error("invalid email");
+  }
+
+  return { membershipId, email };
+};

5924ce9209beea2809040985bcbb37ec2e241cf7
nafees nazik <nafeesnazik21@gmail.com>
2024-01-22 21:28:51 +0530

    feat: add types

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import {
+  type AuditActions as TAuditActions,
+  type AuditActor as TAuditActor,
+  type AuditContext as TAuditContext,
+  type AuditTarget as TAuditTarget,
+} from "@/server/audit/types";
 
 import { env } from "@/env";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace PrismaJson {
+    type AuditAction = TAuditActions;
+    type AuditActor = TAuditActor;
+    type AuditTarget = TAuditTarget;
+    type AuditContext = TAuditContext;
+  }
+}
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;

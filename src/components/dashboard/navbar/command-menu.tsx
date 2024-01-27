5c09752c00fe556c6f3a248f3a0b23f7fe552c4e
Puru D <puru@dahal.me>
2024-01-26 23:10:52 -0600

    fix: linting errors

--- src/components/dashboard/navbar/command-menu.tsx
+++ src/components/dashboard/navbar/command-menu.tsx
@@ -73,7 +73,7 @@ export function CommandMenu() {
         </div>
         <p className="text-sm text-muted-foreground">
           {" "}
-          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-3 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <kbd className="font-mono pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-3">
             <span className="text-sm">⌘</span>K
           </kbd>
         </p>

f33c030154ad3e0994d7ebeb907bc7eb96e18bef
Puru Dahal <dahal@users.noreply.github.com>
2024-01-27 02:15:37 -0600

    feat: add progress bar provider when transitioning between pages (#76)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { cookies } from "next/headers";
 import { TRPCReactProvider } from "@/trpc/react";
 import { type Metadata } from "next";
+import ProgressBarProvider from "./ProgressBarProvider";
 import { constants } from "@/lib/constants";
 import { Toaster } from "@/components/ui/toaster";
 import { NextAuthProvider } from "@/providers/next-auth";
@@ -28,12 +29,14 @@ export default async function RootLayout({
   return (
     <html lang="en" className={cn(satoshi.variable, inter.variable)}>
       <body className="min-h-screen">
-        <NextAuthProvider session={session}>
-          <TRPCReactProvider cookies={cookies().toString()}>
-            <main>{children}</main>
-            <Toaster />
-          </TRPCReactProvider>
-        </NextAuthProvider>
+        <ProgressBarProvider>
+          <NextAuthProvider session={session}>
+            <TRPCReactProvider cookies={cookies().toString()}>
+              <main>{children}</main>
+              <Toaster />
+            </TRPCReactProvider>
+          </NextAuthProvider>
+        </ProgressBarProvider>
       </body>
     </html>
   );

adf42540b0d68160ea31995c2030cc2f3c9c5459
Puru D <puru@dahal.me>
2024-01-29 18:15:37 -0600

    feat: add card component

--- src/app/(authenticated)/(dashboard)/[publicId]/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/page.tsx
@@ -3,6 +3,7 @@
 import { useParams } from "next/navigation";
 import { useSession } from "next-auth/react";
 import EmptyOverview from "@/components/overview/empty";
+import { Flex, Grid, Col, Card, Text, Metric } from "@tremor/react";
 
 const OverviewPage = () => {
   const params = useParams<{ publicId: string }>();
@@ -12,7 +13,26 @@ const OverviewPage = () => {
 
   return (
     <>
-      <EmptyOverview firstName={firstName} publicCompanyId={publicCompanyId} />
+      {/* <EmptyOverview firstName={firstName} publicCompanyId={publicCompanyId} /> */}
+
+      <div className="xs:grid-cols-1 grid gap-8 sm:grid-cols-2 lg:grid-cols-4 ">
+        <Card>
+          <Text>Title</Text>
+          <Metric>KPI 1</Metric>
+        </Card>
+        <Card>
+          <Text>Title</Text>
+          <Metric>KPI 2</Metric>
+        </Card>
+        <Card>
+          <Text>Title</Text>
+          <Metric>KPI 3</Metric>
+        </Card>
+        <Card>
+          <Text>Title</Text>
+          <Metric>KPI 4</Metric>
+        </Card>
+      </div>
     </>
   );
 };

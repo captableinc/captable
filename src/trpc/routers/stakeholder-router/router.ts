853264fe7a3b1a07612ee8cfedb289123cb59acb
Nafees Nazik <84864519+G3root@users.noreply.github.com>
2024-02-02 11:37:01 +0530

    feat: use trpc for server calls (#88)

--- src/trpc/routers/stakeholder-router/router.ts
+++ src/trpc/routers/stakeholder-router/router.ts
@@ -315,4 +315,32 @@ export const stakeholderRouter = createTRPCRouter({
 
       return { success: true };
     }),
+
+  getMembers: protectedProcedure.query(async ({ ctx }) => {
+    const {
+      db,
+      session: { user },
+    } = ctx;
+
+    const data = await db.membership.findMany({
+      where: {
+        companyId: user.companyId,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+            image: true,
+          },
+        },
+      },
+
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return { data };
+  }),
 });

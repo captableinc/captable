75915e4268f9f4512e4bf23d6d8ed65fcb0bc197
Puru D <puru@dahal.me>
2024-02-04 23:56:43 -0600

    feat: getting started with new share class

--- src/app/(authenticated)/(dashboard)/[publicId]/share-class/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/share-class/page.tsx
@@ -1,5 +1,28 @@
+"use client";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import EmptyState from "@/components/shared/empty-state";
+import { RiPieChart2Line, RiAddFill } from "@remixicon/react";
+
 const SharesPage = () => {
-  return <div>Shares classes page</div>;
+  const params = useParams<{ publicId: string }>();
+  const publicCompanyId = params.publicId;
+
+  return (
+    <EmptyState
+      icon={<RiPieChart2Line />}
+      title="You do not have any share classes!"
+      subtitle="Please click the button below to create a new share class."
+    >
+      <Link href={`/${publicCompanyId}/share-class/new`} passHref>
+        <Button size="lg">
+          <RiAddFill className="mr-2 h-5 w-5" />
+          Create a share class
+        </Button>
+      </Link>
+    </EmptyState>
+  );
 };
 
 export default SharesPage;

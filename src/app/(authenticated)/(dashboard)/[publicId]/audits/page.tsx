59344343b851e6148763a83dda83420c88a4e2d0
nafees nazik <nafeesnazik21@gmail.com>
2024-02-06 15:58:29 +0530

    feat: init layout

--- src/app/(authenticated)/(dashboard)/[publicId]/audits/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/audits/page.tsx
@@ -1,5 +1,16 @@
 const AuditsPage = () => {
-  return <div>Audits page</div>;
+  return (
+    <div className="flex flex-col gap-y-3">
+      <div className="flex items-center justify-between gap-y-3 ">
+        <div className="gap-y-3">
+          <h3 className="font-medium">Audits</h3>
+          <p className="text-sm text-muted-foreground">
+            Manage your company{`'`}s audits
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuditsPage;

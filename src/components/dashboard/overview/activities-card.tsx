5340801ff7158648304ad39e1a4916c2f09d93e0
nafees nazik <nafeesnazik21@gmail.com>
2024-02-06 22:00:24 +0530

    fix: use css only style

--- src/components/dashboard/overview/activities-card.tsx
+++ src/components/dashboard/overview/activities-card.tsx
@@ -26,15 +26,14 @@ const ActivityCard = async ({ className, publicId }: Props) => {
 
       <CardContent>
         <ul role="list" className="-mb-8">
-          {activity.data.map((activityItem, activityItemIdx) => (
-            <li key={activityItem.id}>
+          {activity.data.map((activityItem) => (
+            <li key={activityItem.id} className="group">
               <div className="relative pb-8">
-                {activityItemIdx !== activity.data.length - 1 ? (
-                  <span
-                    className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200"
-                    aria-hidden="true"
-                  />
-                ) : null}
+                <span
+                  className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200 group-last:hidden"
+                  aria-hidden="true"
+                />
+
                 <div className="relative flex items-start space-x-3">
                   <>
                     <div>

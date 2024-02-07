ca563a1ebb81f66edd8804c204cd57ed8b5534a2
nafees nazik <nafeesnazik21@gmail.com>
2024-02-07 11:11:23 +0530

    refactor: accept member procedure

---
+++ src/trpc/routers/stakeholder-router/accept-member.ts
@@ -0,0 +1,68 @@
+import { protectedProcedure } from "@/trpc/api/trpc";
+import { ZodAcceptMemberMutationSchema } from "./schema";
+import { Audit } from "@/server/audit";
+
+export const acceptMemberProcedure = protectedProcedure
+  .input(ZodAcceptMemberMutationSchema)
+  .mutation(async ({ ctx, input }) => {
+    const user = ctx.session.user;
+
+    const { publicId } = await ctx.db.$transaction(async (trx) => {
+      await trx.verificationToken.delete({
+        where: {
+          token: input.token,
+        },
+      });
+
+      await trx.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          name: input.name,
+        },
+      });
+
+      const membership = await trx.membership.update({
+        where: {
+          id: input.membershipId,
+        },
+        data: {
+          active: true,
+          status: "accepted",
+          lastAccessed: new Date(),
+          isOnboarded: true,
+          userId: user.id,
+        },
+        select: {
+          company: {
+            select: {
+              publicId: true,
+              name: true,
+              id: true,
+            },
+          },
+          userId: true,
+          access: true,
+          user: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+
+      await Audit.create({
+        action: "stakeholder.accepted",
+        companyId: membership.company.id,
+        actor: { type: "user", id: user.id },
+        context: {},
+        target: [{ type: "user", id: membership.userId }],
+        summary: `${membership?.user?.name} accepted to join ${membership.company.name} with ${membership.access} access`,
+      });
+
+      return { publicId: membership.company.publicId };
+    });
+
+    return { success: true, publicId };
+  });
--- src/trpc/routers/stakeholder-router/router.ts
+++ src/trpc/routers/stakeholder-router/router.ts
@@ -20,74 +20,12 @@ import {
 } from "@/server/stakeholder";
 import { Audit } from "@/server/audit";
 import { inviteMemberProcedure } from "./invite-member";
+import { acceptMemberProcedure } from "./accept-member";
 
 export const stakeholderRouter = createTRPCRouter({
   inviteMember: inviteMemberProcedure,
 
-  acceptMember: protectedProcedure
-    .input(ZodAcceptMemberMutationSchema)
-    .mutation(async ({ ctx, input }) => {
-      const user = ctx.session.user;
-
-      const { publicId } = await ctx.db.$transaction(async (trx) => {
-        await trx.verificationToken.delete({
-          where: {
-            token: input.token,
-          },
-        });
-
-        await trx.user.update({
-          where: {
-            id: user.id,
-          },
-          data: {
-            name: input.name,
-          },
-        });
-
-        const membership = await trx.membership.update({
-          where: {
-            id: input.membershipId,
-          },
-          data: {
-            active: true,
-            status: "accepted",
-            lastAccessed: new Date(),
-            isOnboarded: true,
-            userId: user.id,
-          },
-          select: {
-            company: {
-              select: {
-                publicId: true,
-                name: true,
-                id: true,
-              },
-            },
-            userId: true,
-            access: true,
-            user: {
-              select: {
-                name: true,
-              },
-            },
-          },
-        });
-
-        await Audit.create({
-          action: "stakeholder.accepted",
-          companyId: membership.company.id,
-          actor: { type: "user", id: user.id },
-          context: {},
-          target: [{ type: "user", id: membership.userId }],
-          summary: `${membership?.user?.name} accepted to join ${membership.company.name} with ${membership.access} access`,
-        });
-
-        return { publicId: membership.company.publicId };
-      });
-
-      return { success: true, publicId };
-    }),
+  acceptMember: acceptMemberProcedure,
 
   revokeInvite: adminOnlyProcedure
     .input(ZodRevokeInviteMutationSchema)

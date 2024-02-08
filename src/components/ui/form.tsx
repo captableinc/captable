03e0c2a116239a4e36c704ecfe9fee1f8929ab20
Puru D <puru@dahal.me>
2024-02-08 03:27:34 -0600

    chore: make error message smaller and lighter

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -156,7 +156,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
+      className={cn("text-xs font-light text-destructive", className)}
       {...props}
     >
       {body}

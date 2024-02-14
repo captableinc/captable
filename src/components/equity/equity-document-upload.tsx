72f8f3c6845113c0789daf50bf97d8e66e17307f
nafees nazik <nafeesnazik21@gmail.com>
2024-02-15 01:09:45 +0530

    feat: add equity document upload page

---
+++ src/components/equity/equity-document-upload.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Modal from "../shared/modal";
+import { Button } from "../ui/button";
+import { Uploader } from "../ui/uploader";
+
+interface EquityDocumentUploadProps {
+  companyPublicId: string;
+}
+
+export function EquityDocumentUpload({
+  companyPublicId,
+}: EquityDocumentUploadProps) {
+  const router = useRouter();
+
+  return (
+    <Modal
+      title="Upload equity document"
+      subtitle=""
+      trigger={<Button>Upload Document</Button>}
+    >
+      <Uploader
+        companyPublicId={companyPublicId}
+        keyPrefix="equity-doc-uploads"
+        type="EQUITY"
+        onSuccess={(data) => {
+          router.push(`/${companyPublicId}/documents/${data.publicId}`);
+        }}
+        accept={{
+          "application/pdf": [".pdf"],
+        }}
+      />
+    </Modal>
+  );
+}

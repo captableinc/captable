f6c46298912b7433455f08d7e9651277875e7839
nafees nazik <nafeesnazik21@gmail.com>
2024-02-15 00:49:01 +0530

    fix: schema

--- src/trpc/routers/document-router/schema.ts
+++ src/trpc/routers/document-router/schema.ts
@@ -1,10 +1,27 @@
 import { z } from "zod";
 
-export const DocumentMutationSchema = z.object({
-  key: z.string(),
+export const type = ["SAFE", "EQUITY", "GENERIC"] as const;
+
+export type TDocumentType = (typeof type)[number];
+
+export const uploadProvider = ["R2", "S3"] as const;
+
+export const ZodCreateDocumentMutationSchema = z.object({
   name: z.string(),
-  type: z.string(),
+  key: z.string(),
+  type: z.enum(type),
+  mimeType: z.string(),
   size: z.number(),
 });
 
-export type DocumentMutationType = z.infer<typeof DocumentMutationSchema>;
+export type TypeZodCreateDocumentMutationSchema = z.infer<
+  typeof ZodCreateDocumentMutationSchema
+>;
+
+export const ZodGetDocumentQuerySchema = z.object({
+  publicId: z.string(),
+});
+
+export type TypeZodGetDocumentQuerySchema = z.infer<
+  typeof ZodGetDocumentQuerySchema
+>;

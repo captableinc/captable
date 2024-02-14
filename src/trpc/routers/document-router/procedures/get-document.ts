b9a1db0e353bd40679eb3e2660d5340aa83a963b
nafees nazik <nafeesnazik21@gmail.com>
2024-02-15 00:47:15 +0530

    feat: add get document procedure

---
+++ src/trpc/routers/document-router/procedures/get-document.ts
@@ -0,0 +1,21 @@
+import { protectedProcedure } from "@/trpc/api/trpc";
+import { ZodGetDocumentQuerySchema } from "../schema";
+import { getPresignedGetUrl } from "@/server/file-uploads";
+
+export const getDocumentProcedure = protectedProcedure
+  .input(ZodGetDocumentQuerySchema)
+  .query(async ({ ctx, input }) => {
+    const user = ctx.session.user;
+
+    const data = await ctx.db.document.findFirstOrThrow({
+      where: {
+        publicId: input.publicId,
+        companyId: user.companyId,
+      },
+      select: {
+        key: true,
+      },
+    });
+
+    return getPresignedGetUrl(data.key);
+  });

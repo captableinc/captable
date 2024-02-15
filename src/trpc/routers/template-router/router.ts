bc69831dd4b8a95e40d363d81d0f2bfb9eabfaf8
nafees nazik <nafeesnazik21@gmail.com>
2024-02-15 20:18:35 +0530

    feat: add get template procedure

--- src/trpc/routers/template-router/router.ts
+++ src/trpc/routers/template-router/router.ts
@@ -1,6 +1,8 @@
 import { createTRPCRouter } from "@/trpc/api/trpc";
 import { createTemplateProcedure } from "./procedures/create-template";
+import { getTemplateProcedure } from "./procedures/get-template";
 
 export const templateRouter = createTRPCRouter({
   create: createTemplateProcedure,
+  get: getTemplateProcedure,
 });
--- src/trpc/routers/template-router/schema.ts
+++ src/trpc/routers/template-router/schema.ts
@@ -4,3 +4,7 @@ export const ZodCreateTemplateMutationSchema = z.object({
   name: z.string(),
   bucketId: z.string(),
 });
+
+export const ZodGetTemplateQuerySchema = z.object({
+  publicId: z.string(),
+});

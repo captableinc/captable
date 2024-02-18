c38115035c7d85423bb2238f341a26adb9075cc8
chetannn <chetankharel7@gmail.com>
2024-02-18 11:53:41 +0545

    fix: redirect user to dashboard
    redirect user to dashboard if they are logged in, when they visit login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,6 @@
+import { redirect } from "next/navigation";
 import LoginForm from "@/components/onboarding/login";
+import { withServerSession } from "@/server/auth";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,6 +8,12 @@ export const metadata: Metadata = {
   description: "Login to OpenCap",
 };
 
-export default function AuthPage() {
+export default async function AuthPage() {
+  const session = await withServerSession();
+
+  if (session.user && session.user.companyPublicId) {
+    return redirect(`/${session.user.companyPublicId}`);
+  }
+
   return <LoginForm />;
 }

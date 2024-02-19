86839b64474362b76f07da53cea4fff8a0951100
chetannn <chetankharel7@gmail.com>
2024-02-19 18:21:58 +0545

    fix: redirect from signup to dashboard if logged in

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,19 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 import SignupForm from "@/components/onboarding/signup";
+import { authOptions } from "@/server/auth";
 import { type Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "OpenCap - Sign Up",
   description: "Create an account with OpenCap",
 };
 
-export default function Onboarding() {
+export default async function Onboarding() {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user?.companyPublicId) {
+    return redirect(`/${session.user.companyPublicId}`);
+  }
   return <SignupForm />;
 }

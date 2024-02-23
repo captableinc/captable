bd9c84c6ddbe1f4e2caba5aec574b93ff0c30898
Puru Dahal <dahal@users.noreply.github.com>
2024-02-23 00:28:36 -0600

    feat: improve settings navigation (#140)

---
+++ src/app/(authenticated)/(dashboard)/[publicId]/settings/layout.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { PageLayout } from "@/components/dashboard/page-layout";
+import { NavLink } from "@/components/dashboard/sidebar/nav-link";
+import {
+  RiGroup2Line,
+  RiGroup2Fill,
+  RiBuildingLine,
+  RiBuildingFill,
+  RiAccountCircleLine,
+  RiAccountCircleFill,
+  RiBankCardLine,
+  RiBankCardFill,
+  RiNotificationLine,
+  RiNotificationFill,
+} from "@remixicon/react";
+
+const companyNav = [
+  {
+    name: "Company",
+    href: "/settings/company",
+    icon: RiBuildingLine,
+    activeIcon: RiBuildingFill,
+  },
+  {
+    name: "Team",
+    href: "/settings/team",
+    icon: RiGroup2Line,
+    activeIcon: RiGroup2Fill,
+  },
+  {
+    name: "Billing",
+    href: "/settings/billing",
+    icon: RiBankCardLine,
+    activeIcon: RiBankCardFill,
+  },
+];
+
+const accountNav = [
+  {
+    name: "Profile",
+    href: "/settings/profile",
+    icon: RiAccountCircleLine,
+    activeIcon: RiAccountCircleFill,
+  },
+  {
+    name: "Notifications",
+    href: "/settings/notifications",
+    icon: RiNotificationLine,
+    activeIcon: RiNotificationFill,
+  },
+];
+
+const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
+  const currentPath = usePathname();
+  const { data } = useSession();
+  const companyPublicId = data?.user.companyPublicId;
+
+  return (
+    <PageLayout title="Settings">
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-12 md:col-span-3">
+          <ul role="list" className="text-sm">
+            {companyNav.map((item) => {
+              const href = `/${companyPublicId}${item.href}`;
+              const isActive = currentPath === href;
+
+              return (
+                <li key={item.name} className="rounded py-1">
+                  <NavLink
+                    active={isActive}
+                    name={item.name}
+                    href={href}
+                    icon={isActive ? item.activeIcon : item.icon}
+                  />
+                </li>
+              );
+            })}
+
+            <div className="mt-3 text-xs font-semibold leading-6 text-gray-400">
+              Account
+            </div>
+
+            {accountNav.map((item) => {
+              const href = `/${companyPublicId}${item.href}`;
+              const isActive = currentPath === href;
+
+              return (
+                <li key={item.name} className="rounded py-1">
+                  <NavLink
+                    active={isActive}
+                    name={item.name}
+                    href={href}
+                    icon={isActive ? item.activeIcon : item.icon}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="col-span-12 md:col-span-9">
+          <Card className="p-5">{children}</Card>
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default SettingsLayout;

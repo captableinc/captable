93ed1fb1e52e29acfd093e5f982ac53c32bf7faa
nafees nazik <nafeesnazik21@gmail.com>
2024-02-24 13:08:25 +0530

    refactor: create document procedure

--- src/trpc/routers/document-router/procedures/create-document.ts
+++ src/trpc/routers/document-router/procedures/create-document.ts
@@ -1,21 +1,36 @@
-import { withAuth } from "@/trpc/api/trpc";
-import { ZodCreateDocumentMutationSchema } from "../schema";
+import { withAuth, type withAuthTrpcContextType } from "@/trpc/api/trpc";
+import {
+  type TypeZodCreateDocumentMutationSchema,
+  ZodCreateDocumentMutationSchema,
+} from "../schema";
 import { generatePublicId } from "@/common/id";
+
+interface createDocumentHandlerOptions {
+  input: TypeZodCreateDocumentMutationSchema;
+  ctx: withAuthTrpcContextType;
+}
+
+export const createDocumentHandler = async ({
+  ctx,
+  input,
+}: createDocumentHandlerOptions) => {
+  const user = ctx.session.user;
+  const publicId = generatePublicId();
+
+  const document = await ctx.db.document.create({
+    data: {
+      companyId: user.companyId,
+      uploaderId: user.memberId,
+      publicId,
+      ...input,
+    },
+  });
+
+  return document;
+};
 
 export const createDocumentProcedure = withAuth
   .input(ZodCreateDocumentMutationSchema)
-  .mutation(async ({ ctx, input }) => {
-    const user = ctx.session.user;
-    const publicId = generatePublicId();
-
-    const document = await ctx.db.document.create({
-      data: {
-        companyId: user.companyId,
-        uploaderId: user.memberId,
-        publicId,
-        ...input,
-      },
-    });
-
-    return document;
+  .mutation(async (opts) => {
+    return createDocumentHandler(opts);
   });

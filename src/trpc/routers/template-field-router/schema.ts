eddd910bead8bc7576e7e48f47c0eb28e8e71ed9
nafees nazik <nafeesnazik21@gmail.com>
2024-02-26 21:56:57 +0530

    feat: add new viewport  fields

--- src/trpc/routers/template-field-router/schema.ts
+++ src/trpc/routers/template-field-router/schema.ts
@@ -15,6 +15,8 @@ export const ZodAddFieldMutationSchema = z.object({
       required: z.boolean(),
       placeholder: z.string(),
       type: z.nativeEnum(FieldTypes),
+      viewportHeight: z.number(),
+      viewportWidth: z.number(),
     }),
   ),
 });

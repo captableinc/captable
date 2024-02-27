ffd9b105f23fcc52314da7fa98f999e432c3ef68
nafees nazik <nafeesnazik21@gmail.com>
2024-02-27 15:02:05 +0530

    refactor: field provider

--- src/providers/template-field-provider.tsx
+++ src/providers/template-field-provider.tsx
@@ -5,15 +5,12 @@ import { type ReactNode } from "react";
 import { useForm } from "react-hook-form";
 import { type TypeZodAddFieldMutationSchema } from "@/trpc/routers/template-field-router/schema";
 import { type FieldTypes } from "@/prisma-enums";
-import { api } from "@/trpc/react";
-import { useToast } from "@/components/ui/use-toast";
 
 type Field = TypeZodAddFieldMutationSchema["data"][number];
 
 interface TemplateFieldProviderProps {
   children: ReactNode;
   fields?: Field[];
-  templatePublicId: string;
 }
 
 export type TemplateFieldForm = {
@@ -24,10 +21,7 @@ export type TemplateFieldForm = {
 export const TemplateFieldProvider = ({
   children,
   fields,
-  templatePublicId,
 }: TemplateFieldProviderProps) => {
-  const { toast } = useToast();
-
   const form = useForm<TemplateFieldForm>({
     defaultValues: {
       fields: fields ?? [],
@@ -35,28 +29,5 @@ export const TemplateFieldProvider = ({
     },
   });
 
-  const { mutateAsync } = api.templateField.add.useMutation({
-    onSuccess: () => {
-      toast({
-        variant: "default",
-        title: "🎉 Successfully created",
-        description: "Your template fields has been created.",
-      });
-    },
-  });
-
-  const onSubmit = async (values: TemplateFieldForm) => {
-    await mutateAsync({ templatePublicId, data: values.fields });
-  };
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex w-full flex-col"
-      >
-        {children}
-      </form>
-    </Form>
-  );
+  return <Form {...form}>{children}</Form>;
 };

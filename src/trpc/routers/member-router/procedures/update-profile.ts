7b6b4a31b7e333b2e9fc335c8bb70d8f28c71b8b
Raju-github-profile <rajukadel.github@gmail.com>
2024-02-27 18:29:23 +0545

    update to update-profile-procedures

--- src/trpc/routers/member-router/procedures/update-profile.ts
+++ src/trpc/routers/member-router/procedures/update-profile.ts
@@ -5,52 +5,100 @@ import { Audit } from "@/server/audit";
 export const updateProfileProcedure = withAuth
   .input(ZodUpdateProfileMutationSchema)
   .mutation(async ({ ctx: { session, db, requestIp, userAgent }, input }) => {
-    const { fullName, loginEmail, workEmail, jobTitle, avatarUrl } = input;
     const user = session.user;
 
-    await db.$transaction(async (tx) => {
-      const member = await tx.member.update({
-        where: {
-          status: "ACTIVE",
-          id: user.memberId,
-          companyId: user.companyId,
-        },
-        data: {
-          title: jobTitle,
-          workEmail,
-          user: {
-            update: {
-              name: fullName,
-              email: loginEmail,
-              image: avatarUrl,
+    if (input.type === "profile") {
+      const { fullName, loginEmail, workEmail, jobTitle } = input.profile;
+
+      await db.$transaction(async (tx) => {
+        const member = await tx.member.update({
+          where: {
+            status: "ACTIVE",
+            id: user.memberId,
+            companyId: user.companyId,
+          },
+          data: {
+            title: jobTitle,
+            workEmail,
+            user: {
+              update: {
+                name: fullName,
+                email: loginEmail,
+              },
             },
           },
-        },
-        select: {
-          userId: true,
-          user: {
-            select: {
-              name: true,
+          select: {
+            userId: true,
+            user: {
+              select: {
+                name: true,
+              },
             },
           },
-        },
+        });
+
+        await Audit.create(
+          {
+            action: "member.updated",
+            companyId: user.companyId,
+            actor: { type: "user", id: user.id },
+            context: {
+              requestIp,
+              userAgent,
+            },
+            target: [{ type: "user", id: member.userId }],
+            summary: `${user.name} updated the profile information.`,
+          },
+          tx,
+        );
       });
 
-      await Audit.create(
-        {
-          action: "member.updated",
-          companyId: user.companyId,
-          actor: { type: "user", id: user.id },
-          context: {
-            requestIp,
-            userAgent,
-          },
-          target: [{ type: "user", id: member.userId }],
-          summary: `${user.name} updated the profile`,
-        },
-        tx,
-      );
-    });
+      return { success: true };
+    }
 
-    return { success: true };
+    if (input.type === "avatar") {
+      const { avatarUrl } = input;
+
+      await db.$transaction(async (tx) => {
+        const member = await tx.member.update({
+          where: {
+            status: "ACTIVE",
+            id: user.memberId,
+            companyId: user.companyId,
+          },
+          data: {
+            user: {
+              update: {
+                image: avatarUrl,
+              },
+            },
+          },
+          select: {
+            userId: true,
+            user: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        });
+
+        await Audit.create(
+          {
+            action: "member.updated",
+            companyId: user.companyId,
+            actor: { type: "user", id: user.id },
+            context: {
+              requestIp,
+              userAgent,
+            },
+            target: [{ type: "user", id: member.userId }],
+            summary: `${user.name} uploaded new profile avatar.`,
+          },
+          tx,
+        );
+      });
+
+      return { success: true };
+    }
   });

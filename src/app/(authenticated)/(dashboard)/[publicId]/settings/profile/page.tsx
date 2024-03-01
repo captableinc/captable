9f9bbbd9a58c5c174d5c4b0c4d975b25d028d201
chetannn <chetankharel7@gmail.com>
2024-03-01 11:17:21 +0545

    style: formatting

--- src/app/(authenticated)/(dashboard)/[publicId]/settings/profile/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/settings/profile/page.tsx
@@ -2,8 +2,8 @@ import { api } from "@/trpc/server";
 import { ProfileSettings } from "@/components/member/member-profile";
 
 const ProfileSettingsPage = async () => {
-   const memberProfile = await api.member.getProfile.query();
-  
+  const memberProfile = await api.member.getProfile.query();
+
   return <ProfileSettings memberProfile={memberProfile} />;
 };
 

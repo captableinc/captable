9d690ed932b72a0c331bdc2621bc4b2481ecfddc
Raju-github-profile <rajukadel.github@gmail.com>
2024-03-16 20:12:33 +0545

    update

--- src/app/(authenticated)/(dashboard)/[publicId]/securities/options/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/securities/options/page.tsx
@@ -1,10 +1,44 @@
 import OptionModal from "@/components/securities/options/option-modal";
 import OptionTable from "@/components/securities/options/option-table";
+import EmptyState from "@/components/shared/empty-state";
+import Tldr from "@/components/shared/tldr";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { api } from "@/trpc/server";
+import { RiAddFill, RiGroup2Fill } from "@remixicon/react";
 
 const OptionsPage = async () => {
   const options = await api.securities.getOptions.query();
+
+  if (options?.data?.length === 0) {
+    return (
+      <EmptyState
+        icon={<RiGroup2Fill />}
+        title="You do not have any options for stakeholders!"
+        subtitle="Please click the button for adding new options."
+      >
+        <OptionModal
+          title="Collect options for Stakeholders"
+          subtitle={
+            <Tldr
+              message="Manage stock options by adding them. 
+            Add approval dates, notes, grantId for the stakeholders. "
+              cta={{
+                label: "Learn more",
+                href: "https://opencap.co/help/stakeholder-options",
+              }}
+            />
+          }
+          trigger={
+            <Button size="lg">
+              <RiAddFill className="mr-2 h-5 w-5" />
+              Add Option
+            </Button>
+          }
+        />
+      </EmptyState>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-y-3">
@@ -16,7 +50,20 @@ const OptionsPage = async () => {
           </p>
         </div>
         <div>
-          <OptionModal />
+          <OptionModal
+            title="Collect options for Stakeholders"
+            subtitle={
+              <Tldr
+                message="Manage stock options by adding them. 
+               Add approval dates, notes, grantId for the stakeholders. "
+                cta={{
+                  label: "Learn more",
+                  href: "https://opencap.co/help/stakeholder-options",
+                }}
+              />
+            }
+            trigger={<Button>Add Options</Button>}
+          />
         </div>
       </div>
       <Card className="mx-auto mt-3 w-[28rem] sm:w-[38rem] md:w-full">

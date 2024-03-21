4a7c2327960e839e05d2691e1de37300569eb8a2
Aashish Upadhyay <boysmall300@gmail.com>
2024-03-21 20:03:02 +0545

    addShareProcedure router

--- src/trpc/routers/securities-router/router.ts
+++ src/trpc/routers/securities-router/router.ts
@@ -2,9 +2,12 @@ import { createTRPCRouter } from "@/trpc/api/trpc";
 import { addOptionProcedure } from "./procedures/add-option";
 import { getOptionsProcedure } from "./procedures/get-options";
 import { deleteOptionProcedure } from "./procedures/delete-option";
+import { addShareProcedure } from "./procedures/add-share";
 
 export const securitiesRouter = createTRPCRouter({
   getOptions: getOptionsProcedure,
   addOptions: addOptionProcedure,
   deleteOption: deleteOptionProcedure,
+
+  addShares: addShareProcedure,
 });

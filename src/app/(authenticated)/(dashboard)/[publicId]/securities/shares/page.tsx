f77f497f1f1e2a3fde68d3f1f52d28647475bf2c
Aashish Upadhyay <boysmall300@gmail.com>
2024-03-22 00:03:38 +0545

    empty share modal

--- src/app/(authenticated)/(dashboard)/[publicId]/securities/shares/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/securities/shares/page.tsx
@@ -1,5 +1,36 @@
+import ShareModal from "@/components/securities/shares/share-modal";
+import EmptyState from "@/components/shared/empty-state";
+import Tldr from "@/components/shared/tldr";
+import { Button } from "@/components/ui/button";
+import { RiAddFill, RiGroup2Fill } from "@remixicon/react";
+
 const SharesPage = () => {
-  return <div>Shares page</div>;
+  return (
+    <EmptyState
+      icon={<RiGroup2Fill />}
+      title="You do not have any options yet."
+      subtitle="Please click the button for adding new options."
+    >
+      <ShareModal
+        title="Create an option"
+        subtitle={
+          <Tldr
+            message="Please fill in the details to create an option. If you need help, click the link below."
+            cta={{
+              label: "Learn more",
+              href: "https://opencap.co/help/stakeholder-options",
+            }}
+          />
+        }
+        trigger={
+          <Button size="lg">
+            <RiAddFill className="mr-2 h-5 w-5" />
+            Add a share
+          </Button>
+        }
+      />
+    </EmptyState>
+  );
 };
 
 export default SharesPage;

5900dc564445385852f225de8f4cbf329f7e516b
Raju-github-profile <rajukadel.github@gmail.com>
2024-03-25 16:38:50 +0545

    use-safe-steps

---
+++ src/components/safe/new/useSafeSteps.tsx
@@ -0,0 +1,40 @@
+import { type stepsType } from "@/components/shared/multistep-modal";
+import {
+  GeneralDetails,
+  GeneralDetailsFields,
+  InvestorDetails,
+  InvestorDetailsFields,
+  Documents,
+  DocumentsFields,
+} from "./steps";
+
+type SafeStepsType = {
+  companyId: string;
+};
+
+const useSafeSteps = ({ companyId: string }: SafeStepsType) => {
+  const steps: Array<stepsType> = [
+    {
+      id: 1,
+      title: "General details",
+      component: GeneralDetails,
+      fields: GeneralDetailsFields,
+    },
+    {
+      id: 2,
+      title: "Investment details",
+      component: InvestorDetails,
+      fields: InvestorDetailsFields,
+    },
+    {
+      id: 3,
+      title: "Sign & send documents",
+      component: Documents,
+      fields: DocumentsFields,
+    },
+  ];
+
+  return steps;
+};
+
+export default useSafeSteps;

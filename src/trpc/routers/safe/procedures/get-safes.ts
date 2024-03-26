828d12bc22ffc8d967b6b14d23551287fcd22799
Raju-github-profile <rajukadel.github@gmail.com>
2024-03-26 06:03:46 +0545

    feat: get-safes

---
+++ src/trpc/routers/safe/procedures/get-safes.ts
@@ -0,0 +1,55 @@
+import { withAuth } from "@/trpc/api/trpc";
+
+export const getSafesProcedure = withAuth.query(async ({ ctx }) => {
+  const user = ctx.session.user;
+
+  const data = await ctx.db.safe.findMany({
+    where: {
+      companyId: user.companyId,
+    },
+    select: {
+      id: true,
+      publicId: true,
+      type: true,
+      status: true,
+      capital: true,
+      safeTemplate: true,
+      valuationCap: true,
+      discountRate: true,
+      mfn: true,
+      proRata: true,
+      additionalTerms: true,
+      issueDate: true,
+      boardApprovalDate: true,
+      stakeholder: {
+        select: {
+          name: true,
+        },
+      },
+      documents: {
+        select: {
+          name: true,
+          uploader: {
+            select: {
+              user: {
+                select: {
+                  name: true,
+                  image: true,
+                },
+              },
+            },
+          },
+          bucket: {
+            select: {
+              key: true,
+              mimeType: true,
+              size: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return { data };
+});

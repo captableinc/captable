9a6721bfd4f50d5c3456f6e44a385c2eb1e65f6a
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-02 14:48:03 +0545

    chore: safe-page-minor-updates

--- src/app/(authenticated)/(dashboard)/[publicId]/securities/safes/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/securities/safes/page.tsx
@@ -1,30 +1,45 @@
 import { withServerSession } from "@/server/auth";
-import { PageLayout } from "@/components/dashboard/page-layout";
 import SafeActions from "@/components/safe/actions";
 import EmptyState from "@/components/shared/empty-state";
 import { RiSafeFill } from "@remixicon/react";
+import { Card } from "@/components/ui/card";
+import { api } from "@/trpc/server";
+import SafeTable from "@/components/safe/existing/safe-table";
 
 const SafePage = async () => {
+  const safes = await api.safe.getSafes.query();
   const session = await withServerSession();
   const user = session.user;
 
+  if (safes?.data?.length === 0) {
+    return (
+      <EmptyState
+        icon={<RiSafeFill />}
+        title="Create and manage SAFE agreements."
+        subtitle="Please click the button for creating agreements."
+      >
+        <SafeActions companyPublicId={user.companyPublicId} />
+      </EmptyState>
+    );
+  }
+
   return (
-    // <PageLayout
-    //   title="SAFE"
-    //   description="Create and manage SAFE agreements for your company."
-    //   action={
-    //     <div className="items-end justify-end text-right">
-    //       <SafeActions companyPublicId={user.companyPublicId} />
-    //     </div>
-    //   }
-    // />
-    <EmptyState
-      icon={<RiSafeFill />}
-      title="SAFE"
-      subtitle="Create and manage SAFE agreements for your company."
-    >
-      <SafeActions companyPublicId={user.companyPublicId} />
-    </EmptyState>
+    <div className="flex flex-col gap-y-3">
+      <div className="flex items-center justify-between gap-y-3 ">
+        <div className="gap-y-3">
+          <h3 className="font-medium">SAFEs</h3>
+          <p className="mt-1 text-sm text-muted-foreground">
+            Create and manage SAFE agreements for your company.
+          </p>
+        </div>
+        <div>
+          <SafeActions companyPublicId={user.companyPublicId} />
+        </div>
+      </div>
+      <Card className="mx-auto mt-3 w-[28rem] sm:w-[38rem] md:w-full">
+        <SafeTable safes={safes.data} />
+      </Card>
+    </div>
   );
 };
 

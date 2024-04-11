37f7af913f29d0270c91f847e43a3c264417e773
Puru Dahal <dahal@users.noreply.github.com>
2024-04-11 03:13:19 -0500

    feat: reorder sidebar nav and files as accordingly (#249)
    * WIP
    
    * feat: reorder sidebar nav and files as accordingly

---
+++ src/app/(authenticated)/(dashboard)/[publicId]/securities/transactions/page.tsx
@@ -0,0 +1,22 @@
+import EmptyState from "@/components/shared/empty-state";
+import { Button } from "@/components/ui/button";
+import { RiPieChartFill } from "@remixicon/react";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Transactions",
+};
+
+const TransactionsPage = () => {
+  return (
+    <EmptyState
+      icon={<RiPieChartFill />}
+      title="Work in progress."
+      subtitle="This page is not yet available."
+    >
+      <Button size="lg">Coming soon...</Button>
+    </EmptyState>
+  );
+};
+
+export default TransactionsPage;

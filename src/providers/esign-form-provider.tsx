a6de737297193736ac04b76f300ce52c9883811a
nafees nazik <nafeesnazik21@gmail.com>
2024-04-12 07:44:51 +0530

    feat: add esign form provider

---
+++ src/providers/esign-form-provider.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useReducer,
+  type Dispatch,
+  type ReactNode,
+} from "react";
+import { type FileWithPath } from "react-dropzone";
+
+interface EsignFormProviderProps {
+  children: ReactNode;
+}
+
+const EsignFormProviderContext = createContext<{
+  value: Value;
+  setValue: Dispatch<Partial<Value>>;
+} | null>(null);
+
+type Value = {
+  recipients: {
+    email: string;
+    name?: string;
+  }[];
+  orderedDelivery: boolean;
+  document: FileWithPath[];
+};
+
+export function EsignFormProvider({ children }: EsignFormProviderProps) {
+  const [value, setValue] = useReducer(
+    (data: Value, partialData: Partial<Value>) => ({ ...data, ...partialData }),
+    {} as Value,
+  );
+
+  return (
+    <EsignFormProviderContext.Provider value={{ value, setValue }}>
+      {children}
+    </EsignFormProviderContext.Provider>
+  );
+}
+
+export const useEsignValues = () => {
+  const data = useContext(EsignFormProviderContext);
+
+  if (!data) {
+    throw new Error("useEsignValues shouldn't be null");
+  }
+
+  return data;
+};

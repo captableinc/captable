c2d26469339cf4d258b662fb8f63cff224116672
nafees nazik <nafeesnazik21@gmail.com>
2024-04-12 07:37:56 +0530

    feat: add hooks

---
+++ src/hooks/use-force-update.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import { useCallback, useState } from "react";
+
+/**
+ * Forces a re-render, similar to `forceUpdate` in class components.
+ */
+export function useForceUpdate() {
+  const [, dispatch] = useState(Object.create(null));
+  return useCallback(() => {
+    dispatch(Object.create(null));
+  }, []);
+}

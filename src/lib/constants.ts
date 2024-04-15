3ecaff6d1c084d5db4e46bce4437c5c3c19e758e
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-15 21:55:40 +0545

    typescript types / constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -23,3 +23,15 @@ export enum PayloadType {
   PROFILE_DATA = "profile-data",
   PROFILE_AVATAR = "profile-avatar",
 }
+export enum UpdateType {
+  SAVE_AND_SEND = "save-and-share",
+  SEND_ONLY = "share-only",
+}
+
+export const publicToggleWarning = `Are you sure you want to make it public? 
+Everybody with credentials can view it and
+ it may also be indexed by search engines.`;
+
+export const privateToggleWarning = `Are you sure you want to make it private?
+  Nobody can view it and link is not
+  shareable to anyone.`;

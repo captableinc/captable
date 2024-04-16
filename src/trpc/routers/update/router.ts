5f4ec472c5036da7be2476c64f158d838705628b
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-16 10:05:23 +0545

    router for trpc procedures

--- src/trpc/routers/update/router.ts
+++ src/trpc/routers/update/router.ts
@@ -1,60 +1,16 @@
-// import { Audit } from "@/server/audit";
-import { generatePublicId } from "@/common/id";
-import { UpdateMutationSchema } from "./schema";
-import { createTRPCRouter, withAuth } from "@/trpc/api/trpc";
+import { createTRPCRouter } from "@/trpc/api/trpc";
+import { deleteUpdateProcedure } from "./procedures/delete-update";
+import { getRecipientsProcedure } from "./procedures/get-recipients";
+import { getUpdatesProcedure } from "./procedures/get-updates";
+import { saveUpdateProcedure } from "./procedures/save-update";
+import { shareUpdateProcedure } from "./procedures/share-update";
+import { toggleStatusProcedure } from "./procedures/toggle-status";
 
 export const updateRouter = createTRPCRouter({
-  save: withAuth
-    .input(UpdateMutationSchema)
-    .mutation(async ({ ctx, input }) => {
-      try {
-        const authorId = ctx.session.user.memberId;
-        const companyId = ctx.session.user.companyId;
-        const publicId = input.publicId ?? generatePublicId();
-        const { title, content, html } = input;
-
-        if (title.length === 0 || content.length === 0) {
-          return {
-            success: false,
-            message: "Title and content cannot be empty.",
-          };
-        } else {
-          await ctx.db.$transaction(async (tx) => {
-            if (input.publicId) {
-              await tx.update.update({
-                where: { publicId },
-                data: {
-                  html,
-                  title,
-                  content,
-                },
-              });
-            } else {
-              await tx.update.create({
-                data: {
-                  html,
-                  title,
-                  content,
-                  publicId,
-                  companyId,
-                  authorId,
-                },
-              });
-            }
-          });
-
-          return {
-            publicId,
-            success: true,
-            message: "Successfully saved an update.",
-          };
-        }
-      } catch (error) {
-        console.error("Error saving an update:", error);
-        return {
-          success: false,
-          message: "Oops, something went wrong. Please try again later.",
-        };
-      }
-    }),
+  get: getUpdatesProcedure,
+  save: saveUpdateProcedure,
+  share: shareUpdateProcedure,
+  getRecipients: getRecipientsProcedure,
+  delete: deleteUpdateProcedure,
+  toggleStatus: toggleStatusProcedure,
 });

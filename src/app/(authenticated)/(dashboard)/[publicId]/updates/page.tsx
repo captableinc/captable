15ac5cea62860ec2a657bb39dc2b3bf8c3a41927
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-16 13:18:08 +0545

    feat: enriching the updates page with react-table

--- src/app/(authenticated)/(dashboard)/[publicId]/updates/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/updates/page.tsx
@@ -1,31 +1,60 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import EmptyState from "@/components/shared/empty-state";
-import { RiMailSendLine, RiAddFill } from "@remixicon/react";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import UpdateTable from "@/components/update/update-table";
+import { api } from "@/trpc/server";
+import { RiAddFill, RiMailSendLine } from "@remixicon/react";
 import { type Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Updates",
 };
 
-const UpdatesPage = ({
+const UpdatesPage = async ({
   params: { publicId },
 }: {
   params: { publicId: string };
 }) => {
+  const updates = await api.update.get.query({ publicId });
+  if (updates.data?.length === 0) {
+    return (
+      <EmptyState
+        icon={<RiMailSendLine />}
+        title="You have not sent any updates."
+        subtitle="Please click the button below to send an update to your stakeholders."
+      >
+        <Link href={`/${publicId}/updates/new`} passHref>
+          <Button size="lg" asChild>
+            <RiAddFill className="mr-2 h-5 w-5" />
+            Draft an update
+          </Button>
+        </Link>
+      </EmptyState>
+    );
+  }
   return (
-    <EmptyState
-      icon={<RiMailSendLine />}
-      title="You have not sent any updates."
-      subtitle="Please click the button below to send an update to your stakeholders."
-    >
-      <Link href={`/${publicId}/updates/new`} passHref>
-        <Button size="lg" asChild>
-          <RiAddFill className="mr-2 h-5 w-5" />
-          Draft an update
-        </Button>
-      </Link>
-    </EmptyState>
+    <div className="flex flex-col gap-y-3">
+      <div className="flex items-center justify-between gap-y-3 ">
+        <div className="gap-y-3">
+          <h3 className="font-medium">Investor Updates</h3>
+          <p className="mt-1 text-sm text-muted-foreground">
+            Send new updates to the investor
+          </p>
+        </div>
+        <div>
+          <Link href={`/${publicId}/updates/new`} passHref>
+            <Button size="lg" asChild>
+              <RiAddFill className="mr-2 h-5 w-5" />
+              Draft an update
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Card className="mx-auto mt-3 w-[28rem] sm:w-[38rem] md:w-full">
+        <UpdateTable updates={updates.data} />
+      </Card>
+    </div>
   );
 };
 

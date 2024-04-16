17c74031526d10c3c4c23dcef788925ebda8d862
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-16 14:56:15 +0545

    feat: alert component for warning author during update status toggle

---
+++ src/components/update/toggle-status-alert.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+type ToggleStatusAlertDialogProps = {
+  status: string | undefined;
+  onContinue: () => void;
+  privateToggleWarning: string;
+  publicToggleWarning: string;
+  trigger: React.ReactNode;
+};
+
+export function ToggleStatusAlertDialog({
+  trigger,
+  status,
+  onContinue,
+  privateToggleWarning,
+  publicToggleWarning,
+}: ToggleStatusAlertDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            {status?.toUpperCase() === "PRIVATE"
+              ? publicToggleWarning
+              : privateToggleWarning}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>
+            <Button disabled={!!status} onClick={onContinue}>
+              Continue
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}

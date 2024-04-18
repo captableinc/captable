54755959fff3878d2bcd59d947f41882625d74e0
nafees nazik <nafeesnazik21@gmail.com>
2024-04-18 18:10:12 +0530

    feat: add zod schema

--- src/providers/template-field-provider.tsx
+++ src/providers/template-field-provider.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { Form } from "@/components/ui/form";
-import { type FieldTypes } from "@/prisma/enums";
+import { ALL_RECIPIENT_VALUE } from "@/constants/esign";
+import { FieldTypes } from "@/prisma/enums";
 import { type TypeZodAddFieldMutationSchema } from "@/trpc/routers/template-field-router/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { type ReactNode } from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 type Field = TypeZodAddFieldMutationSchema["data"][number];
 
@@ -13,10 +16,32 @@ interface TemplateFieldProviderProps {
   fields?: Field[];
 }
 
-export type TemplateFieldForm = {
-  fields: Field[];
-  fieldType: FieldTypes | undefined;
-};
+const formSchema = z.object({
+  fieldType: z.nativeEnum(FieldTypes).optional(),
+  fields: z
+    .array(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        width: z.number(),
+        height: z.number(),
+        top: z.number(),
+        left: z.number(),
+        required: z.boolean(),
+        type: z.nativeEnum(FieldTypes),
+        viewportHeight: z.number(),
+        viewportWidth: z.number(),
+        page: z.number(),
+        defaultValue: z.string(),
+        readOnly: z.boolean(),
+        group: z.string().min(1),
+      }),
+    )
+    .nonempty(),
+  recipient: z.string(),
+});
+
+export type TemplateFieldForm = z.infer<typeof formSchema>;
 
 export const TemplateFieldProvider = ({
   children,
@@ -26,7 +51,9 @@ export const TemplateFieldProvider = ({
     defaultValues: {
       fields: fields ?? [],
       fieldType: undefined,
+      recipient: ALL_RECIPIENT_VALUE,
     },
+    resolver: zodResolver(formSchema),
   });
 
   return <Form {...form}>{children}</Form>;

8a0a5f78ec337d132870c58ce03a487891161252
nafees nazik <nafeesnazik21@gmail.com>
2024-04-27 07:39:46 +0530

    feat: render esign table

--- src/app/(authenticated)/(dashboard)/[publicId]/documents/esign/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/documents/esign/page.tsx
@@ -1,7 +1,10 @@
 import { PageLayout } from "@/components/dashboard/page-layout";
 import { AddEsignModal } from "@/components/esign/add-esign-modal";
+import { Card } from "@/components/ui/card";
 import { withServerSession } from "@/server/auth";
+import { api } from "@/trpc/server";
 import { type Metadata } from "next";
+import { ESignTable } from "./components/table";
 
 export const metadata: Metadata = {
   title: "Documents",
@@ -9,13 +12,23 @@ export const metadata: Metadata = {
 
 const EsignDocumentPage = async () => {
   const session = await withServerSession();
+  const { documents } = await api.template.all.query();
 
   return (
     <PageLayout
       title="eSign documents"
       description="Upload, sign and send documents for electronic signatures."
       action={<AddEsignModal companyPublicId={session.user.companyPublicId} />}
-    />
+    >
+      <Card className="mt-3">
+        <div className="p-6">
+          <ESignTable
+            companyPublicId={session.user.companyPublicId}
+            documents={documents}
+          />
+        </div>
+      </Card>
+    </PageLayout>
   );
 };
 

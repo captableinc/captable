057e8464f24aa7f784b000f9cbda3d1a23fc5a74
nafees nazik <nafeesnazik21@gmail.com>
2024-05-01 23:58:44 +0530

    feat: add all esign procedure

---
+++ src/trpc/routers/audit-router/procedures/all-esign-audits.ts
@@ -0,0 +1,34 @@
+import { checkMembership } from "@/server/auth";
+import { withAuth } from "@/trpc/api/trpc";
+import { ZodAllEsignAuditsQuerySchema } from "../schema";
+
+export const allEsignAuditsProcedure = withAuth
+  .input(ZodAllEsignAuditsQuerySchema)
+  .query(async ({ ctx, input }) => {
+    const { db, session } = ctx;
+    const { templatePublicId } = input;
+
+    const { audits } = await db.$transaction(async (tx) => {
+      const { companyId } = await checkMembership({ session, tx });
+
+      const { id: templateId } = await tx.template.findFirstOrThrow({
+        where: {
+          publicId: templatePublicId,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      const audits = await tx.esignAudit.findMany({
+        where: {
+          companyId,
+          templateId,
+        },
+      });
+
+      return { audits };
+    });
+
+    return { audits };
+  });

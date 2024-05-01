7f2478c2baf009c6fade6eeebd4191ced033a31a
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-01 15:01:39 +0545

    feat: zod schema for sending-esign-emails

--- src/trpc/routers/template-router/schema.ts
+++ src/trpc/routers/template-router/schema.ts
@@ -26,3 +26,26 @@ export const ZodSignTemplateMutationSchema = z.object({
 export const ZodGetSigningFieldsSchema = z.object({
   token: z.string(),
 });
+
+export const ZodSendEsignEmailSchema = z.object({
+  optionalMessage: z.string().or(z.null()),
+  completedOn: z.date().or(z.null()),
+  documentName: z.string(),
+  templateId: z.string(),
+  recipients: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string().or(z.null()),
+      email: z.string().email().min(6),
+    }),
+  ),
+  company: z.object({
+    name: z.string(),
+    logo: z.string(),
+  }),
+  expiryDate: z.string().optional(),
+});
+
+export type TypeZodSendEsignEmailSchema = z.infer<
+  typeof ZodSendEsignEmailSchema
+>;

07e0e089d9a39e6d88d9d164fbbe565c995acf33
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-02 02:21:59 +0545

    feat: zod schemas updated in template-field for extending email logic

--- src/trpc/routers/template-field-router/schema.ts
+++ src/trpc/routers/template-field-router/schema.ts
@@ -4,6 +4,7 @@ import { FieldTypes, TemplateStatus } from "@/prisma/enums";
 
 export const ZodAddFieldMutationSchema = z.object({
   status: z.nativeEnum(TemplateStatus),
+  completedOn: z.date().optional(),
   templatePublicId: z.string(),
   data: z.array(
     z.object({
@@ -23,6 +24,15 @@ export const ZodAddFieldMutationSchema = z.object({
       recipientId: z.string(),
     }),
   ),
+  emailPayload: z.object({
+    optionalMessage: z.string().optional(),
+    expiryDate: z.date().or(z.null()).or(z.string()).optional(),
+    documentName: z.string(),
+    company: z.object({
+      name: z.string(),
+      logo: z.string().optional(),
+    }),
+  }),
 });
 
 export type TypeZodAddFieldMutationSchema = z.infer<

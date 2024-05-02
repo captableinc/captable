98157260321a0b9ba155efe8a34539acd18801ad
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-02 12:48:20 +0545

    fix: some improvements in optional message modal

--- src/components/esign/optional-message-modal.tsx
+++ src/components/esign/optional-message-modal.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import Modal from "@/components/common/modal";
-import { Button } from "@/components/ui/button";
 import {
-  Form,
   FormControl,
   FormField,
   FormItem,
@@ -11,13 +9,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
-import { useToast } from "@/components/ui/use-toast";
 import { getSanitizedDateTime } from "@/lib/utils";
-import { api } from "@/trpc/react";
 import { type RouterOutputs } from "@/trpc/shared";
 import { useSession } from "next-auth/react";
 import React, { type SetStateAction } from "react";
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
+import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 
@@ -26,7 +23,6 @@ type Recipients = RouterOutputs["template"]["get"]["recipients"];
 interface Payload {
   completedOn: Date | null;
   name: string;
-  templateId: string;
   recipients: Recipients;
   company: {
     name: string;
@@ -34,6 +30,7 @@ interface Payload {
   };
 }
 type MemberModalType = {
+  callback: (canSubmit: boolean) => void;
   payload: Payload;
   title: string;
   subtitle: string;
@@ -48,121 +45,87 @@ export const OptionalMessageModal = ({
   title,
   subtitle,
   dialogProps,
+  callback,
 }: MemberModalType) => {
-  const { toast } = useToast();
   const { data: session } = useSession();
-
-  const sendEsignMail = api.template.sendEsignMail.useMutation({
-    onSuccess: ({ success, message }) => {
-      if (!success) return;
-      toast({
-        variant: "default",
-        title: "🎉 Sent!",
-        description: message,
-      });
-      form.reset();
-    },
-    onError: (error) => {
-      toast({
-        variant: "destructive",
-        title: error.message,
-        description: "",
-      });
-    },
-    onSettled: () => {
-      dialogProps.onOpenChange((prev) => !prev);
-    },
-  });
-  const form = useForm();
-  const optionalMessage: string = form.watch("optionalMessage");
+  const form = useFormContext();
   const isSubmitting = form.formState.isSubmitting;
 
-  const onSubmit = async () => {
-    try {
-      const optionalMessage: string = form.getValues("optionalMessage");
-      const expiryDate: string = form.getValues("expiryDate");
-
-      if (!optionalMessage && !expiryDate) {
-        toast({
-          variant: "destructive",
-          title: "Required",
-          description: "Please add expiry date for default message.",
-        });
-        return;
-      }
-      if (session) {
-        const _payload = {
-          optionalMessage: optionalMessage ?? null,
-          completedOn: payload.completedOn ?? null,
-          documentName: payload.name,
-          templateId: payload.templateId,
-          recipients: payload.recipients,
-          company: {
-            name: payload.company.name,
-            logo:
-              payload.company.logo ??
-              "https://avatars.githubusercontent.com/u/163377635?s=48&v=4",
-          },
-          expiryDate: expiryDate ? getSanitizedDateTime(expiryDate) : undefined,
-        };
-        await sendEsignMail.mutateAsync(_payload);
-      }
-    } catch (error) {
-      console.log({ error });
+  const onSendEsignEmail = async () => {
+    const optionalMessage = form.getValues("emailPayload.optionalMessage") as
+      | string
+      | undefined;
+    const expiryDate = form.getValues("emailPayload.expiryDate") as
+      | string
+      | undefined;
+    if (session) {
+      const email_payload = {
+        optionalMessage: optionalMessage ?? "",
+        completedOn: payload.completedOn,
+        documentName: payload.name,
+        company: {
+          name: payload.company.name,
+          logo:
+            payload.company.logo ??
+            "https://avatars.githubusercontent.com/u/163377635?s=48&v=4",
+        },
+        expiryDate: expiryDate ? getSanitizedDateTime(expiryDate) : null,
+      };
+      form.setValue("emailPayload", email_payload);
+      callback(true);
     }
   };
 
   return (
     <Modal
-      trigger={<p></p>}
+      trigger={<></>}
       title={title}
       subtitle={subtitle}
       dialogProps={dialogProps}
     >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col">
-          <FormField
-            control={form.control}
-            name="optionalMessage"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Optional message</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Type email body here instead of default message..."
-                    disabled={isSubmitting}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage className="text-xs font-light" />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            disabled={!!optionalMessage}
-            control={form.control}
-            name="expiryDate"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Set expiry date</FormLabel>
-                <FormControl>
-                  <Input type="datetime-local" {...field} />
-                </FormControl>
-                <FormMessage className="text-xs font-light" />
-              </FormItem>
-            )}
-          />
-
-          <Button loading={isSubmitting} onSubmit={onSubmit} className="mt-5">
-            Send E-sign Email
-          </Button>
-
-          <p className="py-1 text-center text-xs text-gray-500">
-            We{`'`}ll send an email with a link to e-sign templates.
-          </p>
-        </form>
-      </Form>
+      <FormField
+        control={form.control}
+        name="emailPayload.optionalMessage"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Optional message</FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Type email body here instead of default message..."
+                disabled={isSubmitting}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="text-xs font-light" />
+          </FormItem>
+        )}
+      />
+      <FormField
+        disabled={isSubmitting}
+        control={form.control}
+        name="emailPayload.expiryDate"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Set expiry date</FormLabel>
+            <FormControl>
+              <Input type="datetime-local" {...field} />
+            </FormControl>
+            <FormMessage className="text-xs font-light" />
+          </FormItem>
+        )}
+      />
+      <div className="flex w-full items-center justify-center">
+        <Button
+          onClick={onSendEsignEmail}
+          loading={isSubmitting}
+          className="mx-auto mt-5"
+        >
+          Send E-sign Email
+        </Button>
+      </div>
+      <p className="py-1 text-center text-xs text-gray-500">
+        We{`'`}ll send an email with a link to e-sign templates.
+      </p>
     </Modal>
   );
 };

578add600a08f455aadebd4e3393ef0fbe63ea5e
nafees nazik <nafeesnazik21@gmail.com>
2024-05-04 08:34:28 +0530

    feat: use trigger for sending email

---
+++ src/jobs/auth-verification-email.ts
@@ -0,0 +1,51 @@
+import AccountVerificationEmail from "@/emails/AccountVerificationEmail";
+import { env } from "@/env";
+import { sendMail } from "@/server/mailer";
+import { client } from "@/trigger";
+import { eventTrigger } from "@trigger.dev/sdk";
+import { render } from "jsx-email";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email(),
+  token: z.string(),
+});
+
+export type TAuthVerificationPayloadSchema = z.infer<typeof schema>;
+
+export const sendAuthVerificationEmail = async (
+  payload: TAuthVerificationPayloadSchema,
+) => {
+  const { email, token } = payload;
+  const baseUrl = env.BASE_URL;
+
+  const confirmLink = `${baseUrl}/verify-email/${token}`;
+
+  const html = await render(
+    AccountVerificationEmail({
+      verifyLink: confirmLink,
+    }),
+  );
+
+  await sendMail({
+    to: email,
+    subject: "Confirm your email",
+    html,
+  });
+};
+
+export const triggerName = "email.auth-verify";
+
+client.defineJob({
+  id: "auth-verification-email",
+  name: "authentication verification email",
+  version: "0.0.1",
+  trigger: eventTrigger({
+    name: triggerName,
+    schema,
+  }),
+
+  run: async (payload) => {
+    await sendAuthVerificationEmail(payload);
+  },
+});
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,24 +1,6 @@
-import AccountVerificationEmail from "@/emails/AccountVerificationEmail";
 import PasswordResetEmail from "@/emails/PasswordResetEmail";
 import { sendMail } from "@/server/mailer";
 import { render } from "jsx-email";
-
-export const sendVerificationEmail = async (email: string, token: string) => {
-  const baseUrl = process.env.BASE_URL;
-  const confirmLink = `${baseUrl}/verify-email/${token}`;
-
-  const html = await render(
-    AccountVerificationEmail({
-      verifyLink: confirmLink,
-    }),
-  );
-
-  await sendMail({
-    to: email,
-    subject: "Confirm your email",
-    html,
-  });
-};
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const baseUrl = process.env.BASE_URL;

bf77398330fcf382bb3c7092ea095077f66246d5
nafees nazik <nafeesnazik21@gmail.com>
2024-05-04 07:50:56 +0530

    feat: add get client utility

--- src/trigger.ts
+++ src/trigger.ts
@@ -1,7 +1,25 @@
 import { TriggerClient } from "@trigger.dev/sdk";
+import { env } from "./env";
 
-export const client = new TriggerClient({
-  id: process.env.TRIGGER_API_ID!,
-  apiKey: process.env.TRIGGER_API_KEY,
-  apiUrl: process.env.TRIGGER_API_URL,
-});
+const apiKey = env.TRIGGER_API_KEY;
+const apiUrl = env.TRIGGER_API_URL;
+const id = env.TRIGGER_API_ID;
+
+const getClient = () =>
+  new TriggerClient({
+    id,
+    apiKey,
+    apiUrl,
+  });
+
+export const client = getClient();
+
+export function getTriggerClient(): TriggerClient | null {
+  if (!apiKey || !apiUrl || !id) {
+    return null;
+  }
+
+  const client = getClient();
+
+  return client;
+}

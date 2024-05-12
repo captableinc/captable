c9e0daf8836d890a6a5d4adac2a36cf32ae52599
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-13 00:23:52 +0545

    feat: modal for adding new passkey

---
+++ src/components/security/create-passkey-modal.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import Modal from "@/components/common/modal";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { api } from "@/trpc/react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { startRegistration } from "@simplewebauthn/browser";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { UAParser } from "ua-parser-js";
+import { z } from "zod";
+import { Alert, AlertDescription } from "../ui/alert";
+
+type PasskeyModalType = {
+  title: string;
+  subtitle: string;
+  trigger: React.ReactNode;
+};
+
+const ZCreatePasskeyFormSchema = z.object({
+  passkeyName: z.string().min(3),
+});
+
+type TCreatePasskeyFormSchema = z.infer<typeof ZCreatePasskeyFormSchema>;
+
+const PasskeyModal = ({ title, subtitle, trigger }: PasskeyModalType) => {
+  const parser = new UAParser();
+  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const form = useForm<TCreatePasskeyFormSchema>({
+    resolver: zodResolver(ZCreatePasskeyFormSchema),
+  });
+
+  const { mutateAsync: createPasskeyRegistrationOptions } =
+    api.passkey.createRegistrationOptions.useMutation();
+
+  const { mutateAsync: createPasskey } = api.passkey.create.useMutation({
+    onSuccess: async () => {
+      toast({
+        description: "🎉 Successfully created passkey",
+        duration: 5000,
+      });
+      router.refresh();
+      setOpen(false);
+    },
+  });
+
+  useEffect(() => {
+    const extractDefaultPasskeyName = () => {
+      if (!window || !window.navigator) {
+        return;
+      }
+      parser.setUA(window.navigator.userAgent);
+
+      const result = parser.getResult();
+      const operatingSystem = result.os.name;
+      const browser = result.browser.name;
+
+      let passkeyName = "";
+
+      if (operatingSystem && browser) {
+        passkeyName = `${browser} (${operatingSystem})`;
+      }
+
+      return passkeyName;
+    };
+
+    if (!open) {
+      const defaultPasskeyName = extractDefaultPasskeyName();
+
+      form.reset({
+        passkeyName: defaultPasskeyName,
+      });
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [open, form]);
+
+  const isSubmitting = form.formState.isSubmitting;
+
+  const onSubmit = async ({ passkeyName }: TCreatePasskeyFormSchema) => {
+    try {
+      const passkeyRegistrationOptions =
+        await createPasskeyRegistrationOptions();
+
+      const registrationResult = await startRegistration(
+        passkeyRegistrationOptions,
+      );
+
+      await createPasskey({
+        passkeyName,
+        verificationResponse: registrationResult,
+      });
+    } catch (err) {
+      console.log({ err });
+
+      toast({
+        title: "Error",
+        variant: "destructive",
+        //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        description: err?.message ?? "Something went out.",
+        duration: 5000,
+      });
+    }
+  };
+
+  return (
+    <Modal
+      title={title}
+      subtitle={subtitle}
+      trigger={trigger}
+      dialogProps={{
+        open,
+        onOpenChange: (val) => {
+          setOpen(val);
+          form.reset();
+        },
+      }}
+    >
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-y-3"
+        >
+          <FormField
+            control={form.control}
+            name="passkeyName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Passkey name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-background"
+                    placeholder="eg. Mac"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Alert className="bg-teal-50">
+            <AlertDescription>
+              By clicking continue, you&apos;ll be prompted to add the first
+              available authenticator on your system. This will enhance your
+              account&apos;s security.
+            </AlertDescription>
+
+            <AlertDescription className="mt-2">
+              If you prefer not to use the prompted authenticator, you can
+              simply close it. This will then display the next available option
+              for you to choose from.
+            </AlertDescription>
+          </Alert>
+
+          <Button loading={isSubmitting} className="mt-5">
+            Create Passkey
+          </Button>
+        </form>
+      </Form>
+    </Modal>
+  );
+};
+
+export default PasskeyModal;

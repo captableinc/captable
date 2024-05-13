a5bdbc0314b9dcb4e22112be61c387d06ef84119
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-13 11:27:28 +0545

    fix: icon warnings

--- src/components/common/icons.tsx
+++ src/components/common/icons.tsx
@@ -136,9 +136,9 @@ export const PasskeyIcon = (props: IconProps) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path d="M2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4a6.5 6.5 0 1 0-4-4Z" />
       <circle cx="16.5" cy="7.5" r=".5" fill="currentColor" />

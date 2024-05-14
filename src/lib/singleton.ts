57474169e440b26a42d99a4c5c7458713705792a
nafees nazik <nafeesnazik21@gmail.com>
2024-05-14 06:32:58 +0530

    feat: add singleton util

---
+++ src/lib/singleton.ts
@@ -0,0 +1,11 @@
+// credit https://github.com/epicweb-dev/remember/blob/main/index.js
+
+export function singleton<Value>(name: string, getValue: () => Value): Value {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const thusly = globalThis as any;
+  thusly.__remember_singleton ??= new Map<string, Value>();
+  if (!thusly.__remember_singleton.has(name)) {
+    thusly.__remember_singleton.set(name, getValue());
+  }
+  return thusly.__remember_singleton.get(name)!;
+}

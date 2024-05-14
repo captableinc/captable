5562321be413132bcaaeccdfd36ce024381f4bb5
nafees nazik <nafeesnazik21@gmail.com>
2024-05-14 07:35:46 +0530

    feat: register auth verification job

--- src/lib/pg-boss-init.ts
+++ src/lib/pg-boss-init.ts
@@ -1,7 +1,8 @@
+import { AuthVerificationEmailJob } from "@/jobs/auth-verification-email";
 import { JobManager, boss } from "./pg-boss-base";
 
 export async function initPgBoss() {
-  const jobs = new JobManager(boss);
+  const jobs = new JobManager(boss).register(AuthVerificationEmailJob);
 
   await jobs.start();
 }

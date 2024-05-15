3a8f06346d6b70f7e223d7080185da6370ef62fa
Puru D <puru@dahal.me>
2024-05-15 02:11:59 -0500

    fix: build errors

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const config = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    // ignoreDuringBuilds: true,
   },
 };
 

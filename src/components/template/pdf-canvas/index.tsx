318b1062ee36a264216f0d1e27438379ac1454c7
nafees nazik <nafeesnazik21@gmail.com>
2024-05-17 21:52:23 +0530

    fix: remove unused function

--- src/components/template/pdf-canvas/index.tsx
+++ src/components/template/pdf-canvas/index.tsx
@@ -5,16 +5,12 @@ import {
   PdfViewerRoot,
   usePdfValue,
 } from "@/components/ui/pdf-viewer";
-import { type PageMeasurement } from "@/lib/pdf-positioning";
-import { type RouterOutputs } from "@/trpc/shared";
+import type { RouterOutputs } from "@/trpc/shared";
 
-import { useCallback, useState } from "react";
-import { DocumentProps } from "react-pdf";
 import { FieldCanvas } from "../field-canvas";
 import { ReadOnlyFieldCanvas } from "../field-canvas/readonly-field-canvas";
 
 type Recipients = RouterOutputs["template"]["get"]["recipients"];
-type LoadCallback = Required<DocumentProps>["onLoadSuccess"];
 
 type TRecipient =
   | {
@@ -41,38 +37,17 @@ function PdfPages({ recipients, mode }: TRecipient) {
       {mode === "edit" && recipients ? (
         <FieldCanvas recipients={recipients} pageNumber={index + 1} />
       ) : (
-        <ReadOnlyFieldCanvas />
+        <ReadOnlyFieldCanvas pageNumber={index + 1} />
       )}
     </div>
   ));
 }
 
 export function PdfCanvas({ url, mode = "edit", recipients }: PdfCanvasProps) {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const [measurements, setPageHeights] = useState<PageMeasurement>([]);
-
-  const onDocumentLoadSuccess: LoadCallback = useCallback(async (e) => {
-    const measurements: PageMeasurement = [];
-
-    for (let index = 0; index < e.numPages; index++) {
-      const page = await e.getPage(index + 1);
-
-      const { height, width } = page.getViewport({ scale: 1 });
-
-      measurements.push({ height, width });
-    }
-
-    setPageHeights(measurements);
-
-    setIsLoaded(true);
-  }, []);
-
   return (
     <div className="col-span-12 select-none">
       <PdfViewerRoot
         className="w-full overflow-hidden rounded flex flex-col gap-y-6"
-        onDocumentLoadSuccess={onDocumentLoadSuccess}
         file={url}
       >
         {mode === "edit" && recipients ? (

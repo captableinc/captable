450c2b706fdee6b746896fbc5d4d3c16fe1aa7fe
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-18 14:10:13 +0545

    fix: conflict in option-modal

--- src/components/securities/options/option-modal.tsx
+++ src/components/securities/options/option-modal.tsx
@@ -1,4 +1,5 @@
 import {
+<<<<<<< HEAD
   StepperModal,
   StepperModalContent,
   type StepperModalProps,
@@ -47,5 +48,107 @@ export const OptionModal = (props: Omit<StepperModalProps, "children">) => {
         </StepperStep>
       </StockOptionFormProvider>
     </StepperModal>
+=======
+  type TypeZodAddOptionMutationSchema,
+  ZodAddOptionMutationSchema,
+} from "@/trpc/routers/securities-router/schema";
+import { useRouter } from "next/navigation";
+import type React from "react";
+import { useState } from "react";
+import {
+  Documents,
+  GeneralDetails,
+  GeneralDetailsField,
+  RelevantDates,
+  RelevantDatesFields,
+  VestingDetails,
+  VestingDetailsFields,
+} from "./steps";
+
+type OptionModalProps = {
+  title: string;
+  subtitle: string | React.ReactNode;
+  trigger: string | React.ReactNode;
+};
+
+const OptionModal = ({ title, subtitle, trigger }: OptionModalProps) => {
+  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const addOptionMutation = api.securities.addOptions.useMutation({
+    onSuccess: ({ message, success }) => {
+      toast({
+        variant: success ? "default" : "destructive",
+        title: message,
+        description: success
+          ? "A new stakeholder option has been created."
+          : "Failed adding an option. Please try again.",
+      });
+      setOpen(false);
+      if (success) {
+        router.refresh();
+      }
+    },
+  });
+
+  const steps = [
+    {
+      id: 1,
+      title: "General details",
+      component: GeneralDetails,
+      fields: GeneralDetailsField,
+    },
+    {
+      id: 2,
+      title: "Vesting details",
+      component: VestingDetails,
+      fields: VestingDetailsFields,
+    },
+    {
+      id: 3,
+      title: "Relevant dates",
+      component: RelevantDates,
+      fields: RelevantDatesFields,
+    },
+    {
+      id: 4,
+      title: "Documents",
+      component: Documents,
+      fields: ["documents"],
+    },
+  ];
+
+  const onSubmit = async (data: TypeZodAddOptionMutationSchema) => {
+    if (data?.documents.length === 0) {
+      toast({
+        variant: "destructive",
+        title: "Uh ohh! Documents not found",
+        description: "Please upload necessary documents",
+      });
+      return;
+    }
+    await addOptionMutation.mutateAsync(data);
+  };
+
+  return (
+    <div>
+      <MultiStepModal
+        //@ts-ignore
+        steps={steps}
+        title={title}
+        subtitle={subtitle}
+        trigger={trigger}
+        dialogProps={{
+          open,
+          onOpenChange: (val) => {
+            setOpen(val);
+          },
+        }}
+        schema={ZodAddOptionMutationSchema}
+        onSubmit={onSubmit}
+      />
+    </div>
+>>>>>>> 66bc51c (chore: humanize vesting schedule)
   );
 };

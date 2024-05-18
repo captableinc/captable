ca643a7d02175bdd476b2996609c9d3727e76626
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-18 14:48:58 +0545

    feat: register share procedures in router

--- src/trpc/routers/securities-router/router.ts
+++ src/trpc/routers/securities-router/router.ts
@@ -1,10 +1,16 @@
 import { createTRPCRouter } from "@/trpc/api/trpc";
 import { addOptionProcedure } from "./procedures/add-option";
+import { addShareProcedure } from "./procedures/add-share";
 import { deleteOptionProcedure } from "./procedures/delete-option";
+import { deleteShareProcedure } from "./procedures/delete-share";
 import { getOptionsProcedure } from "./procedures/get-options";
+import { getSharesProcedure } from "./procedures/get-shares";
 
 export const securitiesRouter = createTRPCRouter({
   getOptions: getOptionsProcedure,
   addOptions: addOptionProcedure,
   deleteOption: deleteOptionProcedure,
+  getShares: getSharesProcedure,
+  addShares: addShareProcedure,
+  deleteShare: deleteShareProcedure,
 });

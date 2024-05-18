b360a5dd894deac5550db0ee80662996dede2433
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-18 15:32:42 +0545

    feat: shares page

--- src/app/(authenticated)/(dashboard)/[publicId]/securities/shares/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/securities/shares/page.tsx
@@ -1,21 +1,79 @@
 import EmptyState from "@/components/common/empty-state";
+import Tldr from "@/components/common/tldr";
+import ShareModal from "@/components/securities/shares/share-modal";
+import ShareTable from "@/components/securities/shares/share-table";
 import { Button } from "@/components/ui/button";
-import { RiPieChartFill } from "@remixicon/react";
-import { type Metadata } from "next";
+import { Card } from "@/components/ui/card";
+import { api } from "@/trpc/server";
+import { RiAddFill, RiPieChartFill } from "@remixicon/react";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Cap table",
+  title: "Captable | Shares",
 };
 
-const SharesPage = () => {
+const SharesPage = async () => {
+  const shares = await api.securities.getShares.query();
+
+  if (shares?.data?.length === 0) {
+    return (
+      <EmptyState
+        icon={<RiPieChartFill />}
+        title="You do not have any shares yet."
+        subtitle="Please click the button for adding new shares."
+      >
+        <ShareModal
+          title="Create a share"
+          subtitle={
+            <Tldr
+              message="Please fill in the details to create a share. If you need help, click the link below."
+              cta={{
+                label: "Learn more",
+                href: "https://opencap.co/help/stakeholder-shares",
+              }}
+            />
+          }
+          trigger={
+            <Button size="lg">
+              <RiAddFill className="mr-2 h-5 w-5" />
+              Add a share
+            </Button>
+          }
+        />
+      </EmptyState>
+    );
+  }
+
   return (
-    <EmptyState
-      icon={<RiPieChartFill />}
-      title="Work in progress."
-      subtitle="This page is not yet available."
-    >
-      <Button size="lg">Coming soon...</Button>
-    </EmptyState>
+    <div className="flex flex-col gap-y-3">
+      <div className="flex items-center justify-between gap-y-3 ">
+        <div className="gap-y-3">
+          <h3 className="font-medium">Shares</h3>
+          <p className="mt-1 text-sm text-muted-foreground">
+            Add shares for stakeholders
+          </p>
+        </div>
+        <div>
+          <ShareModal
+            title="Draft Shares"
+            subtitle={
+              <Tldr
+                message="Manage shares by adding them. 
+               Add approval dates, notes, certificateId for the stakeholders. "
+                cta={{
+                  label: "Learn more",
+                  href: "https://opencap.co/help/stakeholder-shares",
+                }}
+              />
+            }
+            trigger={<Button>Draft Shares</Button>}
+          />
+        </div>
+      </div>
+      <Card className="mx-auto mt-3 w-[28rem] sm:w-[38rem] md:w-full">
+        <ShareTable shares={shares.data} />
+      </Card>
+    </div>
   );
 };
 

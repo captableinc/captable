8450a1691dba6942e1ffa9a7e3ffbeb55b443f19
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-18 15:10:11 +0545

    feat: documents step

---
+++ src/components/securities/shares/steps/documents.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import Uploader from "@/components/ui/uploader";
+import { useFormContext } from "react-hook-form";
+
+type Documents = {
+  bucketId: string;
+  name: string;
+};
+export const DocumentFields = ["documents"];
+export const Documents = () => {
+  const form = useFormContext();
+  //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const documents: [Documents] = form.watch("documents");
+
+  return (
+    <>
+      <Uploader
+        multiple={true}
+        identifier={"documenter"}
+        keyPrefix="equity-doc"
+        onSuccess={(bucketData) => {
+          form.setValue("documents", [
+            //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            ...(form.getValues("documents") || []),
+            {
+              bucketId: bucketData.id,
+              name: bucketData.name,
+            },
+          ]);
+        }}
+        accept={{
+          "application/pdf": [".pdf"],
+        }}
+      />
+      {documents?.length ? (
+        <Alert className="mt-5 bg-teal-100" variant="default">
+          <AlertTitle>
+            {documents.length > 1
+              ? `${documents.length} documents uploaded`
+              : `${documents.length} document uploaded`}
+          </AlertTitle>
+          <AlertDescription>
+            You can submit the form to proceed.
+          </AlertDescription>
+        </Alert>
+      ) : (
+        <Alert variant="destructive" className="mt-5">
+          <AlertTitle>0 document uploaded</AlertTitle>
+          <AlertDescription>
+            Please upload necessary documents to continue.
+          </AlertDescription>
+        </Alert>
+      )}
+    </>
+  );
+};

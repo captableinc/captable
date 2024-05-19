d2ef842e72a25d8936b6e11ffe4b770af33236d1
Puru D <puru@dahal.me>
2024-05-19 04:08:14 -0500

    feat: replace createTransport config with EMAIL_SERVER

--- src/server/mailer.ts
+++ src/server/mailer.ts
@@ -7,21 +7,8 @@ import { env } from "@/env";
 import { type SendMailOptions, createTransport } from "nodemailer";
 
 const getTransport = () => {
-  if (env.EMAIL_SERVER) {
-    return createTransport(env.EMAIL_SERVER);
-  }
-
   return createTransport({
-    host: env.EMAIL_SERVER_HOST,
-    port: env.EMAIL_SERVER_PORT,
-    secure: env.EMAIL_SERVER_SECURE,
-    ...(env.EMAIL_SERVER_PASSWORD &&
-      env.EMAIL_SERVER_USERNAME && {
-        auth: {
-          user: env.EMAIL_SERVER_USERNAME,
-          pass: env.EMAIL_SERVER_PASSWORD,
-        },
-      }),
+    url: env.EMAIL_SERVER,
   });
 };
 

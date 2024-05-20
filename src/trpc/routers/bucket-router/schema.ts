31b5d22cf49de445530691b99aa0e577cd1d36ef
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-20 16:13:41 +0545

    chore: schema update for bucket-router

--- src/trpc/routers/bucket-router/schema.ts
+++ src/trpc/routers/bucket-router/schema.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 
-export const ZodCreateBucketMutationSchema = z.object({
-  name: z.string(),
-  key: z.string(),
-  mimeType: z.string(),
-  size: z.number(),
-});
+export const ZodCreateBucketMutationSchema = z.array(
+  z.object({
+    name: z.string(),
+    key: z.string(),
+    mimeType: z.string(),
+    size: z.number(),
+    tags: z.array(z.string()),
+  }),
+);
 
 export type TypeZodCreateBucketMutationSchema = z.infer<
   typeof ZodCreateBucketMutationSchema

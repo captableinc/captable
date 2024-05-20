d603ec791abbe55b7a6ca06e016642c8d827e03c
nafees nazik <nafeesnazik21@gmail.com>
2024-05-20 15:24:57 +0530

    fix: drawing field color

--- src/components/template/field-canvas/drawing-field.tsx
+++ src/components/template/field-canvas/drawing-field.tsx
@@ -1,4 +1,7 @@
+import { COLORS } from "@/constants/esign";
 import { cn } from "@/lib/utils";
+import type { TemplateFieldForm } from "@/providers/template-field-provider";
+import { useFormContext } from "react-hook-form";
 interface DrawingFieldProps {
   left: number;
   top: number;
@@ -7,10 +10,18 @@ interface DrawingFieldProps {
 }
 
 export function DrawingField({ height, left, top, width }: DrawingFieldProps) {
+  const { getValues } = useFormContext<TemplateFieldForm>();
+
+  const recipient = getValues("recipient");
+  const recipientColors = getValues("recipientColors");
+  const color = (recipientColors?.[recipient] ?? "") as keyof typeof COLORS;
+
   return (
     <div
       className={cn(
-        "absolute overflow-visible border-2 bg-opacity-30 bg-red-400 border-red-400",
+        "absolute overflow-visible border-2 bg-opacity-30",
+        COLORS?.[color]?.border,
+        COLORS?.[color]?.bg,
       )}
       style={{
         left,

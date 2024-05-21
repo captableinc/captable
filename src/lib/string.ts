038c66b8bce42e3bcff3179aca4a818e3b9945bd
nafees nazik <nafeesnazik21@gmail.com>
2024-05-21 19:07:04 +0530

    feat: add title case util

---
+++ src/lib/string.ts
@@ -0,0 +1,9 @@
+export function toTitleCase(str: string) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}

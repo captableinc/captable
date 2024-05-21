db1559e23fcf461833313e502bc4dd38d8f89740
nafees nazik <nafeesnazik21@gmail.com>
2024-05-21 17:52:30 +0530

    chore: add space

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,4 @@
 import { env } from "next-runtime-env";
 
-export const NEXT_PUBLIC_BASE_URL = () => env("NEXT_PUBLIC_BASE_URL") as string;
+export const NEXT_PUBLIC_BASE_URL = () =>
+  env("NEXT_PUBLIC_BASE_URL ") as string;

a650dfb80b7bcdffd4bd935a67b117dc8d4deeeb
nafees nazik <nafeesnazik21@gmail.com>
2024-05-22 05:51:10 +0530

    feat: add submit button

--- src/components/securities/options/steps/general-details.tsx
+++ src/components/securities/options/steps/general-details.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -16,8 +17,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  StepperModalFooter,
+  StepperPrev,
+  useStepper,
+} from "@/components/ui/stepper";
 import { toTitleCase } from "@/lib/string";
 import { OptionStatusEnum, OptionTypeEnum } from "@/prisma/enums";
+import { useStockOptionFormValues } from "@/providers/stock-option-form-provider";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -30,101 +37,121 @@ const TYPES = Object.values(OptionTypeEnum);
 
 const formSchema = z.object({
   grantId: z.string(),
-  type: z.nativeEnum(OptionStatusEnum),
+  type: z.nativeEnum(OptionTypeEnum),
   quantity: z.coerce.number(),
-  status: z.nativeEnum(OptionTypeEnum),
+  status: z.nativeEnum(OptionStatusEnum),
 });
 
 type TFormSchema = z.infer<typeof formSchema>;
 
 export const GeneralDetails = () => {
+  const { next } = useStepper();
+  const { setValue } = useStockOptionFormValues();
+
   const form = useForm<TFormSchema>({
     resolver: zodResolver(formSchema),
   });
 
+  const handleSubmit = (data: TFormSchema) => {
+    setValue(data);
+    next();
+  };
   return (
-    <Form {...form}>
-      <div className="space-y-4">
-        <FormField
-          control={form.control}
-          name="grantId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Grant ID</FormLabel>
-              <FormControl>
-                <Input type="text" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+    <>
+      <Form {...form}>
+        <form
+          id="general-details-form"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="grantId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Grant ID</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-light" />
+                </FormItem>
+              )}
+            />
 
-        <FormField
-          control={form.control}
-          name="type"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Grant type</FormLabel>
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Grant type</FormLabel>
 
-              <Select onValueChange={field.onChange} value={field.value}>
-                <FormControl>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Select type" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {TYPES.map((type) => (
-                    <SelectItem key={type} value={type}>
-                      {type}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select type" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TYPES.map((type) => (
+                        <SelectItem key={type} value={type}>
+                          {type}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage className="text-xs font-light" />
+                </FormItem>
+              )}
+            />
 
-        <FormField
-          control={form.control}
-          name="quantity"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Quantity</FormLabel>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+            <FormField
+              control={form.control}
+              name="quantity"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Quantity</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs font-light" />
+                </FormItem>
+              )}
+            />
 
-        <FormField
-          control={form.control}
-          name="status"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Status</FormLabel>
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Status</FormLabel>
 
-              <Select onValueChange={field.onChange} value={field.value}>
-                <FormControl>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Select status" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {STATUSES.map((status) => (
-                    <SelectItem key={status.label} value={status.value}>
-                      {status.label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
-      </div>
-    </Form>
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select status" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {STATUSES.map((status) => (
+                        <SelectItem key={status.label} value={status.value}>
+                          {status.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage className="text-xs font-light" />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
+      <StepperModalFooter>
+        <StepperPrev>Back</StepperPrev>
+        <Button type="submit" form="general-details-form">
+          Save & Continue
+        </Button>
+      </StepperModalFooter>
+    </>
   );
 };

3c80a31aaec05f11bafcd6939e43bf09c38113b3
nafees nazik <nafeesnazik21@gmail.com>
2024-05-23 08:49:14 +0530

    feat: add relevant dates form

--- src/components/securities/options/steps/relevant-dates.tsx
+++ src/components/securities/options/steps/relevant-dates.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
+  Form,
   FormControl,
   FormField,
   FormItem,
@@ -8,93 +10,117 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import React from "react";
-import { useFormContext } from "react-hook-form";
+import {
+  StepperModalFooter,
+  StepperPrev,
+  useStepper,
+} from "@/components/ui/stepper";
+import { useStockOptionFormValues } from "@/providers/stock-option-form-provider";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export const RelevantDatesFields = [
-  "issueDate",
-  "expirationDate",
-  "vestingStartDate",
-  "boardApprovalDate",
-  "rule144Date",
-];
+const formSchema = z.object({
+  boardApprovalDate: z.string().date(),
+  rule144Date: z.string().date(),
+  issueDate: z.string().date(),
+  expirationDate: z.string().date(),
+  vestingStartDate: z.string().date(),
+});
+
+type TFormSchema = z.infer<typeof formSchema>;
 
 export const RelevantDates = () => {
-  const form = useFormContext();
+  const form = useForm<TFormSchema>({ resolver: zodResolver(formSchema) });
+  const { next } = useStepper();
+  const { setValue } = useStockOptionFormValues();
 
+  const handleSubmit = (data: TFormSchema) => {
+    setValue(data);
+    next();
+  };
   return (
-    <>
-      <div className="grid gap-4">
-        <FormField
-          control={form.control}
-          name="issueDate"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Issue date</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-y-4"
+        onSubmit={form.handleSubmit(handleSubmit)}
+      >
+        <div className="grid grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="issueDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Issue date</FormLabel>
+                <FormControl>
+                  <Input type="date" {...field} />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="expirationDate"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Expiry date</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="expirationDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Expiry date</FormLabel>
+                <FormControl>
+                  <Input type="date" {...field} />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="vestingStartDate"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Vesting start date</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="vestingStartDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Vesting start date</FormLabel>
+                <FormControl>
+                  <Input type="date" {...field} />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="boardApprovalDate"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Board approval date</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="boardApprovalDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Board approval date</FormLabel>
+                <FormControl>
+                  <Input type="date" {...field} />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="rule144Date"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Rule 144 date</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs font-light" />
-            </FormItem>
-          )}
-        />
-      </div>
-    </>
+          <FormField
+            control={form.control}
+            name="rule144Date"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Rule 144 date</FormLabel>
+                <FormControl>
+                  <Input type="date" {...field} />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
+        </div>
+        <StepperModalFooter>
+          <StepperPrev>Back</StepperPrev>
+          <Button type="submit">Save & Continue</Button>
+        </StepperModalFooter>
+      </form>
+    </Form>
   );
 };

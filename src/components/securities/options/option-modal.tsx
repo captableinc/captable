1c8d2c457155bfd81645193b6ec1e09d50b7d6f3
nafees nazik <nafeesnazik21@gmail.com>
2024-05-23 08:50:17 +0530

    feat: render steps

--- src/components/securities/options/option-modal.tsx
+++ src/components/securities/options/option-modal.tsx
@@ -1,12 +1,14 @@
 import {
   StepperModal,
   StepperModalContent,
-  StepperStep,
   type StepperModalProps,
+  StepperStep,
 } from "@/components/ui/stepper";
 import { StockOptionFormProvider } from "@/providers/stock-option-form-provider";
 import { api } from "@/trpc/server";
+import { Documents } from "./steps/documents";
 import { GeneralDetails } from "./steps/general-details";
+import { RelevantDates } from "./steps/relevant-dates";
 import { VestingDetails } from "./steps/vesting-details";
 
 async function VestingDetailsStep() {
@@ -31,6 +33,16 @@ export const OptionModal = (props: Omit<StepperModalProps, "children">) => {
         <StepperStep title="Vesting details">
           <StepperModalContent>
             <VestingDetailsStep />
+          </StepperModalContent>
+        </StepperStep>
+        <StepperStep title="Relevant dates">
+          <StepperModalContent>
+            <RelevantDates />
+          </StepperModalContent>
+        </StepperStep>
+        <StepperStep title="Documents">
+          <StepperModalContent>
+            <Documents />
           </StepperModalContent>
         </StepperStep>
       </StockOptionFormProvider>

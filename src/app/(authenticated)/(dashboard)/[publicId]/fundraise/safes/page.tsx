155324c65dacc62f4c109a9d54e833473e1930d4
nafees nazik <nafeesnazik21@gmail.com>
2024-05-24 08:46:14 +0530

    feat: create safe modal and render

--- src/app/(authenticated)/(dashboard)/[publicId]/fundraise/safes/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/fundraise/safes/page.tsx
@@ -1,51 +1,46 @@
 import EmptyState from "@/components/common/empty-state";
-import SafeActions from "@/components/safe/actions";
+import { PageLayout } from "@/components/dashboard/page-layout";
+import { CreateSafeModal } from "@/components/safe/create-safe-modal";
+import SafeTable from "@/components/safe/existing/safe-table";
+import { Card } from "@/components/ui/card";
 import { withServerSession } from "@/server/auth";
+import { api } from "@/trpc/server";
 import { RiSafeFill } from "@remixicon/react";
-import { type Metadata } from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "SAFEs",
 };
-
-import SafeTable from "@/components/safe/existing/safe-table";
-import { Card } from "@/components/ui/card";
-import { api } from "@/trpc/server";
 
 const SafePage = async () => {
   const safes = await api.safe.getSafes.query();
   const session = await withServerSession();
   const user = session.user;
 
-  if (safes?.data?.length === 0) {
+  if (!safes?.data?.length) {
     return (
       <EmptyState
         icon={<RiSafeFill />}
         title="Create and manage SAFE agreements."
         subtitle="Please click the button for creating agreements."
       >
-        <SafeActions companyPublicId={user.companyPublicId} />
+        <CreateSafeModal />
       </EmptyState>
     );
   }
 
   return (
-    <div className="flex flex-col gap-y-3">
-      <div className="flex items-center justify-between gap-y-3 ">
-        <div className="gap-y-3">
-          <h3 className="font-medium">SAFEs</h3>
-          <p className="mt-1 text-sm text-muted-foreground">
-            Create and manage SAFE agreements for your company.
-          </p>
+    <PageLayout
+      title="SAFEs"
+      description="Create and manage SAFE agreements for your company."
+      action={<CreateSafeModal />}
+    >
+      <Card className="mt-3">
+        <div className="p-6">
+          <SafeTable safes={safes.data} />
         </div>
-        <div>
-          <SafeActions companyPublicId={user.companyPublicId} />
-        </div>
-      </div>
-      <Card className="mx-auto mt-3 w-[28rem] sm:w-[38rem] md:w-full">
-        <SafeTable safes={safes.data} />
       </Card>
-    </div>
+    </PageLayout>
   );
 };
 

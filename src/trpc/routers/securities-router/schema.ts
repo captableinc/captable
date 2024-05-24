89dd1f8feef4e34c427959bb90ad986afa00691d
Raju-github-profile <rajukadel.github@gmail.com>
2024-05-24 16:21:49 +0545

    chore: schema refactoring of securities router

--- src/trpc/routers/securities-router/schema.ts
+++ src/trpc/routers/securities-router/schema.ts
@@ -46,58 +46,29 @@ export type TypeZodDeleteOptionMutationSchema = z.infer<
 
 // SHARES
 export const ZodAddShareMutationSchema = z.object({
-  id: z.string().optional(),
-  status: z.nativeEnum(SecuritiesStatusEnum, {
-    errorMap: () => ({ message: "Invalid value for status type" }),
-  }),
-  certificateId: z.string().min(1, {
-    message: "Certificate ID is required",
-  }),
-  quantity: z.coerce.number().min(0, {
-    message: "Quantity is required",
-  }),
-  pricePerShare: z.coerce.number().optional(),
-  capitalContribution: z.coerce.number().min(0, {
-    message: "Capital contribution is required",
-  }),
-  ipContribution: z.coerce.number().optional(),
-  debtCancelled: z.coerce.number().optional(),
-  otherContributions: z.coerce.number().optional(),
-  vestingSchedule: z.nativeEnum(VestingScheduleEnum, {
-    errorMap: () => ({ message: "Invalid value for vesting schedule" }),
-  }),
-  // companyLegends: z.array(z.string()),
-  companyLegends: z
-    .nativeEnum(ShareLegendsEnum, {
-      errorMap: () => ({ message: "Invalid value for compnay legends" }),
-    })
-    .array(),
-  issueDate: z.coerce.date({
-    required_error: "Issue date is required",
-    invalid_type_error: "This is not valid date",
-  }),
-  rule144Date: z.coerce.date({
-    invalid_type_error: "This is not a valid date",
-  }),
-  vestingStartDate: z.coerce.date({
-    invalid_type_error: "This is not a valid date",
-  }),
-  boardApprovalDate: z.coerce.date({
-    required_error: "Board approval date is required",
-    invalid_type_error: "This is not valid date",
-  }),
+  id: z.string().optional().nullable(),
+  stakeholderId: z.string(),
+  shareClassId: z.string(),
+  certificateId: z.string(),
+  quantity: z.coerce.number().min(0),
+  pricePerShare: z.coerce.number().min(0),
+  capitalContribution: z.coerce.number().min(0),
+  ipContribution: z.coerce.number().min(0),
+  debtCancelled: z.coerce.number().min(0),
+  otherContributions: z.coerce.number().min(0),
+  status: z.nativeEnum(SecuritiesStatusEnum),
+  vestingSchedule: z.nativeEnum(VestingScheduleEnum),
+  companyLegends: z.nativeEnum(ShareLegendsEnum).array(),
+  issueDate: z.string().date(),
+  rule144Date: z.string().date(),
+  vestingStartDate: z.string().date(),
+  boardApprovalDate: z.string().date(),
   documents: z.array(
     z.object({
       bucketId: z.string(),
       name: z.string(),
     }),
   ),
-  stakeholderId: z.string().min(1, {
-    message: "Stakeholder is required",
-  }),
-  shareClassId: z.string().min(1, {
-    message: "Share Class is required",
-  }),
 });
 
 export type TypeZodAddShareMutationSchema = z.infer<

2c1b4813c6d479dd1e9bd230c7c90668246aa54c
Raju kadel <rajukadel.github@gmail.com>
2024-05-26 14:13:12 +0545

    feat: security page

--- src/app/(authenticated)/(dashboard)/[publicId]/settings/security/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/settings/security/page.tsx
@@ -1,25 +1,58 @@
-import { UpdatePasswordForm } from "@/components/security/password/update-password-form";
-import { Card } from "@/components/ui/card";
+import { SecurityList } from "@/components/security/SecurityList";
+import { SettingsHeader } from "@/components/security/SettingHeader";
+import { getServerAuthSession } from "@/server/auth";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Security",
 };
-export default function SecurityPage() {
-  return (
-    <div className="flex flex-col gap-y-3">
-      <div>
-        <div className="gap-y-3">
-          <h3 className="font-medium">Security</h3>
-          <p className="text-sm text-muted-foreground">
-            Manage your password and security settings
-          </p>
-        </div>
-      </div>
 
-      <Card className="mx-auto mt-3 w-[28rem] sm:w-[38rem] md:w-full">
-        <UpdatePasswordForm />
-      </Card>
-    </div>
+const SecurityLists = [
+  {
+    title: "Change your password",
+    description:
+      "Let's update your current password with something more secure",
+    href: "security/password",
+    buttonDisplayName: "Update",
+  },
+  {
+    title: "Passkeys",
+    description: `Allows authenticating using biometrics, password managers,
+    hardware keys, etc.`,
+    href: "security/passkey",
+    buttonDisplayName: "Manage",
+  },
+  {
+    title: "Two factor authentication",
+    description:
+      "Add an authenticator to make password signing more secure and authentic.",
+    href: "security/2fa",
+    buttonDisplayName: "Add 2FA",
+  },
+];
+
+export default async function SecurityPage() {
+  const session = await getServerAuthSession();
+  if (!session?.user) {
+    redirect("/login");
+  }
+  return (
+    <>
+      <SettingsHeader
+        title="Security"
+        subtitle="Manage your password and security settings."
+        showBackArrow={false}
+      />
+      {SecurityLists.map((security) => (
+        <SecurityList
+          key={security.href}
+          title={security.title}
+          description={security.description}
+          href={security.href}
+          buttonDisplayName={security.buttonDisplayName}
+        />
+      ))}
+    </>
   );
 }

5c341220ce88bed2fe9fe12defebcfca6a6767d2
nafees nazik <nafeesnazik21@gmail.com>
2024-05-28 00:12:02 +0530

    chore: fix warning comment

--- next.config.js
+++ next.config.js
@@ -32,8 +32,6 @@ const config = {
     instrumentationHook: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };

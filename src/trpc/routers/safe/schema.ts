aba490451cc214a655e4bef76191bd24fffa1921
nafees nazik <nafeesnazik21@gmail.com>
2024-05-27 07:36:32 +0530

    fix: schema and procedure

--- src/trpc/routers/safe/schema.ts
+++ src/trpc/routers/safe/schema.ts
@@ -59,40 +59,14 @@ export type TypeZodDeleteSafesMutationSchema = z.infer<
 >;
 
 export const ZodAddExistingSafeMutationSchema = z.object({
-  id: z.string().optional(),
-  publicId: z.string().optional(),
-  mfn: z.boolean().optional(),
-  additionalTerms: z.string().optional(),
-  investorName: z.string().optional(),
-  investorEmail: z.string().email().optional(),
-  investorInstitutionName: z.string().optional(),
-
+  safeId: z.string().min(1),
+  valuationCap: z.coerce.number(),
+  discountRate: z.coerce.number().optional(),
+  proRata: z.boolean(),
+  capital: z.coerce.number(),
+  issueDate: z.string().date(),
+  boardApprovalDate: z.string(),
   stakeholderId: z.string(),
-  capital: z.number(),
-  valuationCap: z.number(),
-  discountRate: z.number().default(0),
-  proRata: z.boolean().optional(),
-
-  type: z
-    .nativeEnum(SafeTypeEnum, {
-      errorMap: () => ({ message: "Invalid SAFE type" }),
-    })
-    .optional(),
-
-  status: z
-    .nativeEnum(SafeStatusEnum, {
-      errorMap: () => ({ message: "Invalid SAFE status" }),
-    })
-    .optional(),
-
-  issueDate: z.coerce.date({
-    required_error: "Issue date is required",
-    invalid_type_error: "This is not a valid date",
-  }),
-  boardApprovalDate: z.coerce.date({
-    required_error: "Board Approval date is required",
-    invalid_type_error: "This is not a valid date",
-  }),
 
   documents: z.array(
     z.object({

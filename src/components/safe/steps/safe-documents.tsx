079727d87f0bd8d64da01339015b03e6894d6cea
nafees nazik <nafeesnazik21@gmail.com>
2024-05-27 07:37:03 +0530

    feat: add safe documents handler

--- src/components/safe/steps/safe-documents.tsx
+++ src/components/safe/steps/safe-documents.tsx
@@ -5,13 +5,62 @@ import { Button } from "@/components/ui/button";
 import { DialogClose } from "@/components/ui/dialog";
 import { StepperModalFooter, StepperPrev } from "@/components/ui/stepper";
 import Uploader from "@/components/ui/uploader";
+import { useFormValueState } from "@/providers/form-value-provider";
 import { useState } from "react";
 import type { FileWithPath } from "react-dropzone";
 
-export function SafeDocuments() {
-  const [documentsList, setDocumentsList] = useState<FileWithPath[]>([]);
+import { uploadFile } from "@/common/uploads";
+import { invariant } from "@/lib/error";
+import { api } from "@/trpc/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import type { TFormSchema as TGeneralDetailsFormSchema } from "./general-details";
+import type { TFormSchema as TInvestorDetailsFormSchema } from "./investor-details";
 
-  const handleComplete = () => {};
+type TFormValueState = TGeneralDetailsFormSchema & TInvestorDetailsFormSchema;
+
+export function SafeDocuments() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const values = useFormValueState<TFormValueState>();
+  const [documentsList, setDocumentsList] = useState<FileWithPath[]>([]);
+  const { mutateAsync: handleBucketUpload } = api.bucket.create.useMutation();
+  const { mutateAsync: handleCreateExistingSafe } =
+    api.safe.addExisting.useMutation({
+      onSuccess: ({ success }) => {
+        if (success) {
+          toast.success("SAFEs created successfully.");
+          router.refresh();
+        } else {
+          toast.error("Failed creating SAFEs. Please try again.");
+        }
+      },
+    });
+
+  const handleComplete = async () => {
+    invariant(session, "session not found");
+
+    const uploadedDocuments: { name: string; bucketId: string }[] = [];
+
+    for (const document of documentsList) {
+      const { key, mimeType, name, size } = await uploadFile(document, {
+        identifier: session.user.companyPublicId,
+        keyPrefix: "existing-safes",
+      });
+
+      const { id: bucketId, name: docName } = await handleBucketUpload({
+        key,
+        mimeType,
+        name,
+        size,
+      });
+
+      uploadedDocuments.push({ bucketId, name: docName });
+    }
+
+    await handleCreateExistingSafe({ ...values, documents: uploadedDocuments });
+  };
   return (
     <div className="flex flex-col gap-y-4">
       <div>

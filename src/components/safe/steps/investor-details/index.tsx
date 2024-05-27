2851d5e97dee50d3b33ed3ea4aecb4b7ed86f07a
nafees nazik <nafeesnazik21@gmail.com>
2024-05-27 23:45:15 +0530

    chore: export type

--- src/components/safe/steps/investor-details/index.tsx
+++ src/components/safe/steps/investor-details/index.tsx
@@ -2,6 +2,7 @@
 
 import { api } from "@/trpc/server";
 import { InvestorDetailsForm } from "./form";
+export { type TFormSchema } from "./form";
 
 export async function InvestorDetails() {
   const stakeholders = await api.stakeholder.getStakeholders.query();

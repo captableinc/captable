49f406f88fa4bf1874881cae4ae6615520cc0a17
chetannn <chetankharel7@gmail.com>
2024-06-03 19:36:54 +0545

    feat: use push modal for member invite modal

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -2,9 +2,11 @@
 
 import { createPushModal } from "pushmodal";
 import { ShareClassModal } from "./share-class-modal";
+import { TeamMemberModal } from "./team-member/team-member-modal";
 
 export const { pushModal, popModal, ModalProvider } = createPushModal({
   modals: {
     ShareClassModal,
+    TeamMemberModal,
   },
 });

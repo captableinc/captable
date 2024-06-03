85fb7491af41be7dba6f2cd42bff908cfb4eaa58
nafees nazik <nafeesnazik21@gmail.com>
2024-06-03 10:17:19 +0530

    feat: refactor sidebar

--- src/app/(authenticated)/(dashboard)/[publicId]/settings/layout.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/settings/layout.tsx
@@ -1,114 +1,14 @@
-"use client";
-
 import { PageLayout } from "@/components/dashboard/page-layout";
-import { NavLink } from "@/components/dashboard/sidebar/nav-link";
+import { SettingsSidebar } from "@/components/settings/settings-sidebar";
 import { Card } from "@/components/ui/card";
-import {
-  RiAccountCircleFill,
-  RiAccountCircleLine,
-  RiBankCardFill,
-  RiBankCardLine,
-  RiBuildingFill,
-  RiBuildingLine,
-  RiGroup2Fill,
-  RiGroup2Line,
-  RiLock2Fill,
-  RiLock2Line,
-  RiNotificationFill,
-  RiNotificationLine,
-} from "@remixicon/react";
-import { useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
-
-const companyNav = [
-  {
-    name: "Company",
-    href: "/settings/company",
-    icon: RiBuildingLine,
-    activeIcon: RiBuildingFill,
-  },
-  {
-    name: "Team",
-    href: "/settings/team",
-    icon: RiGroup2Line,
-    activeIcon: RiGroup2Fill,
-  },
-  {
-    name: "Billing",
-    href: "/settings/billing",
-    icon: RiBankCardLine,
-    activeIcon: RiBankCardFill,
-  },
-];
-
-const accountNav = [
-  {
-    name: "Profile",
-    href: "/settings/profile",
-    icon: RiAccountCircleLine,
-    activeIcon: RiAccountCircleFill,
-  },
-  {
-    name: "Notifications",
-    href: "/settings/notifications",
-    icon: RiNotificationLine,
-    activeIcon: RiNotificationFill,
-  },
-  {
-    name: "Security",
-    href: "/settings/security",
-    icon: RiLock2Line,
-    activeIcon: RiLock2Fill,
-  },
-];
+import { IS_BILLING_ENABLED } from "@/constants/stripe";
 
 const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
-  const currentPath = usePathname();
-  const { data } = useSession();
-  const companyPublicId = data?.user.companyPublicId;
-
   return (
     <PageLayout title="Settings">
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-12 md:col-span-3">
-          <ul className="text-sm">
-            {companyNav.map((item) => {
-              const href = `/${companyPublicId}${item.href}`;
-              const isActive = currentPath === href;
-
-              return (
-                <li key={item.name} className="rounded py-1">
-                  <NavLink
-                    active={isActive}
-                    name={item.name}
-                    href={href}
-                    icon={isActive ? item.activeIcon : item.icon}
-                  />
-                </li>
-              );
-            })}
-
-            <div className="mt-3 text-xs font-semibold leading-6 text-gray-400">
-              Account
-            </div>
-
-            {accountNav.map((item) => {
-              const href = `/${companyPublicId}${item.href}`;
-              const isActive =
-                currentPath.includes(href) || currentPath === href;
-
-              return (
-                <li key={item.name} className="rounded py-1">
-                  <NavLink
-                    active={isActive}
-                    name={item.name}
-                    href={href}
-                    icon={isActive ? item.activeIcon : item.icon}
-                  />
-                </li>
-              );
-            })}
-          </ul>
+          <SettingsSidebar isBillingEnabled={IS_BILLING_ENABLED} />
         </div>
         <div className="col-span-12 md:col-span-9">
           <Card className="p-5">{children}</Card>

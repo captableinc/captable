cc1f7c94153d17f4ad49f09b98b6f6b08f21e67a
nafees nazik <nafeesnazik21@gmail.com>
2024-06-06 14:50:04 +0530

    feat: add STRIPE_PUBLISHABLE_KEY to list

--- src/constants/stripe.ts
+++ src/constants/stripe.ts
@@ -1,5 +1,7 @@
 import { env } from "@/env";
 
 export const IS_BILLING_ENABLED = !!(
-  env.STRIPE_API_KEY && env.STRIPE_WEBHOOK_SECRET
+  env.STRIPE_API_KEY &&
+  env.STRIPE_WEBHOOK_SECRET &&
+  env.STRIPE_PUBLISHABLE_KEY
 );

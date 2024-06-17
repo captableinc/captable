9c83ba50c65c97ec9635f275e97d813a12f3f6cb
Vishnu Kumar <vishnukumart96@gmail.com>
2024-06-17 14:20:07 +0530

    feat: create useDebounce Hook

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = (value: string) => {
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setDebouncedValue(value);
+    }, 250);
+
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [value]);
+
+  return { debouncedValue };
+};

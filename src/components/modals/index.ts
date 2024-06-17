3357fe7f6fb8b3e5852562dd803aa60ad948c152
Raju kadel <rajukadel.github@gmail.com>
2024-06-17 12:21:59 +0545

    fix: conflicts in push modal index file

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import { WipModal } from "./wip-modal";
 import { createPushModal } from "pushmodal";
 import { BankAccountModal } from "./bank-account-modal";
 import { DocumentUploadModal } from "./document-upload-modal";
@@ -10,14 +11,13 @@ import { IssueStockOptionModal } from "./issue-stock-option-modal";
 import { NewSafeModal } from "./new-safe-modal";
 import { RoleCreateUpdateModal } from "./role-create-update-modal";
 import { ShareClassModal } from "./share-class/share-class-modal";
+import { ManageEsignRecipientsModal } from "./esign-recipients/manage-esign-recipients-modal";
 import { ShareDataRoomModal } from "./share-dataroom-modal";
 import { ShareUpdateModal } from "./share-update-modal";
 import { MultipleStakeholdersModal } from "./stakeholder/multiple-stakeholders-modal";
 import { SingleStakeholdersModal } from "./stakeholder/single-stake-holder-modal";
 import { UpdateSingleStakeholderModal } from "./stakeholder/update-stakeholder-modal";
 import { TeamMemberModal } from "./team-member/team-member-modal";
-import { WipModal } from "./wip-modal";
-
 import { AddEsignDocumentModal } from "./esign-doc";
 import { InvestorModal } from "./investor/add-investor-modal";
 
@@ -29,6 +29,7 @@ export const { pushModal, popModal, ModalProvider } = createPushModal({
     UpdateSingleStakeholderModal,
     ShareUpdateModal,
     ShareDataRoomModal,
+
     RoleCreateUpdate: RoleCreateUpdateModal,
     SingleStakeholdersModal,
     MultipleStakeholdersModal,
@@ -38,6 +39,7 @@ export const { pushModal, popModal, ModalProvider } = createPushModal({
     IssueStockOptionModal,
     AddEsignDocumentModal,
     BankAccountModal,
+    ManageEsignRecipientsModal,
 
     // Safe modals
     NewSafeModal,

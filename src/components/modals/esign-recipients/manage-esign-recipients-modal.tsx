5f072b8cf9ce2c6e05e780702f870c3f865d1696
Raju kadel <rajukadel.github@gmail.com>
2024-06-17 11:56:02 +0545

    feat: manage-esign-recipients push modal

---
+++ src/components/modals/esign-recipients/manage-esign-recipients-modal.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Modal from "@/components/common/push-modal";
+
+import { api } from "@/trpc/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import {
+  ManageEsignRecipientsForm,
+  type TRecipientFormSchema,
+} from "./manage-esign-recipients-form";
+
+export type RecipientSelectorModalProps = {
+  title: string | React.ReactNode;
+  subtitle: string | React.ReactNode;
+  templateId: string;
+  defaultValues: TRecipientFormSchema;
+};
+
+export const ManageEsignRecipientsModal = ({
+  title,
+  subtitle,
+  templateId,
+  defaultValues,
+}: RecipientSelectorModalProps) => {
+  const router = useRouter();
+
+  const { data: recipients, refetch: refetchRecipients } =
+    api.template.getRecipients.useQuery({ templateId });
+
+  const { mutateAsync: toggleOrderedDelivery } =
+    api.template.toggleOrderedDelivery.useMutation({
+      onSuccess: ({ success, message }) => {
+        if (success) {
+          toast.success(message);
+          refetchRecipients();
+          router.refresh();
+        }
+      },
+    });
+
+  const { mutateAsync: deleteRecipientMutation } =
+    api.template.removeRecipient.useMutation({
+      onSuccess: ({ success, message }) => {
+        if (success) {
+          toast.success(message);
+          refetchRecipients();
+          router.refresh();
+        }
+      },
+    });
+
+  const { mutateAsync: addRecipientMutation } =
+    api.template.addRecipient.useMutation({
+      onSuccess: ({ success, message }) => {
+        if (success) {
+          toast.success(message);
+          refetchRecipients();
+          router.refresh();
+        }
+      },
+    });
+
+  return (
+    <Modal size="2xl" title={title} subtitle={subtitle}>
+      <ManageEsignRecipientsForm
+        isUpdate={true}
+        recipientsPayload={recipients}
+        defaultValues={defaultValues}
+        onSubmit={() => {}}
+        onSave={async ({ recipient }) => {
+          await addRecipientMutation({
+            recipient,
+            templateId,
+          });
+        }}
+        onDelete={async ({ recipientId }) => {
+          await deleteRecipientMutation({ recipientId });
+        }}
+        onToggleOrderedDelivery={async (newStatus, setLoading) => {
+          await toggleOrderedDelivery({
+            orderedDelivery: newStatus,
+            templateId,
+          });
+          setLoading(false);
+        }}
+      />
+    </Modal>
+  );
+};

ab03aa80b6668ce1387d414df59910f120835e1f
Raju kadel <rajukadel.github@gmail.com>
2024-06-20 21:57:55 +0545

    feat: added loading effect capability and some improvements in code

--- src/components/modals/esign-recipients/manage-esign-recipients-form.tsx
+++ src/components/modals/esign-recipients/manage-esign-recipients-form.tsx
@@ -15,7 +15,13 @@ import type { RouterOutputs } from "@/trpc/shared";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { RiDeleteBinLine } from "@remixicon/react";
 import type React from "react";
-import { type SetStateAction, useEffect, useRef, useState } from "react";
+import {
+  Fragment,
+  type SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
@@ -35,12 +41,14 @@ export type TRecipientFormSchema = z.infer<typeof FormSchema>;
 export type OnDelete = {
   recipientId: string;
   email: string;
+  setLoading: React.Dispatch<SetStateAction<boolean>>;
 };
 export type OnSave = {
   recipient: {
     name: string;
     email: string;
   };
+  setLoading: React.Dispatch<SetStateAction<boolean>>;
 };
 type RecipientsPayload = RouterOutputs["template"]["getRecipients"];
 type SubmitProps =
@@ -58,7 +66,8 @@ type ManageEsignRecipientsFormProps = SubmitProps & {
   onDelete?: (data: OnDelete) => void;
   onToggleOrderedDelivery?: (
     newStatus: boolean,
-    set: React.Dispatch<SetStateAction<boolean>>,
+    handleCheck: (newStatusValue: boolean) => void,
+    setLoading: React.Dispatch<SetStateAction<boolean>>,
   ) => void;
   defaultValues?: TRecipientFormSchema;
   recipientsPayload?: RecipientsPayload;
@@ -73,7 +82,6 @@ export const ManageEsignRecipientsForm = ({
   defaultValues,
   recipientsPayload,
 }: ManageEsignRecipientsFormProps) => {
-  const effectRanCount = useRef<number>(0);
   const [canAddMoreRecipient, setCanAdd] = useState<boolean>(true);
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -98,210 +106,216 @@ export const ManageEsignRecipientsForm = ({
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: its-fine
   useEffect(() => {
-    if (recipientsPayload?.recipients?.length) {
+    if (recipientsPayload) {
       form.reset(recipientsPayload as TRecipientFormSchema);
       if (isUpdate) {
         setCanAdd(true);
       }
     }
-  }, [recipientsPayload?.recipients]);
+  }, [recipientsPayload]);
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: its-fine
-  useEffect(() => {
-    if (effectRanCount.current > 1 && onToggleOrderedDelivery) {
-      setLoading(true);
-      onToggleOrderedDelivery(form.getValues("orderedDelivery"), setLoading);
-    }
-    effectRanCount.current += 1;
-  }, [form.getValues("orderedDelivery")]);
-
-  function getRecipientIdByEmail(email: string) {
+  const getRecipientIdByEmail = useCallback((email: string) => {
     const recipients = form.getValues("recipients");
     const targetRecipient = recipients.filter((recp) => recp.email === email);
-    return targetRecipient[0]?.id ?? "";
-  }
+    return targetRecipient[0]?.id ?? ("" as string);
+  }, []);
 
-  function getNewRecipient() {
+  // biome-ignore lint/correctness/useExhaustiveDependencies: its-fine
+  const isExistingRecipient = useCallback(
+    (email: string): boolean => !!getRecipientIdByEmail(email),
+    [],
+  );
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: its-fine
+  const getCurrentSavingRecipient = useCallback(() => {
     return form.getValues("recipients")[
       form.getValues("recipients").length - 1
-    ];
-  }
-
-  function getNewRecipientName() {
-    return getNewRecipient()?.name;
-  }
-
-  function getNewRecipientEmail() {
-    return getNewRecipient()?.email;
-  }
+    ] as {
+      id: string;
+      name: string | undefined;
+      email: string;
+    };
+  }, []);
 
   return (
-    <Form {...form}>
-      <form id="recipient-form" onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-y-2">
-          <div className="flex flex-col">
-            {fields.map((item, index) => (
-              <div
-                className="my-3 flex items-end justify-between gap-x-2"
-                key={item.id}
-              >
-                <div className="flex items-center gap-x-10">
-                  <FormField
-                    control={form.control}
-                    name={`recipients.${index}.name`}
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                          <Input
-                            disabled={!!getRecipientIdByEmail(item.email)}
-                            className="h-8 min-w-16"
-                            type="text"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+    <Fragment>
+      <Form {...form}>
+        <form id="recipient-form" onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-y-2">
+            <div className="flex flex-col">
+              {fields.map((item, index) => (
+                <div
+                  className="my-3 flex items-end justify-between gap-x-2"
+                  key={item.id}
+                >
+                  <div className="flex items-center gap-x-10">
+                    <FormField
+                      control={form.control}
+                      name={`recipients.${index}.name`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input
+                              disabled={isExistingRecipient(item.email)}
+                              className="h-8 min-w-16"
+                              type="text"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
 
-                  <FormField
-                    control={form.control}
-                    name={`recipients.${index}.email`}
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                          <Input
-                            disabled={!!getRecipientIdByEmail(item.email)}
-                            className="h-8 min-w-16"
-                            type="email"
-                            required
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+                    <FormField
+                      control={form.control}
+                      name={`recipients.${index}.email`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              disabled={isExistingRecipient(item.email)}
+                              className="h-8 min-w-16"
+                              type="email"
+                              required
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
 
-                <div className="flex items-center">
-                  <div className="flex items-center space-x-5">
-                    {isUpdate && !getRecipientIdByEmail(item.email) && (
-                      <Button
-                        type="submit"
-                        variant={"outline"}
-                        size="sm"
-                        onClick={() => {
-                          if (onSave) {
-                            if (!getNewRecipientEmail()) {
-                              toast.error("Email must be present");
+                  <div className="flex items-center">
+                    <div className="flex items-center space-x-5">
+                      {isUpdate && !isExistingRecipient(item.email) && (
+                        <Button
+                          type="submit"
+                          variant={"outline"}
+                          size="sm"
+                          onClick={() => {
+                            if (onSave) {
+                              if (!getCurrentSavingRecipient().email) {
+                                toast.error("Email must be present");
+                                return;
+                              }
+                              onSave({
+                                recipient: {
+                                  name: getCurrentSavingRecipient().name ?? "",
+                                  email: getCurrentSavingRecipient().email,
+                                },
+                                setLoading,
+                              });
                             }
-                            console.log("SAVING...");
-                            onSave({
-                              recipient: {
-                                name: getNewRecipientName() ?? "",
-                                email: getNewRecipientEmail() as string,
-                              },
-                            });
-                          }
-                        }}
-                      >
-                        Save
-                      </Button>
-                    )}
+                          }}
+                        >
+                          Save
+                        </Button>
+                      )}
 
-                    {isUpdate && (
-                      <Button
-                        onClick={() => {
-                          if (onDelete && getRecipientIdByEmail(item.email)) {
-                            onDelete({
-                              recipientId: getRecipientIdByEmail(item.email),
-                              email: item.email,
-                            });
-                          } else {
+                      {isUpdate && (
+                        <Button
+                          onClick={() => {
+                            if (onDelete && isExistingRecipient(item.email)) {
+                              onDelete({
+                                recipientId: getRecipientIdByEmail(item.email),
+                                email: item.email,
+                                setLoading,
+                              });
+                            } else {
+                              remove(index);
+                              setCanAdd(true);
+                            }
+                          }}
+                          variant="ghost"
+                          size="sm"
+                          className="group h-8 w-8 p-2"
+                        >
+                          <RiDeleteBinLine
+                            aria-hidden
+                            className="h-8 w-8 text-red-500/70 group-hover:text-red-500"
+                          />
+                        </Button>
+                      )}
+
+                      {!isUpdate && fields.length > 1 && (
+                        <Button
+                          disabled={isDeleteDisabled}
+                          onClick={() => {
                             remove(index);
-                            setCanAdd(true);
-                          }
-                        }}
-                        variant="ghost"
-                        size="sm"
-                        className="group h-8 w-8 p-2"
-                      >
-                        <RiDeleteBinLine
-                          aria-hidden
-                          className="h-8 w-8 text-red-500/70 group-hover:text-red-500"
-                        />
-                      </Button>
-                    )}
-
-                    {!isUpdate && fields.length > 1 && (
-                      <Button
-                        disabled={isDeleteDisabled}
-                        onClick={() => {
-                          remove(index);
-                        }}
-                        variant="ghost"
-                        size="sm"
-                        className="group h-8 w-8 p-2"
-                      >
-                        <RiDeleteBinLine
-                          aria-hidden
-                          className="h-8 w-8 text-red-500/70 group-hover:text-red-500"
-                        />
-                      </Button>
-                    )}
+                          }}
+                          variant="ghost"
+                          size="sm"
+                          className="group h-8 w-8 p-2"
+                        >
+                          <RiDeleteBinLine
+                            aria-hidden
+                            className="h-8 w-8 text-red-500/70 group-hover:text-red-500"
+                          />
+                        </Button>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
 
-          <div className="w-full">
-            <FormField
-              control={form.control}
-              name="orderedDelivery"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                  <FormControl>
-                    {loading ? (
-                      <Loading />
-                    ) : (
+            <div className="w-full">
+              <FormField
+                control={form.control}
+                name="orderedDelivery"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                    <FormControl>
                       <Checkbox
                         checked={field.value}
-                        onCheckedChange={field.onChange}
+                        onCheckedChange={(newStatusValue: boolean) => {
+                          if (isUpdate && onToggleOrderedDelivery) {
+                            onToggleOrderedDelivery(
+                              newStatusValue,
+                              field.onChange,
+                              setLoading,
+                            );
+                          } else {
+                            return field.onChange(newStatusValue);
+                          }
+                        }}
                       />
-                    )}
-                  </FormControl>
-                  <div className="leading-none w-full flex justify-between item-center ">
-                    <FormLabel>
-                      Require recipients to sign in the order they are added
-                    </FormLabel>
-                  </div>
-                </FormItem>
-              )}
-            />
-          </div>
+                    </FormControl>
+                    <div className="leading-none w-full flex justify-between item-center ">
+                      <FormLabel>
+                        Require recipients to sign in the order they are added
+                      </FormLabel>
+                    </div>
+                  </FormItem>
+                )}
+              />
+            </div>
 
-          <div className="flex justify-between items-center my-2">
-            <Button
-              disabled={!canAddMoreRecipient}
-              type="button"
-              variant={isUpdate ? "default" : "outline"}
-              size="sm"
-              onClick={() => {
-                append({ email: "", name: "", id: "" });
-                if (isUpdate) {
-                  setCanAdd(false);
-                }
-              }}
-            >
-              Add more recipient
-            </Button>
+            <div className="flex justify-between items-center my-2">
+              <Button
+                disabled={!canAddMoreRecipient}
+                type="button"
+                variant={isUpdate ? "default" : "outline"}
+                size="sm"
+                onClick={() => {
+                  append({ email: "", name: "", id: "" });
+                  if (isUpdate) {
+                    setCanAdd(false);
+                  }
+                }}
+              >
+                Add more recipient
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+      {loading && <Loading />}
+    </Fragment>
   );
 };

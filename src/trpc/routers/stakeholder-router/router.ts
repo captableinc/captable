9219e6306cb4b51db9fbcff732511d860426f8d6
Raju kadel <rajukadel.github@gmail.com>
2024-06-23 16:14:30 +0545

    feat: register update stakeholder procedure in root router

--- src/trpc/routers/stakeholder-router/router.ts
+++ src/trpc/routers/stakeholder-router/router.ts
@@ -2,8 +2,10 @@ import { createTRPCRouter } from "@/trpc/api/trpc";
 
 import { addStakeholdersProcedure } from "./procedures/add-stakeholders";
 import { getStakeholdersProcedure } from "./procedures/get-stakeholders";
+import { updateStakeholderProcedure } from "./procedures/update-stakeholder";
 
 export const stakeholderRouter = createTRPCRouter({
   addStakeholders: addStakeholdersProcedure,
   getStakeholders: getStakeholdersProcedure,
+  updateStakeholder: updateStakeholderProcedure,
 });

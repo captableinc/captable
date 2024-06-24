c7c74c772d784ed4d237a712e42364e5d1dffe62
nafees nazik <nafeesnazik21@gmail.com>
2024-06-24 23:32:34 +0530

    feat: use t3 env

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,8 @@
+import { env } from "@/env";
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1,
   debug: false,
   replaysOnErrorSampleRate: 1.0,

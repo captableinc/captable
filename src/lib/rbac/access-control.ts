6844da1669667634d2426c445e71cf75dbb2c721
nafees nazik <nafeesnazik21@gmail.com>
2024-06-27 19:26:39 +0530

    feat: add access control utils

---
+++ src/lib/rbac/access-control.ts
@@ -0,0 +1,34 @@
+import { defaultRoles } from "@/constants/rbac";
+import type { Roles } from "@/prisma/enums";
+import { checkMembership } from "@/server/auth";
+import type { TPrismaOrTransaction } from "@/server/db";
+import type { Session } from "next-auth";
+import { wrap } from "../error";
+import { BaseError } from "../error/errors/base";
+
+export interface checkMembershipOptions {
+  session: Session;
+  tx: TPrismaOrTransaction;
+}
+
+class MembershipNotFoundError extends BaseError {
+  public readonly name = "MembershipNotFoundError";
+  public readonly retry = false;
+}
+
+export async function checkAccessControlMembership({
+  session,
+  tx,
+}: checkMembershipOptions) {
+  return wrap(
+    checkMembership({ session, tx }),
+    (err) => new MembershipNotFoundError({ message: err.message }),
+  );
+}
+
+export function getRules(role: Roles) {
+  if (role !== "CUSTOM") {
+    return defaultRoles[role];
+  }
+  return defaultRoles.SUPER_USER;
+}

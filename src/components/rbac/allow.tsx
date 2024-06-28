04a6467da3567e9115739fae1db46811eaf93826
nafees nazik <nafeesnazik21@gmail.com>
2024-06-28 09:41:35 +0530

    feat: improve allow component dx

--- src/components/rbac/allow.tsx
+++ src/components/rbac/allow.tsx
@@ -4,11 +4,18 @@ import { useAllowed, type useAllowedOptions } from "@/hooks/use-allowed";
 import type { ReactNode } from "react";
 
 interface AllowProps extends useAllowedOptions {
-  children: ReactNode;
+  children: ReactNode | ((authorized: boolean) => ReactNode);
 }
 
 export const Allow = ({ children, ...rest }: AllowProps) => {
   const { isAllowed } = useAllowed({ ...rest });
 
-  return isAllowed ? children : null;
+  if (isAllowed) {
+    if (typeof children === "function") {
+      return children(isAllowed);
+    }
+    return children;
+  }
+
+  return null;
 };

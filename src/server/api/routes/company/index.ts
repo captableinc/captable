8bba06269c157812b5726e74aa71fd7f59b2c35f
Puru D <puru@dahal.me>
2024-06-30 04:45:55 -0500

    chore: remove team endpoint for now because of some build error

--- src/server/api/routes/company/index.ts
+++ src/server/api/routes/company/index.ts
@@ -1,14 +1,10 @@
 import type { PublicAPI } from "@/server/api/hono";
 import getMany from "./getMany";
 import getOne from "./getOne";
-// import create from "./create";
-// import update from "./update";
 
 const companyRoutes = (api: PublicAPI) => {
   getOne(api);
   getMany(api);
-  // create(api); // TODO - Implement create
-  // update(api); // TODO - Implement update
 };
 
 export default companyRoutes;

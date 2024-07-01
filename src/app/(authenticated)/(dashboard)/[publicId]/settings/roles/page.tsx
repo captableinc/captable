8b791e20e1e216e6469126d440bda8a7c2971194
nafees nazik <nafeesnazik21@gmail.com>
2024-07-01 17:24:05 +0530

    feat: add basic page

--- src/app/(authenticated)/(dashboard)/[publicId]/settings/roles/page.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/settings/roles/page.tsx
@@ -1,3 +1,28 @@
+import { PageLayout } from "@/components/dashboard/page-layout";
+import { Allow } from "@/components/rbac/allow";
+import { CreateRbacModal } from "@/components/rbac/create-rbac-modal";
+import { Card } from "@/components/ui/card";
+
 export default function RolesPage() {
-  return <div>hello world</div>;
+  return (
+    <div className="flex flex-col gap-y-3">
+      <PageLayout
+        title="Roles"
+        description="Manage your roles."
+        action={
+          <Allow action="create" subject="roles">
+            {/* biome-ignore lint/suspicious/useAwait: <explanation> */}
+            {async () => {
+              "use server";
+              return <CreateRbacModal />;
+            }}
+          </Allow>
+        }
+      />
+
+      <Card className="mt-3">
+        <div className="p-6" />
+      </Card>
+    </div>
+  );
 }

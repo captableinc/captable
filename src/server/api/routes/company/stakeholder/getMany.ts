f9bd67fa6c3ede9bc48baed786cabe5989436bd6
Raju kadel <rajukadel.github@gmail.com>
2024-07-04 01:56:14 +0545

    fix: getting query parameters

--- src/server/api/routes/company/stakeholder/getMany.ts
+++ src/server/api/routes/company/stakeholder/getMany.ts
@@ -1,30 +1,26 @@
 import { withCompanyAuth } from "@/server/api/auth";
 import { ErrorResponses } from "@/server/api/error";
 import type { PublicAPI } from "@/server/api/hono";
-import { ProxyPrismaModel } from "@/server/api/pagination/prisma-proxy";
 import {
   PaginationQuerySchema,
   PaginationResponseSchema,
-  type TPaginationQuerySchema,
 } from "@/server/api/schema/pagination";
 import { StakeholderApiResponseSchema } from "@/server/api/schema/stakeholder";
 import { db } from "@/server/db";
+import { getPaginatedStakeholders } from "@/server/services/stakeholder/get-stakeholders";
 import { createRoute, z } from "@hono/zod-openapi";
 import type { Context } from "hono";
 
 export const RequestParamsSchema = z.object({
-  id: z
-    .string()
-    .cuid()
-    .openapi({
-      description: "Company ID",
-      param: {
-        name: "id",
-        in: "path",
-      },
-
-      example: "clxwbok580000i7nge8nm1ry0",
-    }),
+  id: z.string().openapi({
+    param: {
+      name: "id",
+      in: "path",
+    },
+    description: "Company ID",
+    type: "string",
+    example: "clxwbok580000i7nge8nm1ry0",
+  }),
 });
 
 const ResponseSchema = z.object({
@@ -34,10 +30,10 @@ const ResponseSchema = z.object({
 
 const route = createRoute({
   method: "get",
-  path: "/v1/companies/:id/stakeholders",
+  path: "/v1/companies/{id}/stakeholders",
   request: {
-    query: PaginationQuerySchema,
     params: RequestParamsSchema,
+    query: PaginationQuerySchema,
   },
   responses: {
     200: {
@@ -56,39 +52,19 @@ const route = createRoute({
 const getMany = (app: PublicAPI) => {
   //@ts-ignore
   app.openapi(route, async (c: Context) => {
-    const { company } = await withCompanyAuth(c);
-    const body: TPaginationQuerySchema = await c.req.json();
+    const query = c.req.query();
 
-    const queryCriteria = {
-      where: {
+    const { company } = await withCompanyAuth(c);
+    const { data, meta } = await getPaginatedStakeholders({
+      db,
+      payload: {
+        limit: Number(query.limit) ?? 10,
+        cursor: query.cursor as string,
         companyId: company.id,
       },
-      orderBy: {
-        createdAt: "desc",
-      },
-    };
-
-    const paginationData = {
-      limit: body.limit,
-      cursor: body.cursor,
-    };
-
-    //@ts-ignore
-    const prismaModel = ProxyPrismaModel(db.stakeholder);
-    const { data, count, total, cursor } = await prismaModel.findManyPaginated(
-      queryCriteria,
-      paginationData,
-    );
-
-    return c.json({
-      data: data,
-      meta: {
-        data,
-        count,
-        total,
-        cursor,
-      },
     });
+
+    return c.json({ data, meta }, 200);
   });
 };
 

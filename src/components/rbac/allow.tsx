fea655785580c2ad6ac3aa426edd7a74e777248a
nafees nazik <nafeesnazik21@gmail.com>
2024-07-06 07:13:58 +0530

    feat: add render prop pattern

--- src/components/rbac/allow.tsx
+++ src/components/rbac/allow.tsx
@@ -4,15 +4,17 @@ import { useAllowed, type useAllowedOptions } from "@/hooks/use-allowed";
 import type { ReactNode } from "react";
 
 interface AllowProps extends useAllowedOptions {
-  children: ReactNode;
-  not?: ReactNode;
+  children: ReactNode | ((authorized: boolean) => ReactNode);
 }
 
-export const Allow = ({ children, not, ...rest }: AllowProps) => {
+export const Allow = ({ children, ...rest }: AllowProps) => {
   const { isAllowed } = useAllowed({ ...rest });
 
   if (isAllowed) {
+    if (typeof children === "function") {
+      return children(isAllowed);
+    }
     return children;
   }
-  return not ? not : null;
+  return null;
 };

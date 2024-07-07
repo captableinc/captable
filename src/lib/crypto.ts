e7046200b5857789bc2ab4dabefe15edbdf1e494
Raju kadel <rajukadel.github@gmail.com>
2024-07-08 01:03:59 +0545

    fix: conflict

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,8 +1,55 @@
-import { randomBytes, scryptSync, subtle } from "node:crypto";
+import crypto from "node:crypto";
+
+const ALGORITHM = "aes256";
+const INPUT_ENCODING = "utf8";
+const OUTPUT_ENCODING = "hex";
+const IV_LENGTH = 16; // AES blocksize
+
+/**
+ *
+ * @param text Value to be encrypted
+ * @param key Key used to encrypt value must be 32 bytes for AES256 encryption algorithm
+ *
+ * @returns Encrypted value using key
+ */
+export const Encrypted = (text: string, key: string) => {
+  const _key = Buffer.from(key, "latin1");
+  const iv = crypto.randomBytes(IV_LENGTH);
+
+  const cipher = crypto.createCipheriv(ALGORITHM, _key, iv);
+  let ciphered = cipher.update(text, INPUT_ENCODING, OUTPUT_ENCODING);
+  ciphered += cipher.final(OUTPUT_ENCODING);
+  const ciphertext = `${iv.toString(OUTPUT_ENCODING)}:${ciphered}`;
+  return ciphertext;
+};
+
+/**
+ *
+ * @param text Value to decrypt
+ * @param key Key used to decrypt value must be 32 bytes for AES256 encryption algorithm
+ */
+export const Decrypted = (text: string, key: string) => {
+  const _key = Buffer.from(key, "latin1");
+
+  const components = text.split(":");
+  const iv_from_ciphertext = Buffer.from(
+    components.shift() || "",
+    OUTPUT_ENCODING,
+  );
+  const decipher = crypto.createDecipheriv(ALGORITHM, _key, iv_from_ciphertext);
+  let deciphered = decipher.update(
+    components.join(":"),
+    OUTPUT_ENCODING,
+    INPUT_ENCODING,
+  );
+  deciphered += decipher.final(INPUT_ENCODING);
+
+  return deciphered;
+};
 
 export const createHash = async (key: string) => {
   const data = new TextEncoder().encode(key);
-  const hash = await subtle.digest("SHA-256", data);
+  const hash = await crypto.subtle.digest("SHA-256", data);
 
   return Array.from(new Uint8Array(hash))
     .map((b) => b.toString(16).padStart(2, "0"))
@@ -16,8 +63,8 @@ export const createApiToken = () => {
 
 export const createSecureHash = (key: string) => {
   const data = new TextEncoder().encode(key);
-  const salt = randomBytes(16).toString("hex");
-  const derivedKey = scryptSync(data, salt, 64);
+  const salt = crypto.randomBytes(16).toString("hex");
+  const derivedKey = crypto.scryptSync(data, salt, 64);
 
   return `${salt}:${derivedKey.toString("hex")}`;
 };
@@ -25,7 +72,7 @@ export const createSecureHash = (key: string) => {
 export const verifySecureHash = (key: string, hash: string) => {
   const data = new TextEncoder().encode(key);
   const [salt, storedHash] = hash.split(":");
-  const derivedKey = scryptSync(data, String(salt), 64);
+  const derivedKey = crypto.scryptSync(data, String(salt), 64);
 
   return storedHash === derivedKey.toString("hex");
 };

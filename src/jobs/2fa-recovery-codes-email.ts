641128bd70ad355de5bc7a3d53094c7e076f55f5
Raju kadel <rajukadel.github@gmail.com>
2024-07-11 01:51:24 +0545

    feat: job for sending recovery codes to registered gmail

---
+++ src/jobs/2fa-recovery-codes-email.ts
@@ -0,0 +1,37 @@
+import TwoFARecoveryCodesEmail from "@/emails/2FARecoveryCodesEmail";
+import { BaseJob } from "@/jobs/base";
+
+import { sendMail } from "@/server/mailer";
+import { render } from "jsx-email";
+import type { Job } from "pg-boss";
+
+export type TwoFARecoveryCodesPayloadType = {
+  email: string;
+  recoveryCodes: string[];
+};
+
+export const send2FARecoveryCodesEmail = async (
+  payload: TwoFARecoveryCodesPayloadType,
+) => {
+  const { email, recoveryCodes } = payload;
+
+  const html = await render(
+    TwoFARecoveryCodesEmail({
+      recoveryCodes,
+    }),
+  );
+
+  await sendMail({
+    to: email,
+    subject: "2FA recovery codes",
+    html,
+  });
+};
+
+export class TwoFARecoveryCodesEmailJob extends BaseJob<TwoFARecoveryCodesPayloadType> {
+  readonly type = "email.2fa-recovery-codes";
+
+  async work(job: Job<TwoFARecoveryCodesPayloadType>): Promise<void> {
+    await send2FARecoveryCodesEmail(job.data);
+  }
+}

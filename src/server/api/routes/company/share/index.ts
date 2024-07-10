7fab72647d459cac9200fee0ea506653c2d2b3cf
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-10 14:53:23 +0530

    feat: integrate createShare route in shareRoutes

--- src/server/api/routes/company/share/index.ts
+++ src/server/api/routes/company/share/index.ts
@@ -1,10 +1,12 @@
 import type { PublicAPI } from "@/server/api/hono";
+import create from "./create";
 import getMany from "./getMany";
 import getOne from "./getOne";
 
 const shareRoutes = (api: PublicAPI) => {
   getOne(api);
   getMany(api);
+  create(api);
 };
 
 export default shareRoutes;

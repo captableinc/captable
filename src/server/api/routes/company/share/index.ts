f8be63b8a9e6a7c20cb6d43d2b07a90b17b8d647
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-10 12:28:51 +0530

    feat: integrate getMany route in shareRoutes

--- src/server/api/routes/company/share/index.ts
+++ src/server/api/routes/company/share/index.ts
@@ -1,8 +1,10 @@
 import type { PublicAPI } from "@/server/api/hono";
+import getMany from "./getMany";
 import getOne from "./getOne";
 
 const shareRoutes = (api: PublicAPI) => {
   getOne(api);
+  getMany(api);
 };
 
 export default shareRoutes;

219eb01810e21d4d1b8822e3ccbdc72f3b65b153
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-11 10:45:10 +0530

    feat: integrate updateShare route

--- src/server/api/routes/company/share/index.ts
+++ src/server/api/routes/company/share/index.ts
@@ -2,11 +2,13 @@ import type { PublicAPI } from "@/server/api/hono";
 import create from "./create";
 import getMany from "./getMany";
 import getOne from "./getOne";
+import update from "./update";
 
 const shareRoutes = (api: PublicAPI) => {
   getOne(api);
   getMany(api);
   create(api);
+  update(api);
 };
 
 export default shareRoutes;

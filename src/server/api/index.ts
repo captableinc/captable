cce66e505e13654bdcadfe700b4f38dd96c4a404
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-11 11:40:19 +0530

    feat: call shareRoutes in index

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,9 +1,13 @@
 import { PublicAPI } from "./hono";
 import companyRoutes from "./routes/company";
+import shareRoutes from "./routes/company/share";
 
 export const api = PublicAPI();
 
 // RESTful routes for company
 companyRoutes(api);
+
+// RESTful routes for shares
+shareRoutes(api);
 
 export default api;

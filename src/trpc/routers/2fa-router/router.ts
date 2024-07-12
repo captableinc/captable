0a34ddd7a9c572385391f56b9fdb30a26b83fb8b
Raju kadel <rajukadel.github@gmail.com>
2024-07-12 10:13:34 +0545

    feat: 2FA router handlings

--- src/trpc/routers/2fa-router/router.ts
+++ src/trpc/routers/2fa-router/router.ts
@@ -1,43 +1,91 @@
+import { disableTwoFactorAuthentication } from "@/server/2FA/disable";
+import { enableTwoFactorAuthentication } from "@/server/2FA/enable";
+import { setupTwoFactorAuthentication } from "@/server/2FA/setup";
 import { createTRPCRouter, withAuth } from "@/trpc/api/trpc";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 export const twoFactorAuthRouter = createTRPCRouter({
-  setup: withAuth.mutation(({ ctx: { session } }) => {
+  setup: withAuth.mutation(async ({ ctx: { session, db } }) => {
     try {
-      console.log({ session });
+      const { user } = session;
+      const { secret, uri } = await setupTwoFactorAuthentication({
+        db,
+        user: {
+          id: user.id,
+          email: user.email as string,
+        },
+      });
+      return {
+        success: true,
+        data: { secret, uri },
+      };
     } catch (error) {
       console.error(error);
       return {
         success: false,
-        error: "Something went out while setting up.Please try again later",
+        data: null,
+        error:
+          "Unable to setup two factor authentication. Please try again later.",
       };
     }
   }),
 
   enable: withAuth
     .input(z.object({ code: z.string() }))
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx: { session, db }, input }) => {
       try {
-        console.log({ ctx, input });
+        const { user } = session;
+        const { recoveryCodes } = await enableTwoFactorAuthentication({
+          db,
+          userId: user.id,
+          code: input.code,
+        });
+        return {
+          success: true,
+          recoveryCodes,
+        };
       } catch (error) {
         console.error(error);
+        if (error instanceof TRPCError) {
+          return {
+            success: false,
+            message: error.message,
+          };
+        }
         return {
           success: false,
-          error: "Something went out while enabling.Please try again later",
+          error:
+            "Failed enabling the two factor authentication.Please try again later",
         };
       }
     }),
 
   disable: withAuth
     .input(z.object({ code: z.string() }))
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx: { session, db }, input }) => {
       try {
-        console.log({ ctx, input });
+        const { user } = session;
+        await disableTwoFactorAuthentication({
+          db,
+          userId: user.id,
+          token: input.code,
+        });
+        return {
+          success: true,
+        };
       } catch (error) {
         console.error(error);
+        if (error instanceof TRPCError) {
+          return {
+            success: false,
+            message: error.message,
+          };
+        }
         return {
           success: false,
-          error: "Something went out while enabling.Please try again later",
+          error:
+            "Failed disabling the two factor authentication.Please try again later",
         };
       }
     }),

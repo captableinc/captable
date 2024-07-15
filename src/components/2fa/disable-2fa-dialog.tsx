f081d96922da18b68efd1e865f8cc6fda4d767e6
Raju kadel <rajukadel.github@gmail.com>
2024-07-16 01:26:20 +0545

    feat: minor improvements

--- src/components/2fa/disable-2fa-dialog.tsx
+++ src/components/2fa/disable-2fa-dialog.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { api } from "@/trpc/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+import { SecurityList } from "../security/SecurityList";
+
 import {
   Dialog,
   DialogClose,
@@ -19,17 +28,11 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { api } from "@/trpc/react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { z } from "zod";
-import { SecurityList } from "../security/SecurityList";
+import { RiRotateLockFill } from "@remixicon/react";
+import { CaptableLogo } from "../common/logo";
 
 export const Disable2FAForm = z.object({
-  code: z.string(),
+  code: z.string().min(6).max(9),
 });
 
 export type TDisable2FAForm = z.infer<typeof Disable2FAForm>;
@@ -38,8 +41,13 @@ export const DisableTwoFactorDialog = () => {
   const router = useRouter();
 
   const [isOpen, setIsOpen] = useState(false);
+  const [twoFAMethod, setTwoFAMethod] = useState<"totp" | "recovery">("totp");
 
-  const { mutateAsync: disable2FA } = api.twoFactorAuth.disable.useMutation();
+  const { mutateAsync: disable2FA } = api.twoFactorAuth.disable.useMutation({
+    onError: () => {
+      toast.error("Unable to disable two factor authentication");
+    },
+  });
 
   const form = useForm<TDisable2FAForm>({
     defaultValues: {
@@ -51,18 +59,26 @@ export const DisableTwoFactorDialog = () => {
   const { isSubmitting: isDisable2FASubmitting } = form.formState;
 
   const onDisable2FAFormSubmit = async ({ code }: TDisable2FAForm) => {
-    try {
-      await disable2FA({ code });
-
-      console.log({ code }, "Disabled clicked");
-
-      toast.success("Two factor authentication disabled");
-
+    const { success, message } = await disable2FA({ code });
+    console.log({ success, message });
+    if (success) {
+      toast.success(message);
       router.refresh();
-    } catch (_err) {
-      toast.error("Unable to disable two factor authentication");
+    } else {
+      toast.error(message);
     }
   };
+
+  const handleDisable2FA = () => {
+    setIsOpen(true);
+  };
+
+  const onToggle2FAMethod = () => {
+    const newMethod = twoFAMethod === "recovery" ? "totp" : "recovery";
+    form.setValue("code", "");
+    setTwoFAMethod(newMethod);
+  };
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild={true}>
@@ -72,19 +88,38 @@ export const DisableTwoFactorDialog = () => {
           description={"Add an extra layer of security to your account."}
           buttonDisplayName={"Enable 2FA"}
           // biome-ignore lint/correctness/noChildrenProp: <explanation>
-          children={<Button className="flex-shrink-0">Disable 2FA</Button>}
+          children={
+            <Button
+              onClick={(e) => {
+                e.preventDefault();
+                handleDisable2FA();
+              }}
+              className="flex-shrink-0"
+            >
+              Disable 2FA
+            </Button>
+          }
         />
       </DialogTrigger>
 
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Disable 2FA</DialogTitle>
-
-          <DialogDescription>
-            Please provide a token from the authenticator, or a backup code. If
-            you do not have a backup code available, please contact support.
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent
+        className={`${
+          twoFAMethod === "recovery" ? "max-w-2xl w-xl" : "max-w-lg w-lg"
+        }`}
+      >
+        <header className="border-b border-gray-200 py-5 px-5">
+          <DialogHeader>
+            <div className="flex justify-center">
+              <CaptableLogo className="mb-3 h-8 w-8 rounded" />
+            </div>
+            <DialogTitle className="mb-4 text-center">Disable 2FA</DialogTitle>
+            <DialogDescription className="text-center mx-auto">
+              {twoFAMethod === "totp"
+                ? "Please provide a token from the authenticator app."
+                : "Please provide one of your recovery code."}
+            </DialogDescription>
+          </DialogHeader>
+        </header>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onDisable2FAFormSubmit)}>
@@ -100,11 +135,19 @@ export const DisableTwoFactorDialog = () => {
                     <FormControl>
                       <>
                         <FormItem>
-                          <OtpStyledInput
-                            numInputs={5}
-                            inputType="number"
-                            {...field}
-                          />
+                          {twoFAMethod === "totp" ? (
+                            <OtpStyledInput
+                              numInputs={6}
+                              inputType="text"
+                              {...field}
+                            />
+                          ) : (
+                            <OtpStyledInput
+                              numInputs={9}
+                              inputType="text"
+                              {...field}
+                            />
+                          )}
                         </FormItem>
                         <FormMessage />
                       </>
@@ -114,19 +157,24 @@ export const DisableTwoFactorDialog = () => {
                 )}
               />
 
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="button" variant="secondary">
-                    Cancel
-                  </Button>
-                </DialogClose>
+              <DialogFooter className="pt-2">
+                <Button
+                  type="button"
+                  variant="secondary"
+                  onClick={onToggle2FAMethod}
+                >
+                  <RiRotateLockFill />
+                  {twoFAMethod === "totp"
+                    ? "Use Backup Code"
+                    : "Use Authenticator"}
+                </Button>
 
                 <Button
-                  type="submit"
                   variant="destructive"
+                  type="submit"
                   loading={isDisable2FASubmitting}
                 >
-                  Disable 2FA
+                  {isDisable2FASubmitting ? "Disabling..." : "Disable 2FA"}
                 </Button>
               </DialogFooter>
             </fieldset>
--- src/components/2fa/enable-2fa-dialog.tsx
+++ src/components/2fa/enable-2fa-dialog.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import type React from "react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { renderSVG } from "uqr";
@@ -18,10 +19,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { send2FARecoveryCodesEmail } from "@/jobs/2fa-recovery-codes-email";
 import { downloadFile } from "@/lib/download-file";
 import { api } from "@/trpc/react";
+import { RiDownload2Line, RiMailLine } from "@remixicon/react";
 import { toast } from "sonner";
+import { CaptableLogo } from "../common/logo";
 import { SecurityList } from "../security/SecurityList";
 import { OtpStyledInput } from "../ui/extension/otp-input";
 import {
@@ -29,13 +31,12 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "../ui/form";
 import { RecoveryCodeList } from "./recovery-code-list";
 
 export const ZEnable2FAForm = z.object({
-  code: z.string(),
+  code: z.string().min(6),
 });
 
 export type TEnable2FAForm = z.infer<typeof ZEnable2FAForm>;
@@ -47,12 +48,19 @@ export type EnableTwoFactorAppDialogProps = {
 export const EnableTwoFactorAppDialog = ({
   onSuccess,
 }: EnableTwoFactorAppDialogProps) => {
-  const _router = useRouter();
+  const router = useRouter();
 
   const [isOpen, setIsOpen] = useState(false);
+  const [hasEmailSent, setHasEmailSent] = useState<boolean>(false);
   const [recoveryCodes, setRecoveryCodes] = useState<string[] | null>(null);
 
   const { mutateAsync: enable2FA } = api.twoFactorAuth.enable.useMutation();
+  const { mutateAsync: sendRecoveryCodes } =
+    api.twoFactorAuth.sendRecoveryCodes.useMutation({
+      onError: () => {
+        toast.error("Unable to send 2FA recovery codes.");
+      },
+    });
 
   const {
     mutateAsync: setup2FA,
@@ -74,27 +82,18 @@ export const EnableTwoFactorAppDialog = ({
   const { isSubmitting: isEnabling2FA } = form.formState;
 
   const onFormSubmit = async ({ code }: TEnable2FAForm) => {
-    try {
-      console.log({ code });
-
-      const { recoveryCodes } = await enable2FA({ code });
-
-      console.log({ recoveryCodes });
-
-      if (recoveryCodes) {
-        setRecoveryCodes(recoveryCodes);
-        onSuccess?.();
-      }
-
-      toast.success("Two factor authentication enabled.");
-
+    const { data: recoveryCodes, message, success } = await enable2FA({ code });
+    if (success) {
+      toast.success(message);
+      setRecoveryCodes(recoveryCodes);
+      onSuccess?.();
       setTimeout(() => {
         toast.success(
           "You will now be required to enter a code from your authenticator app when signing in.",
         );
-      }, 2000);
-    } catch (_err) {
-      toast.error("Unable to setup two-factor authentication");
+      }, 3000);
+    } else {
+      toast.error(message);
     }
   };
 
@@ -104,36 +103,53 @@ export const EnableTwoFactorAppDialog = ({
         type: "text/plain",
       });
       downloadFile({
-        filename: "documenso-2FA-recovery-codes.txt",
+        filename: "recovery_codes_2fa.txt",
         data: blob,
       });
     }
   };
 
   const onSendRecoveryCodes = async () => {
-    if (recoveryCodes) {
-      await send2FARecoveryCodesEmail({
-        email: "rajukadel27@gmail.com",
-        recoveryCodes,
-      });
+    try {
+      if (recoveryCodes) {
+        const { success, message } = await sendRecoveryCodes({ recoveryCodes });
+        if (success) {
+          toast.success(message);
+          setHasEmailSent(success);
+        } else {
+          toast.error(message);
+        }
+      }
+    } catch (error) {
+      toast.error(
+        //@ts-ignore
+        error?.message ?? "Something went out. Please try again later.",
+      );
     }
   };
 
-  const handleEnable2FA = async () => {
+  const handleEnable2FA = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     if (!setup2FAData) {
-      await setup2FA();
+      const { success, message } = await setup2FA();
+      if (success) {
+        setIsOpen(true);
+      } else {
+        toast.error(message);
+      }
     }
-    setIsOpen(true);
   };
 
-  // useEffect(() => {
-  //   enable2FAForm.reset();
-  //   if (!isOpen && recoveryCodes && recoveryCodes.length > 0) {
-  //     setRecoveryCodes(null);
-  //     router.refresh();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [isOpen]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+    form.reset();
+    if (!isOpen && recoveryCodes && recoveryCodes.length > 0) {
+      setRecoveryCodes(null);
+      setIsOpen(false);
+      router.refresh();
+    }
+  }, [isOpen]);
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -148,65 +164,99 @@ export const EnableTwoFactorAppDialog = ({
             <Button
               className="flex-shrink-0"
               loading={isSettingUp2FA}
-              onClick={async (e) => {
-                e.preventDefault();
-                console.log("Enabling 2fa");
-                await handleEnable2FA();
-              }}
+              onClick={handleEnable2FA}
             >
               Enable 2FA
             </Button>
           }
         />
       </DialogTrigger>
-
       <DialogContent>
         {setup2FAData && (
           <>
             {recoveryCodes ? (
-              <div>
-                <DialogHeader>
-                  <DialogTitle>Backup codes</DialogTitle>
-                  <DialogDescription>
-                    Your recovery codes are listed below. Please store them in a
-                    safe place.
-                  </DialogDescription>
-                </DialogHeader>
-
+              <>
+                <header className="border-b border-gray-200 py-2 px-5">
+                  <div className="">
+                    <DialogHeader>
+                      <div className="flex justify-center">
+                        <CaptableLogo className="mb-3 h-8 w-8 rounded" />
+                      </div>
+                      <DialogTitle className="mb-4 text-center">
+                        Recovery codes
+                      </DialogTitle>
+                      <DialogDescription className="text-center">
+                        Your recovery codes are listed below. Please store them
+                        in a safe place.
+                      </DialogDescription>
+                    </DialogHeader>
+                  </div>
+                </header>
                 <div className="mt-4">
                   <RecoveryCodeList recoveryCodes={recoveryCodes} />
                 </div>
-
-                <DialogFooter className="mt-4">
-                  <DialogClose asChild>
-                    <Button variant="secondary">Close</Button>
-                  </DialogClose>
-
-                  <div className="flex space-between items-center">
-                    <Button onClick={onSendRecoveryCodes}>
-                      Backup in my email
-                    </Button>
-                    <Button onClick={downloadRecoveryCodes}>Download</Button>
+                <DialogFooter className="mt-4 w-full">
+                  <div className="w-full flex justify-between items-center">
+                    <div>
+                      <Button
+                        disabled={hasEmailSent}
+                        variant="outline"
+                        onClick={onSendRecoveryCodes}
+                      >
+                        <RiMailLine />
+                        Send to my email
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        className="secondary"
+                        onClick={downloadRecoveryCodes}
+                      >
+                        <RiDownload2Line />
+                        Download
+                      </Button>
+                    </div>
                   </div>
                 </DialogFooter>
-              </div>
+              </>
             ) : (
               <Form {...form}>
                 <form onSubmit={form.handleSubmit(onFormSubmit)}>
-                  <DialogHeader>
-                    <DialogTitle>Enable Authenticator App</DialogTitle>
-                    <DialogDescription>
-                      To enable two-factor authentication, scan the following QR
-                      code using your authenticator app.
-                    </DialogDescription>
-                  </DialogHeader>
+                  <header className="border-b border-gray-200 py-2 px-5">
+                    <div className="">
+                      <DialogHeader>
+                        <div className="flex justify-center">
+                          <CaptableLogo className="mb-3 h-8 w-8 rounded" />
+                        </div>
+                        <DialogTitle className="mb-1 text-center">
+                          Enable Authenticator App
+                        </DialogTitle>
+                      </DialogHeader>
+                    </div>
+                  </header>
 
                   <fieldset
                     disabled={isEnabling2FA}
                     className="mt-4 flex flex-col gap-y-4"
                   >
+                    <p className="text-muted-foreground text-sm">
+                      You can use this code instead if QR code is not supported
+                      in your authenticator app.
+                    </p>
+
+                    <p className="bg-muted/60 text-muted-foreground rounded-lg p-2 text-center font-mono tracking-widest">
+                      {setup2FAData?.data?.secret}
+                    </p>
+
+                    <DialogHeader>
+                      <DialogDescription>
+                        To enable two-factor authentication, scan the following
+                        QR code using your authenticator app.
+                      </DialogDescription>
+                    </DialogHeader>
+
                     <div
-                      className="flex h-36 justify-center"
+                      className="flex h-36 mt-5 justify-center"
                       // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
                       dangerouslySetInnerHTML={{
                         __html: renderSVG(setup2FAData.data?.uri ?? ""),
@@ -214,18 +264,7 @@ export const EnableTwoFactorAppDialog = ({
                     />
 
                     <p className="text-muted-foreground text-sm">
-                      If your authenticator app does not support QR codes, you
-                      can use the following code instead:
-                    </p>
-
-                    <p className="bg-muted/60 text-muted-foreground rounded-lg p-2 text-center font-mono tracking-widest">
-                      {setup2FAData?.data?.secret}
-                    </p>
-
-                    <p className="text-muted-foreground text-sm">
-                      Once you have scanned the QR code or entered the code
-                      manually, enter the code provided by your authenticator
-                      app below.
+                      Please provide the OTP code from your authenticator app.
                     </p>
 
                     <FormField
@@ -233,10 +272,6 @@ export const EnableTwoFactorAppDialog = ({
                       control={form.control}
                       render={({ field }) => (
                         <FormItem>
-                          <FormLabel className="text-muted-foreground">
-                            Token
-                          </FormLabel>
-
                           <FormControl>
                             <>
                               <FormItem>
@@ -255,7 +290,11 @@ export const EnableTwoFactorAppDialog = ({
                         <Button variant="secondary">Cancel</Button>
                       </DialogClose>
 
-                      <Button type="submit" loading={isEnabling2FA}>
+                      <Button
+                        type="submit"
+                        disabled={isEnabling2FA}
+                        loading={isEnabling2FA}
+                      >
                         Enable 2FA
                       </Button>
                     </DialogFooter>

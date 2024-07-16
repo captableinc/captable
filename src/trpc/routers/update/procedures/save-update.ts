03444a9711a397662c72a981041e8b200759c2c9
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-16 11:24:24 +0530

    feat: add Audit functionality in update router

--- src/trpc/routers/update/procedures/save-update.ts
+++ src/trpc/routers/update/procedures/save-update.ts
@@ -1,4 +1,5 @@
 import { generatePublicId } from "@/common/id";
+import { Audit } from "@/server/audit";
 import { withAuth } from "@/trpc/api/trpc";
 import { UpdateMutationSchema } from "../schema";
 
@@ -8,6 +9,9 @@ export const saveUpdateProcedure = withAuth
     try {
       const authorId = ctx.session.user.memberId;
       const companyId = ctx.session.user.companyId;
+      const userId = ctx.session.user.id;
+      const userName = ctx.session.user.name;
+      const { userAgent, requestIp } = ctx;
       const publicId = input.publicId ?? generatePublicId();
       const { title, content, html } = input;
 
@@ -16,37 +20,66 @@ export const saveUpdateProcedure = withAuth
           success: false,
           message: "Title and content cannot be empty.",
         };
-      } else {
-        await ctx.db.$transaction(async (tx) => {
-          if (input.publicId) {
-            await tx.update.update({
-              where: { publicId },
-              data: {
-                html,
-                title,
-                content,
-              },
-            });
-          } else {
-            await tx.update.create({
-              data: {
-                html,
-                title,
-                content,
-                publicId,
-                companyId,
-                authorId,
-              },
-            });
-          }
-        });
-
-        return {
-          publicId,
-          success: true,
-          message: "Successfully saved an update.",
-        };
       }
+      await ctx.db.$transaction(async (tx) => {
+        if (input.publicId) {
+          const update = await tx.update.update({
+            where: { publicId },
+            data: {
+              html,
+              title,
+              content,
+            },
+          });
+
+          await Audit.create(
+            {
+              action: "update.updated",
+              companyId: companyId,
+              actor: { type: "user", id: userId },
+              context: {
+                userAgent,
+                requestIp,
+              },
+              target: [{ type: "update", id: update.id }],
+              summary: `${userName} updated an Update ${update.title} for the company with id ${companyId}`,
+            },
+            tx,
+          );
+        } else {
+          const update = await tx.update.create({
+            data: {
+              html,
+              title,
+              content,
+              publicId,
+              companyId,
+              authorId,
+            },
+          });
+
+          await Audit.create(
+            {
+              action: "update.created",
+              companyId: companyId,
+              actor: { type: "user", id: userId },
+              context: {
+                userAgent,
+                requestIp,
+              },
+              target: [{ type: "update", id: update.id }],
+              summary: `${userName} created an Update ${update.title} for the company with id ${companyId}`,
+            },
+            tx,
+          );
+        }
+      });
+
+      return {
+        publicId,
+        success: true,
+        message: "Successfully saved an update.",
+      };
     } catch (error) {
       console.error("Error saving an update:", error);
       return {

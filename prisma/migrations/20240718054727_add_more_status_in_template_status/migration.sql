95a0de35da6bb2d5065bc0bf6964d06b8a7f8baf
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-18 11:36:04 +0530

    feat: add templateStatus migration

---
+++ prisma/migrations/20240718054727_add_more_status_in_template_status/migration.sql
@@ -0,0 +1,11 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "TemplateStatus" ADD VALUE 'SENT';
+ALTER TYPE "TemplateStatus" ADD VALUE 'WAITING';
+ALTER TYPE "TemplateStatus" ADD VALUE 'CANCELLED';

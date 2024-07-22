000e15e46348fdcfd8eb720410834cf0b45da9c3
nafees nazik <nafeesnazik21@gmail.com>
2024-07-22 08:22:23 +0530

    feat: add new create endpoint

---
+++ src/server/api/routes/stakeholder/create.ts
@@ -0,0 +1,99 @@
+import { z } from "@hono/zod-openapi";
+import { ApiError } from "../../error";
+import {
+  type TCreateStakeholderSchema,
+  createStakeholderSchema,
+  stakeholderSchema,
+} from "../../schema/stakeholder";
+import { withAuthApiV1 } from "../../utils/endpoint-creator";
+
+const responseSchema = z.object({
+  message: z.string(),
+  data: z.array(stakeholderSchema.pick({ id: true, name: true })),
+});
+
+export const create = withAuthApiV1
+  .createRoute({
+    method: "post",
+    path: "/v1/stakeholders",
+    summary: "Create stakeholders",
+    description: "Create one or stakeholders account in a company.",
+    tags: ["Stakeholder"],
+    request: {
+      body: {
+        content: {
+          "application/json": {
+            schema: createStakeholderSchema,
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: responseSchema,
+          },
+        },
+        description: "create a stakeholder for a company",
+      },
+    },
+  })
+  .handler(async (c) => {
+    const { db, audit } = c.get("services");
+    const { membership } = c.get("session");
+    const { requestIp, userAgent } = c.get("info");
+
+    const body = await c.req.json<TCreateStakeholderSchema>();
+
+    try {
+      const stakeholders = await db.$transaction(async (tx) => {
+        const inputDataWithCompanyId = body.map((stakeholder) => ({
+          ...stakeholder,
+          companyId: membership.companyId,
+        }));
+
+        const addedStakeholders = await tx.stakeholder.createManyAndReturn({
+          data: inputDataWithCompanyId,
+          select: {
+            id: true,
+            name: true,
+          },
+        });
+
+        const auditPromises = addedStakeholders.map((stakeholder) =>
+          audit.create(
+            {
+              action: "stakeholder.added",
+              companyId: membership.companyId,
+              actor: { type: "user", id: membership.userId },
+              context: {
+                requestIp,
+                userAgent,
+              },
+              target: [{ type: "stakeholder", id: stakeholder.id }],
+              summary: `${membership.user.name} added the stakholder in the company : ${stakeholder.name}`,
+            },
+            tx,
+          ),
+        );
+        await Promise.all(auditPromises);
+
+        return addedStakeholders;
+      });
+
+      const data: z.infer<typeof responseSchema>["data"] = stakeholders;
+      return c.json(
+        {
+          data,
+          message: "Stakeholders successfully created.",
+        },
+        200,
+      );
+    } catch (_error) {
+      throw new ApiError({
+        code: "BAD_REQUEST",
+        message: "Something went wrong. Please try again later.",
+      });
+    }
+  });

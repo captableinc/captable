3fe8c08f571e991b5480e857cb9f4e068841f5ce
nafees nazik <nafeesnazik21@gmail.com>
2024-07-23 08:14:49 +0530

    chore: fix name

--- src/server/api/schema/shares.ts
+++ src/server/api/schema/shares.ts
@@ -126,9 +126,7 @@ export const ShareSchema = z
       example: "2024-01-01T00:00:00.000Z",
     }),
   })
-  .openapi({
-    description: "Get a Single Share by the ID",
-  });
+  .openapi("Shares");
 
 export const CreateShareSchema = ShareSchema.omit({
   id: true,
--- src/server/api/schema/stakeholder.ts
+++ src/server/api/schema/stakeholder.ts
@@ -12,72 +12,74 @@ const StakeholderRelationshipArray = Object.values(
   StakeholderRelationshipEnum,
 ) as [StakeholderRelationshipEnum, ...StakeholderRelationshipEnum[]];
 
-export const stakeholderSchema = z.object({
-  id: z.string().cuid().openapi({
-    description: "Stakeholder ID",
-    example: "cly13ipa40000i7ng42mv4x7b",
-  }),
+export const stakeholderSchema = z
+  .object({
+    id: z.string().cuid().openapi({
+      description: "Stakeholder ID",
+      example: "cly13ipa40000i7ng42mv4x7b",
+    }),
 
-  name: z.string().openapi({
-    description: "Stakeholder name",
-    example: "John Doe",
-  }),
+    name: z.string().openapi({
+      description: "Stakeholder name",
+      example: "John Doe",
+    }),
 
-  email: z.string().email().openapi({
-    description: "Stakeholder email",
-    example: "email@example.com",
-  }),
+    email: z.string().email().openapi({
+      description: "Stakeholder email",
+      example: "email@example.com",
+    }),
 
-  institutionName: z.string().nullish().openapi({
-    description: "Institution name",
-    example: "ACME Corp",
-  }),
+    institutionName: z.string().nullish().openapi({
+      description: "Institution name",
+      example: "ACME Corp",
+    }),
 
-  stakeholderType: z.enum(StakeholderTypeArray).openapi({
-    description: "Stakeholder type",
-    example: "INDIVIDUAL",
-  }),
+    stakeholderType: z.enum(StakeholderTypeArray).openapi({
+      description: "Stakeholder type",
+      example: "INDIVIDUAL",
+    }),
 
-  currentRelationship: z.enum(StakeholderRelationshipArray).openapi({
-    description: "Current relationship with the company",
-    example: "EMPLOYEE",
-  }),
+    currentRelationship: z.enum(StakeholderRelationshipArray).openapi({
+      description: "Current relationship with the company",
+      example: "EMPLOYEE",
+    }),
 
-  streetAddress: z.string().nullish().openapi({
-    description: "Street address",
-    example: "123 Main St",
-  }),
+    streetAddress: z.string().nullish().openapi({
+      description: "Street address",
+      example: "123 Main St",
+    }),
 
-  city: z.string().nullish().openapi({
-    description: "City",
-    example: "San Francisco",
-  }),
+    city: z.string().nullish().openapi({
+      description: "City",
+      example: "San Francisco",
+    }),
 
-  state: z.string().nullish().openapi({
-    description: "State",
-    example: "CA",
-  }),
+    state: z.string().nullish().openapi({
+      description: "State",
+      example: "CA",
+    }),
 
-  zipcode: z.string().nullish().openapi({
-    description: "Zip code",
-    example: "94105",
-  }),
+    zipcode: z.string().nullish().openapi({
+      description: "Zip code",
+      example: "94105",
+    }),
 
-  country: z.string().optional().openapi({
-    description: "Country",
-    example: "USA",
-  }),
+    country: z.string().optional().openapi({
+      description: "Country",
+      example: "USA",
+    }),
 
-  createdAt: z.string().date().openapi({
-    description: "Date the stakeholder was created",
-    example: "2022-01-01T00:00:00Z",
-  }),
+    createdAt: z.string().date().openapi({
+      description: "Date the stakeholder was created",
+      example: "2022-01-01T00:00:00Z",
+    }),
 
-  updatedAt: z.string().date().openapi({
-    description: "Date the stakeholder was last updated",
-    example: "2022-01-01T00:00:00Z",
-  }),
-});
+    updatedAt: z.string().date().openapi({
+      description: "Date the stakeholder was last updated",
+      example: "2022-01-01T00:00:00Z",
+    }),
+  })
+  .openapi("Stakeholder");
 
 export const createStakeholderSchema = z.array(
   stakeholderSchema.omit({

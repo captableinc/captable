70c7fb912cf2255213587e73718f1734d4a7ea5b
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-24 12:51:21 +0530

    feat: add getMany Members route in index.ts

--- src/server/api/routes/company/team-member/index.ts
+++ src/server/api/routes/company/team-member/index.ts
@@ -1,6 +1,8 @@
 import type { PublicAPI } from "@/server/api/hono";
+import getMany from "./getMany";
 import getOne from "./getOne";
 
 export const registerTeamMemberRoutes = (api: PublicAPI) => {
   getOne(api);
+  getMany(api);
 };

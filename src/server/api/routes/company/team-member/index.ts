3b3c687b8dba051e33db3421f05f1339cf91ff76
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-25 12:05:27 +0530

    feat: call create route in index.ts

--- src/server/api/routes/company/team-member/index.ts
+++ src/server/api/routes/company/team-member/index.ts
@@ -1,8 +1,10 @@
 import type { PublicAPI } from "@/server/api/hono";
+import create from "./create";
 import getMany from "./getMany";
 import getOne from "./getOne";
 
 export const registerTeamMemberRoutes = (api: PublicAPI) => {
   getOne(api);
   getMany(api);
+  create(api);
 };

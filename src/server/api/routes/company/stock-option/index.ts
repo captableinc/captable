47fea80ac5f0082ca5f4dca3c7cbce9be82cce68
Raju kadel <rajukadel.github@gmail.com>
2024-07-26 02:03:36 +0545

    feat: getting started with stock-option index file

---
+++ src/server/api/routes/company/stock-option/index.ts
@@ -0,0 +1,14 @@
+import type { PublicAPI } from "@/server/api/hono";
+import create from "./create";
+import deleteOne from "./delete";
+import getMany from "./getMany";
+import getOne from "./getOne";
+import update from "./update";
+
+export const registerStockOptionRoutes = (api: PublicAPI) => {
+  create(api);
+  getOne(api);
+  getMany(api);
+  update(api);
+  deleteOne(api);
+};

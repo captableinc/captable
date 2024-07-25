19aa3a20f4a8f8211b0e86255a4ff1fc83919d96
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-25 22:26:18 +0530

    feat: call update member route

--- src/server/api/routes/company/team-member/index.ts
+++ src/server/api/routes/company/team-member/index.ts
@@ -2,9 +2,11 @@ import type { PublicAPI } from "@/server/api/hono";
 import create from "./create";
 import getMany from "./getMany";
 import getOne from "./getOne";
+import update from "./update";
 
 export const registerTeamMemberRoutes = (api: PublicAPI) => {
   getOne(api);
   getMany(api);
   create(api);
+  update(api);
 };

463ffd4373d70976a8429cecdff742b4e5129590
Raju kadel <rajukadel.github@gmail.com>
2024-07-29 02:38:11 +0545

    feat: add create option route

---
+++ src/server/api/routes/company/stock-option/create.ts
@@ -0,0 +1,100 @@
+import { withCompanyAuth } from "@/server/api/auth";
+import {
+  ApiError,
+  type ErrorCodeType,
+  ErrorResponses,
+} from "@/server/api/error";
+import type { PublicAPI } from "@/server/api/hono";
+import { OptionSchema, type TCreateOptionSchema } from "../../../schema/option";
+import type { TOptionSchema } from "./../../../schema/option";
+
+import { CreateOptionSchema } from "@/server/api/schema/option";
+import { getHonoUserAgent, getIp } from "@/server/api/utils";
+import { addOption } from "@/server/services/stock-option/add-option";
+import { createRoute, z } from "@hono/zod-openapi";
+import type { Context } from "hono";
+
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .cuid()
+    .openapi({
+      description: "Company ID",
+      param: {
+        name: "id",
+        in: "path",
+      },
+
+      example: "clycjihpy0002c5fzcyf4gjjc",
+    }),
+});
+
+const ResponseSchema = z.object({
+  message: z.string(),
+  data: OptionSchema,
+});
+
+const route = createRoute({
+  method: "post",
+  path: "/v1/companies/{id}/options",
+  summary: "Issue option",
+  description: "Issue stock option to a stakeholder in a company.",
+  tags: ["Options"],
+  request: {
+    params: ParamsSchema,
+    body: {
+      content: {
+        "application/json": {
+          schema: CreateOptionSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: ResponseSchema,
+        },
+      },
+      description: "Issue options to stakeholders",
+    },
+    ...ErrorResponses,
+  },
+});
+
+const create = (app: PublicAPI) => {
+  app.openapi(route, async (c: Context) => {
+    const { company, member, user } = await withCompanyAuth(c);
+    const body = await c.req.json();
+
+    const { success, message, data, code } = await addOption({
+      requestIP: getIp(c.req),
+      userAgent: getHonoUserAgent(c.req),
+      data: body as TCreateOptionSchema,
+      companyId: company.id,
+      memberId: member.id,
+      user: {
+        id: user.id,
+        name: user.name || "",
+      },
+    });
+
+    if (!success) {
+      throw new ApiError({
+        code: code as ErrorCodeType,
+        message,
+      });
+    }
+
+    return c.json(
+      {
+        message: "Stock option added successfully.",
+        data: data as unknown as TOptionSchema,
+      },
+      200,
+    );
+  });
+};
+
+export default create;

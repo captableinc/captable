ee093304adb5b4f6ab1553f74941776b1617935b
Raju kadel <rajukadel.github@gmail.com>
2024-07-29 02:40:28 +0545

    feat: zod schemas for option endpoint

---
+++ src/server/api/schema/option.ts
@@ -0,0 +1,156 @@
+import { OptionStatusEnum, OptionTypeEnum } from "@/prisma/enums";
+import { z } from "zod";
+
+const OptionTypeArray = Object.values(OptionTypeEnum) as [
+  OptionTypeEnum,
+  ...OptionTypeEnum[],
+];
+const OptionStatusArray = Object.values(OptionStatusEnum) as [
+  OptionStatusEnum,
+  ...OptionStatusEnum[],
+];
+
+export const OptionSchema = z
+  .object({
+    id: z.string().optional().openapi({
+      description: "Stock Option ID",
+      example: "abc123",
+    }),
+
+    grantId: z.string().openapi({
+      description: "Grant ID",
+      example: "grant123",
+    }),
+
+    quantity: z.coerce.number().min(0).openapi({
+      description: "Quantity of Stock Options",
+      example: 100,
+    }),
+
+    exercisePrice: z.coerce.number().min(0).openapi({
+      description: "Exercise Price per Stock Option",
+      example: 10.5,
+    }),
+
+    type: z.enum(OptionTypeArray).openapi({
+      description: "Type of Stock Option",
+      example: "ISO",
+    }),
+
+    status: z.enum(OptionStatusArray).openapi({
+      description: "Status of Stock Option",
+      example: "DRAFT",
+    }),
+
+    cliffYears: z.coerce.number().min(0).openapi({
+      description: "Cliff Years",
+      example: 1,
+    }),
+
+    vestingYears: z.coerce.number().min(0).openapi({
+      description: "Vesting Years",
+      example: 4,
+    }),
+
+    issueDate: z.string().datetime().openapi({
+      description: "Issue Date",
+      example: "2024-01-01",
+    }),
+
+    expirationDate: z.string().datetime().openapi({
+      description: "Expiration Date",
+      example: "2028-01-01",
+    }),
+
+    vestingStartDate: z.string().datetime().openapi({
+      description: "Vesting Start Date",
+      example: "2024-01-01",
+    }),
+
+    boardApprovalDate: z.string().datetime().openapi({
+      description: "Board Approval Date",
+      example: "2024-01-01",
+    }),
+
+    rule144Date: z.string().datetime().openapi({
+      description: "Rule 144 Date",
+      example: "2024-01-01",
+    }),
+
+    stakeholderId: z.string().openapi({
+      description: "Stakeholder ID",
+      example: "stakeholder123",
+    }),
+
+    companyId: z.string().cuid().openapi({
+      description: "Company ID",
+      example: "clyvb28ak0000f1ngcn2i0p2m",
+    }),
+
+    equityPlanId: z.string().openapi({
+      description: "Equity Plan ID",
+      example: "equityPlan123",
+    }),
+
+    documents: z
+      .array(
+        z.object({
+          bucketId: z.string(),
+          name: z.string(),
+        }),
+      )
+      .optional()
+      .openapi({
+        description: "Documents",
+        example: [
+          { bucketId: "clyvb28ak0000f1ngcn2i0p2m", name: "Esign docs" },
+        ],
+      }),
+
+    createdAt: z.string().date().nullish().openapi({
+      description: "Option Created at",
+      example: "2024-01-01T00:00:00.000Z",
+    }),
+
+    updatedAt: z.string().date().nullish().openapi({
+      description: "Option Updated at",
+      example: "2024-01-01T00:00:00.000Z",
+    }),
+  })
+  .openapi({
+    description: "Get a Single Option by the ID",
+  });
+
+export const CreateOptionSchema = OptionSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  companyId: true,
+})
+  .strict()
+  .openapi({
+    description: "Issue options to a stakeholder in a company.",
+  });
+
+export const UpdateOptionSchema = OptionSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+})
+  .partial()
+  .strict()
+  .refine(
+    (data) => {
+      return Object.values(data).some((value) => value !== undefined);
+    },
+    {
+      message: "At least one field must be provided to update.",
+    },
+  )
+  .openapi({
+    description: "Update an option by ID",
+  });
+
+export type TOptionSchema = z.infer<typeof OptionSchema>;
+export type TCreateOptionSchema = z.infer<typeof CreateOptionSchema>;
+export type TUpdateOptionSchema = z.infer<typeof UpdateOptionSchema>;

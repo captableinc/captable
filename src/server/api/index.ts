98d171ce21e33084fd525013167b3b91a5cf4893
Raju kadel <rajukadel.github@gmail.com>
2024-07-29 02:42:22 +0545

    feat: register option routes

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -3,6 +3,7 @@ import { initMiddleware } from "./middlewares/init";
 import { registerCompanyRoutes } from "./routes/company";
 import { registerShareRoutes } from "./routes/company/share";
 import { registerStakeholderRoutes } from "./routes/company/stakeholder";
+import { registerOptionRoutes } from "./routes/company/stock-option";
 
 const api = PublicAPI();
 
@@ -12,5 +13,6 @@ api.use("*", initMiddleware());
 registerCompanyRoutes(api);
 registerShareRoutes(api);
 registerStakeholderRoutes(api);
+registerOptionRoutes(api);
 
 export default api;

71da37c757d58732aaa77fe9f86fde2d20437e48
nafees nazik <nafeesnazik21@gmail.com>
2024-07-29 20:33:42 +0530

    feat: add tokens utility

---
+++ src/lib/tokens.ts
@@ -0,0 +1,45 @@
+import { customId } from "@/common/id";
+import bcrypt from "bcryptjs";
+import { base58 } from "./base-58";
+
+type TTokenPrefixes = "pat" | "api";
+
+const SEPARATOR = "_";
+const SPLITTER = ":";
+
+export const generateTokens = (prefix: TTokenPrefixes) => {
+  const identifier = customId(16);
+  const tokenPasskey = customId(16);
+  const encodedToken = base58.encode(
+    Buffer.from(identifier + SPLITTER + tokenPasskey, "utf8"),
+  );
+  const tokenWithPrefix = [prefix, encodedToken].join(SEPARATOR);
+  const partialToken = `${tokenWithPrefix.slice(
+    0,
+    3,
+  )}...${tokenWithPrefix.slice(-4)}`;
+  return {
+    encodedToken,
+    partialToken,
+    identifier,
+    tokenWithPrefix,
+    tokenPasskey,
+  };
+};
+
+export const hashToken = async (key: string) => {
+  const salt = await bcrypt.genSalt(10);
+
+  return bcrypt.hash(key, salt);
+};
+
+export const verifyToken = (password: string, hash: string) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const decodeToken = (key: string) => {
+  const decodeValue = Buffer.from(base58.decode(key)).toString("utf-8");
+
+  const splittedKey = decodeValue.split(SPLITTER);
+  return { identifier: splittedKey[0] ?? "", passkey: splittedKey[1] ?? "" };
+};

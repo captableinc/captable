92cc18d482093508e393443091d2930911705ff7
nafees nazik <nafeesnazik21@gmail.com>
2024-07-31 06:17:27 +0530

    feat: add utils for tokens

--- src/lib/access-token.ts
+++ src/lib/access-token.ts
@@ -1,3 +1,18 @@
-import { generateTokens } from "./tokens";
+import { generateTokens, hashToken, verifyToken } from "./tokens";
 
 export const generateAccessToken = () => generateTokens("pat");
+
+interface hashApiKeyOptions {
+  identifier: string;
+  passkey: string;
+}
+export const hashAccessToken = ({ identifier, passkey }: hashApiKeyOptions) => {
+  return hashToken(identifier + passkey);
+};
+
+export const verifyAccessToken = (
+  { identifier, passkey }: hashApiKeyOptions,
+  hash: string,
+) => {
+  return verifyToken(identifier + passkey, hash);
+};

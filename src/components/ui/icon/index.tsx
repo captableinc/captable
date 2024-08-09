e164b0fc195531cf97768ce72824ebb717243021
nafees nazik <nafeesnazik21@gmail.com>
2024-08-09 23:26:52 +0530

    fix: icon

--- src/components/ui/icon/index.tsx
+++ src/components/ui/icon/index.tsx
@@ -6,10 +6,9 @@ import { cn } from "@/lib/utils";
 export type { IconName };
 
 const sizeClassName = {
-  sm: "w-5 h-5",
-  md: "w-6 h-6",
-  lg: "w-7 h-7",
-  xl: "w-8 h-8",
+  sm: "w-6 h-6",
+  md: "w-7 h-7",
+  lg: "w-8 h-8",
 } as const;
 
 type Size = keyof typeof sizeClassName;

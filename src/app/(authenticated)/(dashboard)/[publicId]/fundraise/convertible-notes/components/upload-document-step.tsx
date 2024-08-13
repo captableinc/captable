860a5c79537041b9b5f7831624aab2dd3c23ecf8
nafees nazik <nafeesnazik21@gmail.com>
2024-08-13 21:58:13 +0530

    feat: add document step

--- src/app/(authenticated)/(dashboard)/[publicId]/fundraise/convertible-notes/components/upload-document-step.tsx
+++ src/app/(authenticated)/(dashboard)/[publicId]/fundraise/convertible-notes/components/upload-document-step.tsx
@@ -1,15 +1,24 @@
+import { Button } from "@/components/ui/button";
 import { FileUploader } from "@/components/ui/file-uploader";
+import { StepperModalFooter, StepperPrev } from "@/components/ui/stepper";
 
 export function UploadDocumentStep() {
   return (
-    <FileUploader
-      accept={{
-        "application/pdf": [".pdf"],
-      }}
-      multiple
-      maxSize={1024 * 1024 * 50}
-      // biome-ignore lint/style/useNumberNamespace: <explanation>
-      maxFileCount={Infinity}
-    />
+    <div>
+      <FileUploader
+        accept={{
+          "application/pdf": [".pdf"],
+        }}
+        multiple
+        maxSize={1024 * 1024 * 50}
+        // biome-ignore lint/style/useNumberNamespace: <explanation>
+        maxFileCount={Infinity}
+      />
+
+      <StepperModalFooter className="pt-6">
+        <StepperPrev>Back</StepperPrev>
+        <Button type="submit">Save & Continue</Button>
+      </StepperModalFooter>
+    </div>
   );
 }

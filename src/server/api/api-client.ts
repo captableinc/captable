ebd8f349d44240521b7c8d48afa2057d493cdaa7
nafees nazik <nafeesnazik21@gmail.com>
2024-08-14 08:16:54 +0530

    fix: api client

--- src/server/api/api-client.ts
+++ src/server/api/api-client.ts
@@ -2,10 +2,8 @@ import { env } from "@/env";
 import { hc } from "hono/client";
 import type { APIType } from ".";
 
-export const apiClient = hc<APIType>(env.NEXTAUTH_URL, {
-  fetch: (input: RequestInfo | URL, init?: RequestInit) =>
-    fetch(input, {
-      ...init,
-      cache: "no-cache",
-    }),
+export const apiClient = hc<APIType>(env.NEXT_PUBLIC_BASE_URL, {
+  init: {
+    cache: "no-cache",
+  },
 });

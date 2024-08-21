c04265cdec8515a399b66f7ef96023e1f4bf8062
nafees nazik <nafeesnazik21@gmail.com>
2024-08-21 14:10:05 +0530

    feat: register jobs

---
+++ src/jobs/register.ts
@@ -0,0 +1,12 @@
+import { sendMemberInviteEmailWorker } from "./member-inivite-email";
+import { passwordResetEmailWorker } from "./password-reset-email";
+import { queueManager } from "./queue";
+import { shareDataRoomEmailWorker } from "./share-data-room-email";
+import { shareUpdateEmailWorker } from "./share-update-email";
+
+export function registerJobs() {
+  queueManager.register(shareDataRoomEmailWorker);
+  queueManager.register(shareUpdateEmailWorker);
+  queueManager.register(passwordResetEmailWorker);
+  queueManager.register(sendMemberInviteEmailWorker);
+}

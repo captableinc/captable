1238f7150bcc95f3aee03d35bd6622bc7b57c867
nafees nazik <nafeesnazik21@gmail.com>
2024-08-28 08:45:11 +0530

    feat: add const

--- src/server/api/const.ts
+++ src/server/api/const.ts
@@ -1,2 +1,3 @@
-export const DEFAULT_PAGINATION_LIMIT = 50;
+export const DEFAULT_PAGINATION_LIMIT = 10 as const;
+export const OFFSET_MAXIMUM_LIMIT = 50 as const;
 export const SECURITY_SCHEME_NAME = "Bearer";

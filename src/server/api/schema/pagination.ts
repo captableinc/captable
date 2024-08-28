99ff880bb8cda2a886a3b4769cba8ab2f625cd84
nafees nazik <nafeesnazik21@gmail.com>
2024-08-28 08:44:26 +0530

    feat: add offset schema

--- src/server/api/schema/pagination.ts
+++ src/server/api/schema/pagination.ts
@@ -1,5 +1,40 @@
 import { z } from "@hono/zod-openapi";
-import { DEFAULT_PAGINATION_LIMIT } from "../const";
+import { DEFAULT_PAGINATION_LIMIT, OFFSET_MAXIMUM_LIMIT } from "../const";
+
+export const OffsetPaginationQuerySchema = z.object({
+  limit: z.coerce
+    .number()
+    .max(OFFSET_MAXIMUM_LIMIT)
+    .positive()
+    .optional()
+    .default(DEFAULT_PAGINATION_LIMIT)
+    .openapi({
+      description: `The number of items to return per page. The maximum value is ${OFFSET_MAXIMUM_LIMIT}.`,
+      param: {
+        name: "limit",
+        in: "query",
+      },
+      example: DEFAULT_PAGINATION_LIMIT,
+      default: DEFAULT_PAGINATION_LIMIT,
+      maximum: OFFSET_MAXIMUM_LIMIT,
+      minimum: 1,
+    }),
+  page: z.coerce
+    .number()
+    .positive()
+    .optional()
+    .default(1)
+    .openapi({
+      description: "The page number to retrieve. Starts from 1.",
+      param: {
+        name: "page",
+        in: "query",
+      },
+      example: 1,
+      default: 1,
+      minimum: 1,
+    }),
+});
 
 export const PaginationQuerySchema = z.object({
   limit: z
@@ -53,7 +88,6 @@ export const PaginationResponseSchema = z
     hasNextPage: z.boolean().openapi({
       description:
         "Indicates if there is a next page available in the pagination. `true` if there are more pages after the current one, `false` otherwise.",
-      example: false,
     }),
 
     startCursor: z.string().nullable().openapi({
@@ -68,4 +102,37 @@ export const PaginationResponseSchema = z
       example: "cly151kxq0000i7ngb3erchgo",
     }),
   })
-  .openapi("Pagination");
+  .openapi("Pagination Cursor");
+
+export const OffsetPaginationResponseSchema = z
+  .object({
+    isFirstPage: z.boolean().openapi({
+      description:
+        "Indicates whether the current page is the first page of the pagination.",
+    }),
+    isLastPage: z.boolean().openapi({
+      description:
+        "Indicates whether the current page is the last page of the pagination.",
+    }),
+    currentPage: z.number().openapi({
+      description: "The current page number in the pagination.",
+    }),
+    previousPage: z.number().nullable().openapi({
+      description:
+        "The previous page number in the pagination. Null if the current page is the first page.",
+    }),
+    nextPage: z.number().nullable().openapi({
+      description:
+        "The next page number in the pagination. Null if the current page is the last page.",
+    }),
+    pageCount: z.number().openapi({
+      description: "The total number of pages available.",
+    }),
+    totalCount: z.number().openapi({
+      description: "The total number of items across all pages.",
+    }),
+  })
+  .openapi("Offset Pagination", {
+    description:
+      "A schema representing the pagination details of an offset-based pagination system.",
+  });

2409c0d6381b93465bb057047cbe9ccc8a262cf4
nafees nazik <nafeesnazik21@gmail.com>
2024-08-29 22:34:10 +0530

    feat: add debounce input

--- src/components/stakeholder/stakeholder-table-toolbar.tsx
+++ src/components/stakeholder/stakeholder-table-toolbar.tsx
@@ -1,21 +1,20 @@
 import { useTable } from "@/components/ui/data-table/data-table";
 import { ResetButton } from "@/components/ui/data-table/data-table-buttons";
 import { DataTableViewOptions } from "@/components/ui/data-table/data-table-view-options";
-import { Input } from "@/components/ui/input";
+import { DebouncedInput } from "../ui/debounced-input";
 
 export const StakeholderTableToolbar = () => {
   const { table } = useTable();
   const isFiltered = table.getState().columnFilters.length > 0;
 
+  const value = table.getColumn("name")?.getFilterValue() as string;
   return (
     <div className="flex w-full items-center justify-between">
       <div className="flex flex-col gap-2 sm:flex-1 sm:flex-row sm:items-center sm:gap-0 sm:space-x-2">
-        <Input
+        <DebouncedInput
           placeholder="Search by name..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
-          }
+          value={value}
+          onChange={(value) => table.getColumn("name")?.setFilterValue(value)}
           className="h-8 w-64"
         />
         <div className="space-x-2">
